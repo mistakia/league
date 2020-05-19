b66180f90b67714a03ee06480bfe0837e3caacac
mistakia <1823355+mistakia@users.noreply.github.com>
2020-05-19 00:26:24 -0400

    feat: add transactions api

--- api/routes/transactions.js
+++ api/routes/transactions.js
@@ -1,12 +1,205 @@
 const express = require('express')
 const router = express.Router()
+const { constants, getEligibleSlots } = require('../../common')
+
+const formatRoster = (roster) => {
+  let result = new Map()
+  Object.keys(roster).forEach(k => k.startsWith('s') && result.set(k, roster[k]))
+  return result
+}
+
 
 router.get('/?', async (req, res) => {
   // TODO return list of league transactions
 })
 
 router.post('/?', async (req, res) => {
-  // TODO create transaction
+  const { db, logger } = req.app.locals
+  try {
+    let { slot, type, tid, player, drop, pos, lid, bid } = req.app.query
+
+    if (!tid) {
+      return res.status(400).send({ error: 'missing tid param' })
+    }
+
+    if (!player) {
+      return res.status(400).send({ error: 'missing player param' })
+    }
+
+    const transactions = await db('transactions')
+      .where({ tid, player })
+      .limit(2)
+      .orderBy('timestamp', 'desc')
+
+    let value = transactions[0].value
+    let dropValue
+
+    const insert = async () => {
+      const inserts = [{
+          userid: req.user.userId,
+          tid,
+          player,
+          type,
+          value,
+          year: constants.year,
+          timestamp: new Date()
+      }]
+
+      if (drop) {
+        inserts.unshift({
+          userid: req.user.userId,
+          tid,
+          value: dropValue,
+          player: drop,
+          type: constants.transactions.ROSTER_DROP,
+          year: constants.year,
+          timestamp: new Date()
+        })
+      }
+
+      const rows = await db('transactions').insert(inserts)
+      res.send({ transactions: rows })
+    }
+
+    const rosters = await db('rosters').where({ tid }).orderBy('last_updated', 'desc')
+    if (rosters.length) {
+      return res.status(500).send({ error: 'unable to locate a roster' })
+    }
+    const roster = formatRoster(roster[0])
+    const isRostered = () => Array.from(roster.values()).includes(player)
+
+    // make sure player is rostered
+    if (type === constants.transactions.ROSTER_DROP) {
+      if (!isRostered()) {
+        return res.status(400).send({ error: `cannot drop unrostered player: ${player}` })
+      }
+
+      return insert()
+    }
+
+    // select a slot if none is provided
+    if (!slot) {
+      const eligbleSlots = getEligbleSlots({ pos, bench: true, ir: true, ps: true })
+      slot = eligibleSlots[0]
+      if (!slot) {
+        return res.status(400).send({ error: 'no eligible slots' })
+      }
+    } else if (drop && drop !== roster[`s${slot}`]) { // validate drop
+      return res.status(400).send({ error: `${drop} does not match player in slot ${slot}` })
+    } else if (roster[`s${slot}`]) { // validate slot
+      return res.status(400).send({ error: `${slot} is occupied and no drop param was sent` })
+    }
+
+    if (drop) {
+      const prev = db('transactions')
+        .where({
+          player: drop,
+          tid
+        })
+        .orderBy('timestamp', 'desc')
+        .limit(1)
+
+      if (!prev.length) {
+        return res.status(400).send({ error: 'could not find transaction for drop player' })
+      }
+
+      dropValue = prev[0].value
+    }
+
+    if (type === constants.transactions.ROSTER_ACTIVATE) {
+      const psSlots = getEligibleSlots({ ps: true })
+      const slots = Array.from(roster.keys())
+      const psPlayers = slots.filter(s => psSlots.includes(s)).map(s => roster.get(s))
+      if (!psPlayers.includes(player)) {
+        return res.status(400).send({ error: `${player} is not on practice squad` })
+      }
+
+      return insert()
+    }
+
+    if (type === constants.transactions.ROSTER_DEACTIVATE) {
+      if (!isRostered()) {
+        return res.status(400).send({ error: 'can not deactivate unrostered player' })
+      }
+
+      const transaction = transactions[0]
+
+      if (!transaction) {
+        return res.status(400).send({ error: 'could not find eligible transaction' })
+      }
+
+      // rostered within the last 48 hours
+      const cutoff = moment().subtract(48, 'hours')
+      if (moment(transaction.timestamp).isBefore(cutoff)) {
+        return res.status(400).send({ error: 'player is not eligible, missed cutoff' })
+      }
+
+      // was not previously on this teams active roster
+      // TODO - handle poach / waiver / trade
+      if (transactions[1].tid !== tid) {
+        return res.status(400).send({ error: 'player is not eligible, was previously on active roster' })
+      }
+
+      return insert()
+    }
+
+    if (type === constants.transactions.ROSTER_ADD) {
+      if (!transactions.length) {
+        return insert()
+      }
+
+      if (transactions[0].type !== constants.transactions.ROSTER_DROP) {
+        return res.status(400).send({ error: 'player is not available' })
+      }
+
+      // TODO - check for waiver periods
+
+      const cutoff = moment().hour() > 6
+        ? moment('6', 'H').subtract(24, 'hours')
+        : moment('6', 'H').subtract(48, 'hours')
+      if (moment(transactions[0].timestamp).isAfter(cutoff)) {
+        return res.status(400).send({ error: 'player is on waivers' })
+      }
+
+      return insert()
+    }
+
+    if (type === constants.transactions.POACH_CLAIM) {
+      if (!lid) {
+        return res.status(400).send({ error: 'missing lid param' })
+      }
+
+      //TODO
+      const psSlots = getEligibleSlots({ ir: true })
+      const onPS = await db('rosters')
+        .where({ lid })
+        .whereNot({ tid })
+        .andWhere(function () {
+          psSlots.forEach((k) => this.orWhere(`s${k}`, player))
+        })
+        .orderBy('week', 'desc')
+
+      if (!onPS.length) {
+        return res.status(400).send({ error: 'player not on a practice squad' })
+      }
+
+      return insert()
+    }
+
+    if (type === constants.transactions.WAIVER_CLAIM) {
+      if (transactions[0].type !== constants.transactions.ROSTER_DROP) {
+        return res.status(400).send({ error: 'player is not available' })
+      }
+
+      // TODO - check for waiver periods
+      value = bid
+      return insert()
+    }
+
+  } catch (err) {
+    logger(err)
+    res.status(500).send({ error: err.toString() })
+  }
 })
 
 module.exports = router
--- common/constants.js
+++ common/constants.js
@@ -1,4 +1,13 @@
+import moment from 'moment'
 import espn from './espn-constants'
+
+const start = moment('9/1', 'M/D')
+const diff = moment().diff(start, 'weeks')
+const week = diff < 0 ? 0 : diff
+
+const year = moment().month() > 2
+  ? moment().year()
+  : moment().year() - 1
 
 const positions = ['QB', 'RB', 'WR', 'TE']
 const stats = [
@@ -23,8 +32,73 @@ const stats = [
   'twoptc'
 ]
 
+const slots = {
+  QB_ONE: 0,
+  QB_TWO: 1,
+  RB_ONE: 2,
+  RB_TWO: 3,
+  RB_THREE: 4,
+  WR_ONE: 5,
+  WR_TWO: 6,
+  WR_THREE: 7,
+  TE_ONE: 8,
+  TE_TWO: 9,
+  WRTE_ONE: 10,
+  WRRB_ONE: 11,
+  WRRB_TWO: 12,
+  WRRBTE_ONE: 13,
+  WRRBTE_TWO: 14,
+  QBRBWRTE_ONE: 15,
+  K_ONE: 16,
+  DST_ONE: 17,
+  IR_ONE: 18,
+  IR_TWO: 19,
+  IR_THREE: 20,
+  PS_ONE: 21,
+  PS_TWO: 22,
+  PS_THREE: 23,
+  PS_FOUR: 24,
+  PS_FIVE: 25,
+  PS_SIX: 26,
+  BENCH_ONE: 27,
+  BENCH_TWO: 28,
+  BENCH_THREE: 29,
+  BENCH_FOUR: 30,
+  BENCH_FIVE: 31,
+  BENCH_SIX: 32,
+  BENCH_SEVEN: 33,
+  BENCH_EIGHT: 34,
+  BENCH_NINE: 35
+}
+
+const transactions = {
+  ROSTER_ADD: 0,
+  ROSTER_DROP: 1,
+
+  ROSTER_ACTIVATE: 2,
+  ROSTER_DEACTIVATE: 3,
+  POACH_CLAIM: 4,
+  POACH_PROCESSED: 5,
+
+  AUCTION_BID: 6,
+  AUCTION_PROCESSED: 7,
+
+  DRAFT: 8,
+
+  EXTENSION: 9,
+  TRANSITION_TAG: 10,
+  FRANCHISE_TAG: 11,
+  ROOKIE_TAG: 12,
+
+  POACH_CLAIM: 13,
+  POACH_PROCESSED: 14
+}
+
 export {
+  start,
+  week,
   positions,
+  slots,
   stats,
   espn
 }
