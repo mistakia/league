a8c3c88555ea3a7439d71c077980c67410f41cdd
mistakia <1823355+mistakia@users.noreply.github.com>
2020-06-14 13:46:49 -0400

    refactor: player filters

--- app/views/components/position-filter/position-filter.js
+++ app/views/components/position-filter/position-filter.js
@@ -1,64 +1,58 @@
 import React from 'react'
 
-import Filter from '@components/filter'
+import PlayerFilter from '@components/player-filter'
 
 class PositionFilter extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      checkboxes: [
+      label: 'POSITIONS',
+      values: [
         {
           label: 'QB',
           value: 'QB',
-          checked: this.props.positions.includes('QB')
+          selected: this.props.positions.includes('QB')
         },
         {
           label: 'RB',
           value: 'RB',
-          checked: this.props.positions.includes('RB')
+          selected: this.props.positions.includes('RB')
         },
         {
           label: 'WR',
           value: 'WR',
-          checked: this.props.positions.includes('WR')
+          selected: this.props.positions.includes('WR')
         },
         {
           label: 'TE',
           value: 'TE',
-          checked: this.props.positions.includes('TE')
+          selected: this.props.positions.includes('TE')
         },
         {
           label: 'K',
           value: 'K',
-          checked: this.props.positions.includes('K')
+          selected: this.props.positions.includes('K')
         },
         {
           label: 'DST',
           value: 'DST',
-          checked: this.props.positions.includes('DST')
+          selected: this.props.positions.includes('DST')
         }
       ]
     }
 
-    this.handleCheckboxgroupChange = this.handleCheckboxgroupChange.bind(this)
+    this.onChange = this.onChange.bind(this)
   }
 
-  handleCheckboxgroupChange (updatedState) {
-    this.setState({
-      checkboxes: updatedState
-    })
-
-    const positions = updatedState.filter(i => i.checked).map(i => i.value)
+  onChange (values) {
+    this.setState({ values })
+    const positions = values.filter(i => i.selected).map(i => i.value)
     this.props.filter(positions)
   }
 
   render () {
-    const { checkboxes } = this.state
     return (
-      <Filter
-        checkboxes={checkboxes}
-        onChange={this.handleCheckboxgroupChange}
-      />
+      <PlayerFilter {...this.state} onChange={this.onChange} />
     )
   }
 }
