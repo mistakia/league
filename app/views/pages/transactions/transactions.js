5fb1d641fc2e725793f3bce55a6fad8063855746
mistakia <1823355+mistakia@users.noreply.github.com>
2020-06-16 09:48:14 -0400

    feat: setup league pages

--- app/views/pages/transactions/transactions.js
+++ app/views/pages/transactions/transactions.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 import PageLayout from '@layouts/page'
 
+import './transactions.styl'
+
 export default function () {
   const body = (
-    <div>Transactions</div>
+    <div className='transactions'>
+      <div className='transactions__filter'>
+        {/* Team Filter */}
+        {/* Transaction Type Filter */}
+      </div>
+      <div className='transactions__body'>
+      </div>
+    </div>
   )
 
   return (
