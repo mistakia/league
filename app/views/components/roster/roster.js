84b9bbbf78114e8965fa4cdeac29bd5643786a1d
mistakia <1823355+mistakia@users.noreply.github.com>
2020-06-15 23:16:30 -0400

    style: lineup

--- app/views/components/roster/roster.js
+++ app/views/components/roster/roster.js
@@ -19,9 +19,108 @@ class Roster extends React.Component {
       )
     }
 
+    const starters = []
+    for (const slot of getEligibleSlots({ pos: 'ALL', league })) {
+      const slotId = constants.slots[slot]
+      const playerId = roster[`s${slotId}`]
+      starters.push(
+        <PlayerSlot key={slot} {...{ slotId, playerId, slot, roster }} />
+      )
+    }
+
+    const ps = []
+    for (const slot of getEligibleSlots({ ps: true, league })) {
+      const slotId = constants.slots[slot]
+      const playerId = roster[`s${slotId}`]
+      ps.push(
+        <PlayerSlot key={slot} {...{ slotId, playerId, slot, roster }} />
+      )
+    }
+
+    const bench = []
+    for (const slot of getEligibleSlots({ bench: true, league })) {
+      const slotId = constants.slots[slot]
+      const playerId = roster[`s${slotId}`]
+      bench.push(
+        <PlayerSlot key={slot} {...{ slotId, playerId, slot, roster }} />
+      )
+    }
+
+    const ir = []
+    for (const slot of getEligibleSlots({ ir: true, league })) {
+      const slotId = constants.slots[slot]
+      const playerId = roster[`s${slotId}`]
+      ir.push(
+        <PlayerSlot key={slot} {...{ slotId, playerId, slot, roster }} />
+      )
+    }
+
     return (
       <div className='roster'>
-        {slots}
+        <div className='roster__starters'>
+          <div className='dashboard__section'>
+            <div className='dashboard__section-header'>
+              <div className='dashboard__section-body-header'>
+                <div className='player__slot-slotName'>Slot</div>
+                <div className='player__item-name'>Name</div>
+                <div className='player__item-metric'>Opp</div>
+                <div className='player__item-metric'>Avg</div>
+                <div className='player__item-metric'>Proj</div>
+                <div className='player__item-metric'>Sos</div>
+              </div>
+            </div>
+            <div className='dashboard__section-body'>
+              {starters}
+            </div>
+          </div>
+        </div>
+        <div className='roster__inactive'>
+          <div className='dashboard__section'>
+            <div className='dashboard__section-header'>
+              <div className='dashboard__section-body-header'>
+                <div className='player__slot-slotName'>Slot</div>
+                <div className='player__item-name'>Name</div>
+                <div className='player__item-metric'>Opp</div>
+                <div className='player__item-metric'>Avg</div>
+                <div className='player__item-metric'>Proj</div>
+                <div className='player__item-metric'>Sos</div>
+              </div>
+            </div>
+            <div className='dashboard__section-body'>
+              {bench}
+            </div>
+          </div>
+          <div className='dashboard__section'>
+            <div className='dashboard__section-header'>
+              <div className='dashboard__section-body-header'>
+                <div className='player__slot-slotName'>Slot</div>
+                <div className='player__item-name'>Name</div>
+                <div className='player__item-metric'>Opp</div>
+                <div className='player__item-metric'>Avg</div>
+                <div className='player__item-metric'>Proj</div>
+                <div className='player__item-metric'>Sos</div>
+              </div>
+            </div>
+            <div className='dashboard__section-body'>
+              {ir}
+            </div>
+          </div>
+          <div className='dashboard__section'>
+            <div className='dashboard__section-header'>
+              <div className='dashboard__section-body-header'>
+                <div className='player__slot-slotName'>Slot</div>
+                <div className='player__item-name'>Name</div>
+                <div className='player__item-metric'>Opp</div>
+                <div className='player__item-metric'>Avg</div>
+                <div className='player__item-metric'>Proj</div>
+                <div className='player__item-metric'>Sos</div>
+              </div>
+            </div>
+            <div className='dashboard__section-body'>
+              {ps}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
