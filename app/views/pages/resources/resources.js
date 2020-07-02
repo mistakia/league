91b2e8e72f72c78a5bbcc07eb3496b552b6cdac9
mistakia <1823355+mistakia@users.noreply.github.com>
2020-07-02 12:33:53 -0400

    feat: add resources page

---
+++ app/views/pages/resources/resources.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import marked from 'marked'
+
+import { DOCS_URL } from '@core/constants'
+import PageLayout from '@layouts/page'
+
+import './resources.styl'
+
+export default class ResourcesPage extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { html: null }
+
+  }
+  componentDidMount = () => {
+    fetch(`${DOCS_URL}/resources.md`)
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          return response
+        } else {
+          const html = (
+            <div className='resources'>Failed to Load</div>
+          )
+          this.setState({ html })
+          const error = new Error(response.statusText)
+          error.response = response
+          throw error
+        }
+      })
+      .then(res => res.json())
+      .then(json => {
+        const content = window.atob(json.content)
+        const renderer = new marked.Renderer()
+        const linkRenderer = renderer.link
+        renderer.link = (href, title, text) => {
+          const html = linkRenderer.call(renderer, href, title, text);
+          return html.replace(/^<a /, '<a target="_blank" rel="nofollow" ')
+        }
+        const markdown = marked(content, { renderer })
+        const html = (
+          <div className='resources' dangerouslySetInnerHTML={{__html:markdown}} />
+        )
+        this.setState({ html })
+      }).catch((error) => {
+        console.log(error)
+      })
+  }
+  render = () => {
+    return (
+      <PageLayout body={this.state.html} scroll />
+    )
+  }
+}
