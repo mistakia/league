fe602d47f6fc5192df21bfac09ceacdc66cc9a42
mistakia <1823355+mistakia@users.noreply.github.com>
2020-07-20 23:10:15 -0400

    docs: add README (closes #3)

--- config.test.js
+++ config.test.js
@@ -1,4 +1,22 @@
+const path = require('path')
+
 module.exports = {
+  mysql: {
+    client: 'mysql',
+    connection: {
+      host: 'localhost',
+      user: 'root',
+      database: 'league_test',
+      multipleStatements: true
+    },
+    migrations: {
+      directory: path.resolve(__dirname, './db/migrations'),
+      tableName: 'league_migrations'
+    },
+    seeds: {
+      directory: path.resolve(__dirname, './db/seeds/test')
+    }
+  },
   ssl: false,
   key: null,
   cert: null,
