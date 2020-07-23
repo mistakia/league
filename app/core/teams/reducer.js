cdf956fd0c8e686d26e09e15b0857c687b242a4f
mistakia <1823355+mistakia@users.noreply.github.com>
2020-07-23 15:01:01 -0400

    fix: update team picks store

--- app/core/teams/reducer.js
+++ app/core/teams/reducer.js
@@ -4,6 +4,7 @@ import { appActions } from '@core/app'
 import { createTeam } from './team'
 import { teamActions } from './actions'
 import { auctionActions } from '@core/auction'
+import { draftActions } from '@core/draft'
 
 const initialState = new Map()
 
@@ -36,6 +37,14 @@ export function teamsReducer (state = initialState, { payload, type }) {
       return state.setIn(
         [payload.opts.teamId, payload.opts.field], payload.data.value
       )
+
+    case draftActions.DRAFTED_PLAYER:
+    case draftActions.POST_DRAFT_FULFILLED: {
+      const { data } = payload
+      const teamPicks = state.getIn([data.tid, 'picks'])
+      const key = teamPicks.findKey(p => p.uid === data.uid)
+      return state.setIn([data.tid, 'picks', key, 'player'], data.player)
+    }
 
     default:
       return state
