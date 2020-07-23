37b07c81bfc494aa2d8c559ec2f11d9f2371a2c1
mistakia <1823355+mistakia@users.noreply.github.com>
2020-07-23 00:14:19 -0400

    feat: save source weights

--- api/routes/sources.js
+++ api/routes/sources.js
@@ -12,4 +12,36 @@ router.get('/?', async (req, res) => {
   }
 })
 
+router.put('/:sourceid', async (req, res) => {
+  const { db, logger } = req.app.locals
+  try {
+    const { weight } = req.body
+    const { sourceid } = req.params
+
+    if (!req.user || !req.user.userId) {
+      return res.status(401).send({ error: 'invalid userId' })
+    }
+
+    if (typeof weight === 'undefined') {
+      return res.status(400).send({ error: 'missing weight param' })
+    }
+
+    if (!sourceid) {
+      return res.status(400).send({ error: 'missing sourceid param' })
+    }
+
+    const rows = await db('users_sources').where({ userid: req.user.userId, sourceid })
+    if (rows.length) {
+      await db('users_sources').update({ weight }).where({ userid: req.user.userId, sourceid })
+    } else {
+      await db('users_sources').insert({ userid: req.user.userId, sourceid, weight })
+    }
+
+    res.send({ weight, sourceid })
+  } catch (error) {
+    logger(error)
+    res.status(500).send({ error: error.toString() })
+  }
+})
+
 module.exports = router
