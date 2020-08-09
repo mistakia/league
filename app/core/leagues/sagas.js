6f567b51560fa57f6b37d02b50c970ab71416004
mistakia <1823355+mistakia@users.noreply.github.com>
2020-08-08 20:43:35 -0400

    fix: turn off error reporter + league setting save notification

--- app/core/leagues/sagas.js
+++ app/core/leagues/sagas.js
@@ -3,11 +3,19 @@ import { call, takeLatest, fork, select, put } from 'redux-saga/effects'
 import { leagueActions } from './actions'
 import { putLeague } from '@core/api'
 import { getApp } from '@core/app'
+import { notificationActions } from '@core/notifications'
 
 export function * updateLeague ({ payload }) {
   const { token } = yield select(getApp)
   if (token) yield call(putLeague, payload)
   else yield put(leagueActions.set(payload))
+}
+
+export function * notification ({ payload }) {
+  yield put(notificationActions.show({
+    message: 'League setting saved',
+    severity: 'success'
+  }))
 }
 
 //= ====================================
@@ -18,10 +26,15 @@ export function * watchUpdateLeague () {
   yield takeLatest(leagueActions.UPDATE_LEAGUE, updateLeague)
 }
 
+export function * watchPutLeagueFulfilled () {
+  yield takeLatest(leagueActions.PUT_LEAGUE_FULFILLED, notification)
+}
+
 //= ====================================
 //  ROOT
 // -------------------------------------
 
 export const leagueSagas = [
-  fork(watchUpdateLeague)
+  fork(watchUpdateLeague),
+  fork(watchPutLeagueFulfilled)
 ]
