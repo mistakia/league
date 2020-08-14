59f19dc99b755a676384f7ec4d1255cf2cd8682d
mistakia <1823355+mistakia@users.noreply.github.com>
2020-08-13 21:49:40 -0400

    chore: add test waiver free agenyc

---
+++ utils/is-player-rostered.js
@@ -0,0 +1,15 @@
+const db = require('../db')
+const { constants } = require('../common')
+
+module.exports = async ({ player, leagueId }) => {
+  const rosterRows = await db('rosters_players')
+    .join('rosters', 'rosters_players.rid', 'rosters.uid')
+    .where({
+      player,
+      lid: leagueId,
+      week: constants.season.week,
+      year: constants.season.year
+    })
+
+  return !!rosterRows.length
+}
