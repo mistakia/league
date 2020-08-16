34bde0270d08f28069132134e8832d34d4003123
mistakia <1823355+mistakia@users.noreply.github.com>
2020-08-16 19:45:29 -0400

    feat: use available & rostered player in auction optimizer

--- app/core/auction/worker.js
+++ app/core/auction/worker.js
@@ -1,14 +1,28 @@
 import solver from 'javascript-lp-solver'
-import { constants } from '@common'
+import { constants, getOptimizerPositionConstraints } from '@common'
 
-export function optimizeLineup ({ constraints, players, vbaseline }) {
+export function optimizeLineup ({
+  limits = {},
+  players,
+  vbaseline,
+  league,
+  active = []
+}) {
   const variables = {}
   const ints = {}
 
-  for (const player of players) {
+  const pool = players.concat(active)
+  const positions = pool.map(p => p.pos1)
+  const positionConstraints = getOptimizerPositionConstraints({ positions, league })
+  const constraints = {
+    value: { max: Math.round(league.cap * 0.8) },
+    ...positionConstraints,
+    ...limits
+  }
+
+  const addPlayer = ({ player, freeAgent }) => {
     variables[player.player] = {
       points: Math.round(player.points.ros.total || 0),
-      value: Math.round(player.values.ros[vbaseline] || 0),
       starter: 1
     }
     variables[player.player][player.player] = 1
@@ -22,7 +36,15 @@ export function optimizeLineup ({ constraints, players, vbaseline }) {
     for (const pos of constants.positions) {
       variables[player.player][pos] = player.pos1 === pos ? 1 : 0
     }
+
+    if (freeAgent) {
+      variables[player.player].fa = 1
+      variables[player.player].value = Math.round(player.values.ros[vbaseline] || 0)
+    }
   }
+
+  active.forEach(player => addPlayer({ player, freeAgent: false }))
+  players.forEach(player => addPlayer({ player, freeAgent: true }))
 
   const model = {
     optimize: 'points',
