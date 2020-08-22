489d45404b4d1e5e3a9578bd10cf2c1415150c39
mistakia <1823355+mistakia@users.noreply.github.com>
2020-08-21 20:34:21 -0400

    feat: export projections to csv

---
+++ app/core/export/csv.js
@@ -0,0 +1,43 @@
+/* global Blob */
+
+function convertToCSV (objArray) {
+  const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray
+  let str = ''
+
+  for (var i = 0; i < array.length; i++) {
+    let line = ''
+    for (const index in array[i]) {
+      if (line !== '') line += ','
+      line += array[i][index]
+    }
+    str += line + '\r\n'
+  }
+  return str
+}
+
+export function csv ({ headers, data, fileName = 'teflon-export.csv' }) {
+  if (headers) {
+    data.unshift(headers)
+  }
+
+  // Convert Object to JSON
+  const jsonObject = JSON.stringify(data)
+  const csv = convertToCSV(jsonObject)
+  const exportedFilenmae = fileName + '.csv'
+  const blob = new Blob([csv], { type: 'text/csvcharset=utf-8' })
+  if (navigator.msSaveBlob) { // IE 10+
+    navigator.msSaveBlob(blob, exportedFilenmae)
+  } else {
+    const link = document.createElement('a')
+    if (link.download !== undefined) { // feature detection
+      // Browsers that support HTML5 download attribute
+      const url = URL.createObjectURL(blob)
+      link.setAttribute('href', url)
+      link.setAttribute('download', exportedFilenmae)
+      link.style.visibility = 'hidden'
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+    }
+  }
+}
