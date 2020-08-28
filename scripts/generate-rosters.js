18157425e7a022ff67a7e25df51c49880e7cce31
mistakia <1823355+mistakia@users.noreply.github.com>
2020-08-28 19:01:45 -0400

    chore: test generate roster script

--- scripts/generate-rosters.js
+++ scripts/generate-rosters.js
@@ -8,40 +8,73 @@ const run = async () => {
   // get list of hosted leagues
   const leagues = await db('leagues').where('hosted', 1)
 
+  const isCurrentYear = constants.season.week < constants.season.finalWeek
+  const week = isCurrentYear ? (constants.season.week + 1) : 0
+  const year = isCurrentYear ? constants.season.year : (constants.season.year + 1)
+
   for (const league of leagues) {
-    // get current week rosters for league
-    const rosters = await db('rosters').where({ lid: league.uid, week: constants.season.week })
+    // get latest rosters for league
+    const rosters = await db('rosters')
+      .where({
+        lid: league.uid,
+        year: constants.season.year,
+        week: constants.season.week
+      })
+
     for (const roster of rosters) {
       // get current roster players
       const { tid, lid, uid } = roster
       const players = await db('rosters_players').where({ rid: uid })
-      const newRoster = await db('rosters').insert({
+      const currentPlayerIds = players.map(p => p.player)
+
+      // get roster id
+      const rosterData = {
         tid,
         lid,
-        week: constants.season.week + 1,
-        year: constants.season.year
-      })
-      const newId = newRoster[0]
+        week,
+        year
+      }
+      const rosterRows = await db('rosters').where(rosterData)
+      let rid = rosterRows.length ? rosterRows[0].uid : null
+      if (!rid) {
+        const newRoster = await db('rosters').insert(rosterData)
+        rid = newRoster[0]
+      }
 
-      const inserts = players.map(p => ({
-        rid: newId,
+      // insert any missing players & remove excess players
+      const existingPlayers = await db('rosters_players').where({ rid })
+      const existingPlayerIds = existingPlayers.map(p => p.player)
+      const missing = players.filter(p => !existingPlayerIds.includes(p.player))
+      const excessive = existingPlayers.filter(p => !currentPlayerIds.includes(p.player))
+      const inserts = missing.map(p => ({
+        rid,
         slot: p.slot,
         player: p.player,
         pos: p.pos
       }))
 
       await db('rosters_players').insert(inserts)
+      if (excessive.length) {
+        await db('rosters_players')
+          .where('rid', rid)
+          .whereIn('player', excessive.map(p => p.player))
+      }
     }
   }
 }
 
+module.exports = run
+
 const main = async () => {
-  await run()
+  try {
+    await run()
+  } catch (error) {
+    console.log(error)
+  }
+
   process.exit()
 }
 
-try {
+if (!module.parent) {
   main()
-} catch (error) {
-  console.log(error)
 }
