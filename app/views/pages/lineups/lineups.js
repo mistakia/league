5071ec0d87d5fcdf0ec1d254aaf70eb422ea71ee
mistakia <1823355+mistakia@users.noreply.github.com>
2020-08-31 19:28:06 -0400

    feat: mobile friendly lineup page

--- app/views/pages/lineups/lineups.js
+++ app/views/pages/lineups/lineups.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Container from '@material-ui/core/Container'
 
 import Lineup from '@components/lineup'
 import PageLayout from '@layouts/page'
@@ -6,9 +7,9 @@ import PageLayout from '@layouts/page'
 export default function () {
   const { roster } = this.props
   const body = (
-    <div>
+    <Container maxWidth='lg'>
       <Lineup roster={roster} />
-    </div>
+    </Container>
   )
 
   return (
