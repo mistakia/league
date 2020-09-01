e09d2a4d9af32f68f20c7cdf65f37b9249034a7f
mistakia <1823355+mistakia@users.noreply.github.com>
2020-09-01 11:14:53 -0400

    chore: add additional trade test

--- test/trade.spec.js
+++ test/trade.spec.js
@@ -1,4 +1,4 @@
-/* global describe, before it */
+/* global describe before it beforeEach*/
 process.env.NODE_ENV = 'test'
 
 const chai = require('chai')
@@ -9,12 +9,20 @@ const knex = require('../db')
 const league = require('../db/seeds/league')
 const draft = require('../db/seeds/draft')
 const { constants } = require('../common')
+const { user1, user2 } = require('./fixtures/token')
+const {
+  addPlayer,
+  selectPlayer,
+  checkLastTransaction,
+  notLoggedIn,
+  missing,
+  invalid,
+  error
+} = require('./utils')
 
 const should = chai.should()
-
-const { user1, user2 } = require('./fixtures/token')
-
 chai.use(chaiHTTP)
+const expect = chai.expect
 
 describe('API /trades', function () {
   before(async function () {
@@ -23,84 +31,222 @@ describe('API /trades', function () {
     await knex.migrate.rollback()
     await knex.migrate.latest()
     await knex.seed.run()
-
-    await league(knex)
-    await draft(knex)
   })
 
-  it('one-for-one player trade', async () => {
-    const proposingTeamPlayerRows = await knex('rosters_players')
-      .leftJoin('rosters', 'rosters_players.rid', 'rosters.uid')
-      .where('rosters.tid', 1)
-      .limit(1)
+  describe('post', function () {
+    beforeEach(async function () {
+      this.timeout(60 * 1000)
+      await league(knex)
+    })
 
-    const acceptingTeamPlayerRows = await knex('rosters_players')
-      .leftJoin('rosters', 'rosters_players.rid', 'rosters.uid')
-      .where('rosters.tid', 2)
-      .limit(1)
+    it('one-for-one player trade', async () => {
+      await draft(knex)
 
-    // TODO - get trading player values
+      const proposingTeamPlayerRows = await knex('rosters_players')
+        .leftJoin('rosters', 'rosters_players.rid', 'rosters.uid')
+        .where('rosters.tid', 1)
+        .limit(1)
 
-    const proposingTeamPlayers = proposingTeamPlayerRows.map(p => p.player)
-    const acceptingTeamPlayers = acceptingTeamPlayerRows.map(p => p.player)
-    const proposeRes = await chai.request(server)
-      .post('/api/leagues/1/trades')
-      .set('Authorization', `Bearer ${user1}`)
-      .send({
-        proposingTeamPlayers,
-        acceptingTeamPlayers,
-        pid: 1,
-        tid: 2,
-        leagueId: 1
+      const acceptingTeamPlayerRows = await knex('rosters_players')
+        .leftJoin('rosters', 'rosters_players.rid', 'rosters.uid')
+        .where('rosters.tid', 2)
+        .limit(1)
+
+      // TODO - get trading player values
+
+      const proposingTeamPlayers = proposingTeamPlayerRows.map(p => p.player)
+      const acceptingTeamPlayers = acceptingTeamPlayerRows.map(p => p.player)
+      const proposeRes = await chai.request(server)
+        .post('/api/leagues/1/trades')
+        .set('Authorization', `Bearer ${user1}`)
+        .send({
+          proposingTeamPlayers,
+          acceptingTeamPlayers,
+          pid: 1,
+          tid: 2,
+          leagueId: 1
+        })
+
+      proposeRes.should.have.status(200)
+      // eslint-disable-next-line
+      proposeRes.should.be.json
+      proposeRes.body.pid.should.be.equal(1)
+      proposeRes.body.tid.should.be.equal(2)
+      proposeRes.body.userid.should.be.equal(1)
+      proposeRes.body.year.should.be.equal(constants.season.year)
+      should.exist(proposeRes.body.offered)
+      should.not.exist(proposeRes.body.cancelled)
+      should.not.exist(proposeRes.body.accepted)
+      should.not.exist(proposeRes.body.rejected)
+      should.not.exist(proposeRes.body.vetoed)
+      proposeRes.body.proposingTeamPlayers.should.be.eql(proposingTeamPlayers)
+      proposeRes.body.acceptingTeamPlayers.should.be.eql(acceptingTeamPlayers)
+
+      const tradeid = proposeRes.body.uid
+
+      const acceptRes = await chai.request(server)
+        .post(`/api/leagues/1/trades/${tradeid}/accept`)
+        .set('Authorization', `Bearer ${user2}`)
+
+      acceptRes.should.have.status(200)
+      // eslint-disable-next-line
+      acceptRes.should.be.json
+      acceptRes.body.pid.should.be.equal(1)
+      acceptRes.body.tid.should.be.equal(2)
+      acceptRes.body.userid.should.be.equal(1)
+      acceptRes.body.year.should.be.equal(constants.season.year)
+      should.exist(acceptRes.body.offered)
+      should.not.exist(acceptRes.body.cancelled)
+      should.exist(acceptRes.body.accepted)
+      should.not.exist(acceptRes.body.rejected)
+      should.not.exist(acceptRes.body.vetoed)
+      acceptRes.body.proposingTeamPlayers.should.be.eql(proposingTeamPlayers)
+      acceptRes.body.acceptingTeamPlayers.should.be.eql(acceptingTeamPlayers)
+
+      const rows = await knex('rosters_players')
+        .leftJoin('rosters', 'rosters_players.rid', 'rosters.uid')
+        .whereIn('rosters_players.player', proposingTeamPlayers.concat(acceptingTeamPlayers))
+
+      rows.length.should.equal(2)
+      const proposingRow = rows.find(p => p.tid === 1)
+      const acceptingRow = rows.find(p => p.tid === 2)
+      proposingRow.player.should.equal(acceptingTeamPlayers[0])
+      acceptingRow.player.should.equal(proposingTeamPlayers[0])
+
+      // TODO - check player values pre/post trade
+    })
+
+    it('one for one trade of rookies: one active, one on practice squad, with subsequent deactivations', async () => {
+      const player1 = await selectPlayer({ rookie: true })
+      const teamId = 1
+      const leagueId = 1
+      const userId = 1
+      const value = 3
+      await addPlayer({
+        teamId,
+        leagueId,
+        userId,
+        player: player1,
+        slot: constants.slots.PS,
+        transaction: constants.transactions.DRAFT
       })
 
-    proposeRes.should.have.status(200)
-    // eslint-disable-next-line
-    proposeRes.should.be.json
-    proposeRes.body.pid.should.be.equal(1)
-    proposeRes.body.tid.should.be.equal(2)
-    proposeRes.body.userid.should.be.equal(1)
-    proposeRes.body.year.should.be.equal(constants.season.year)
-    should.exist(proposeRes.body.offered)
-    should.not.exist(proposeRes.body.cancelled)
-    should.not.exist(proposeRes.body.accepted)
-    should.not.exist(proposeRes.body.rejected)
-    should.not.exist(proposeRes.body.vetoed)
-    proposeRes.body.proposingTeamPlayers.should.be.eql(proposingTeamPlayers)
-    proposeRes.body.acceptingTeamPlayers.should.be.eql(acceptingTeamPlayers)
+      const player2 = await selectPlayer({ rookie: true })
+      await addPlayer({
+        teamId: 2,
+        leagueId,
+        userId: 2,
+        player: player2,
+        slot: constants.slots.PS,
+        transaction: constants.transactions.DRAFT,
+        value
+      })
 
-    const tradeid = proposeRes.body.uid
+      const proposingTeamPlayers = [player1.player]
+      const acceptingTeamPlayers = [player2.player]
+      const proposeRes = await chai.request(server)
+        .post('/api/leagues/1/trades')
+        .set('Authorization', `Bearer ${user1}`)
+        .send({
+          proposingTeamPlayers,
+          acceptingTeamPlayers,
+          pid: 1,
+          tid: 2,
+          leagueId: 1
+        })
 
-    const acceptRes = await chai.request(server)
-      .post(`/api/leagues/1/trades/${tradeid}/accept`)
-      .set('Authorization', `Bearer ${user2}`)
+      proposeRes.should.have.status(200)
+      // eslint-disable-next-line
+      proposeRes.should.be.json
+      proposeRes.body.pid.should.be.equal(1)
+      proposeRes.body.tid.should.be.equal(2)
+      proposeRes.body.userid.should.be.equal(1)
+      proposeRes.body.year.should.be.equal(constants.season.year)
+      should.exist(proposeRes.body.offered)
+      should.not.exist(proposeRes.body.cancelled)
+      should.not.exist(proposeRes.body.accepted)
+      should.not.exist(proposeRes.body.rejected)
+      should.not.exist(proposeRes.body.vetoed)
+      proposeRes.body.proposingTeamPlayers.should.be.eql(proposingTeamPlayers)
+      proposeRes.body.acceptingTeamPlayers.should.be.eql(acceptingTeamPlayers)
 
-    acceptRes.should.have.status(200)
-    // eslint-disable-next-line
-    acceptRes.should.be.json
-    acceptRes.body.pid.should.be.equal(1)
-    acceptRes.body.tid.should.be.equal(2)
-    acceptRes.body.userid.should.be.equal(1)
-    acceptRes.body.year.should.be.equal(constants.season.year)
-    should.exist(acceptRes.body.offered)
-    should.not.exist(acceptRes.body.cancelled)
-    should.exist(acceptRes.body.accepted)
-    should.not.exist(acceptRes.body.rejected)
-    should.not.exist(acceptRes.body.vetoed)
-    acceptRes.body.proposingTeamPlayers.should.be.eql(proposingTeamPlayers)
-    acceptRes.body.acceptingTeamPlayers.should.be.eql(acceptingTeamPlayers)
+      const tradeid = proposeRes.body.uid
 
-    const rows = await knex('rosters_players')
-      .leftJoin('rosters', 'rosters_players.rid', 'rosters.uid')
-      .whereIn('rosters_players.player', proposingTeamPlayers.concat(acceptingTeamPlayers))
+      const acceptRes = await chai.request(server)
+        .post(`/api/leagues/1/trades/${tradeid}/accept`)
+        .set('Authorization', `Bearer ${user2}`)
 
-    rows.length.should.equal(2)
-    const proposingRow = rows.find(p => p.tid === 1)
-    const acceptingRow = rows.find(p => p.tid === 2)
-    proposingRow.player.should.equal(acceptingTeamPlayers[0])
-    acceptingRow.player.should.equal(proposingTeamPlayers[0])
+      acceptRes.should.have.status(200)
+      // eslint-disable-next-line
+      acceptRes.should.be.json
+      acceptRes.body.pid.should.be.equal(1)
+      acceptRes.body.tid.should.be.equal(2)
+      acceptRes.body.userid.should.be.equal(1)
+      acceptRes.body.year.should.be.equal(constants.season.year)
+      should.exist(acceptRes.body.offered)
+      should.not.exist(acceptRes.body.cancelled)
+      should.exist(acceptRes.body.accepted)
+      should.not.exist(acceptRes.body.rejected)
+      should.not.exist(acceptRes.body.vetoed)
+      acceptRes.body.proposingTeamPlayers.should.be.eql(proposingTeamPlayers)
+      acceptRes.body.acceptingTeamPlayers.should.be.eql(acceptingTeamPlayers)
 
-    // TODO - check player values pre/post trade
+      const rows = await knex('rosters_players')
+        .leftJoin('rosters', 'rosters_players.rid', 'rosters.uid')
+        .whereIn('rosters_players.player', proposingTeamPlayers.concat(acceptingTeamPlayers))
+
+      rows.length.should.equal(2)
+      const proposingRow = rows.find(p => p.tid === 1)
+      const acceptingRow = rows.find(p => p.tid === 2)
+      proposingRow.player.should.equal(acceptingTeamPlayers[0])
+      acceptingRow.player.should.equal(proposingTeamPlayers[0])
+
+      const res = await chai.request(server)
+        .post('/api/teams/1/deactivate')
+        .set('Authorization', `Bearer ${user1}`)
+        .send({
+          player: player2.player,
+          leagueId
+        })
+
+      res.should.have.status(200)
+      // eslint-disable-next-line
+      res.should.be.json
+
+      res.body.tid.should.equal(teamId)
+      res.body.player.should.equal(player2.player)
+      res.body.slot.should.equal(constants.slots.PS)
+      res.body.transaction.userid.should.equal(userId)
+      res.body.transaction.tid.should.equal(teamId)
+      res.body.transaction.lid.should.equal(leagueId)
+      res.body.transaction.player.should.equal(player2.player)
+      res.body.transaction.type.should.equal(constants.transactions.ROSTER_DEACTIVATE)
+      res.body.transaction.value.should.equal(value)
+      res.body.transaction.year.should.equal(constants.season.year)
+      res.body.transaction.timestamp.should.equal(Math.round(Date.now() / 1000))
+
+      const rosterRows = await knex('rosters_players')
+        .join('rosters', 'rosters_players.rid', 'rosters.uid')
+        .where({
+          year: constants.season.year,
+          week: constants.season.week,
+          player: player2.player
+        })
+        .limit(1)
+
+      const rosterRow = rosterRows[0]
+      expect(rosterRow.slot).to.equal(constants.slots.PS)
+
+      await checkLastTransaction({
+        leagueId,
+        type: constants.transactions.ROSTER_DEACTIVATE,
+        value,
+        year: constants.season.year,
+        player: player2.player,
+        teamId,
+        userId
+      })
+    })
   })
   // one for one trade
 
@@ -120,7 +266,61 @@ describe('API /trades', function () {
   // reject trade
 
   describe('errors', function () {
+    beforeEach(async function () {
+      this.timeout(60 * 1000)
+      await league(knex)
+    })
 
+    it('trade player with existing poaching claim', async () => {
+      const player1 = await selectPlayer({ rookie: true })
+      const teamId = 1
+      const leagueId = 1
+      const userId = 1
+      const value = 3
+      await addPlayer({
+        teamId,
+        leagueId,
+        userId,
+        player: player1,
+        slot: constants.slots.PS,
+        transaction: constants.transactions.DRAFT
+      })
+
+      await knex('poaches').insert({
+        userid: 3,
+        tid: 3,
+        lid: 1,
+        player: player1.player,
+        drop: null,
+        submitted: Math.round(Date.now() / 1000)
+      })
+
+      const player2 = await selectPlayer({ rookie: true })
+      await addPlayer({
+        teamId: 2,
+        leagueId,
+        userId: 2,
+        player: player2,
+        slot: constants.slots.PS,
+        transaction: constants.transactions.DRAFT
+      })
+
+      const proposingTeamPlayers = [player1.player]
+      const acceptingTeamPlayers = [player2.player]
+      const request = chai.request(server)
+        .post('/api/leagues/1/trades')
+        .set('Authorization', `Bearer ${user1}`)
+        .send({
+          proposingTeamPlayers,
+          acceptingTeamPlayers,
+          pid: 1,
+          tid: 2,
+          leagueId: 1
+        })
+
+
+      await error(request, 'player has poaching claim')
+    })
   })
   // errors
 
