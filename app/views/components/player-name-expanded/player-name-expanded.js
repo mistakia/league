1d26ee66d06ac9df125dd12401f38c0ce648f8ca
mistakia <1823355+mistakia@users.noreply.github.com>
2020-09-02 12:51:34 -0400

    style: mobile friendly dashboard

--- app/views/components/player-name-expanded/player-name-expanded.js
+++ app/views/components/player-name-expanded/player-name-expanded.js
@@ -1,42 +1,48 @@
 import React from 'react'
 
+import { Player, connect } from '@components/player'
 import Position from '@components/position'
 import Tooltip from '@material-ui/core/Tooltip'
 import Team from '@components/team'
 import { constants } from '@common'
+import IconButton from '@components/icon-button'
 
 import './player-name-expanded.styl'
 
-export default class PlayerNameExpanded extends React.Component {
-  handleClick = () => {
-    this.props.select(this.props.player.player)
-  }
-
+class PlayerNameExpanded extends Player {
   render = () => {
-    const { player } = this.props
+    const { player, isHosted } = this.props
     return (
       <div className='player__name-expanded'>
-        <div
-          className='player__name-expanded-row player__name-expanded-name cursor'
-          onClick={this.handleClick}
-        >
-          <span>{player.fname} {player.lname}</span>
-          {(constants.season.year === player.draft_year) &&
-            <sup className='player__label-rookie'>
-              R
-            </sup>}
+        <div className='player__name-expanded-main'>
+          <div
+            className='player__name-expanded-row player__name-expanded-name cursor'
+            onClick={this.handleClick}
+          >
+            <span>{player.fname} {player.lname}</span>
+            {(constants.season.year === player.draft_year) &&
+              <sup className='player__label-rookie'>
+                R
+              </sup>}
+          </div>
+          <div className='player__name-expanded-row'>
+            <Position pos={player.pos1} />
+            <Team team={player.team} />
+            {!!player.status &&
+              <Tooltip title={player.status} placement='bottom'>
+                <span className='player__label-status'>
+                  {constants.status[player.status]}
+                </span>
+              </Tooltip>}
+          </div>
         </div>
-        <div className='player__name-expanded-row'>
-          <Position pos={player.pos1} />
-          <Team team={player.team} />
-          {!!player.status &&
-            <Tooltip title={player.status} placement='bottom'>
-              <span className='player__label-status'>
-                {constants.status[player.status]}
-              </span>
-            </Tooltip>}
-        </div>
+        {isHosted &&
+          <div className='player__name-expanded-action'>
+            <IconButton small text onClick={this.handleContextClick} icon='more' />
+          </div>}
       </div>
     )
   }
 }
+
+export default connect(PlayerNameExpanded)
--- app/views/components/player-roster/player-roster.js
+++ app/views/components/player-roster/player-roster.js
@@ -2,58 +2,54 @@ import React from 'react'
 
 import PlayerNameExpanded from '@components/player-name-expanded'
 import { Player, connect } from '@components/player'
-import IconButton from '@components/icon-button'
 import Icon from '@components/icon'
 import { sortableHandle } from 'react-sortable-hoc'
 
 const DragHandle = sortableHandle(() =>
-  <div className='player__item-action reorder'>
+  <div className='player__item-action reorder table__cell'>
     <Icon name='reorder' />
   </div>
 )
 
 class PlayerRoster extends Player {
   render () {
-    const { player, vbaseline, selected, claim, reorder, isHosted } = this.props
+    const { player, vbaseline, selected, claim, reorder, waiverId } = this.props
 
     const isClaim = !!claim
 
-    const classNames = ['player__item']
+    const classNames = ['player__item', 'table__row']
     if (selected === player.player) classNames.push('selected')
 
     return (
       <div className={classNames.join(' ')}>
-        <div className='player__item-name'>
-          <PlayerNameExpanded playerId={player.player} />
+        {reorder && <DragHandle />}
+        <div className='player__item-name table__cell sticky__column'>
+          <PlayerNameExpanded playerId={player.player} waiverId={waiverId} />
         </div>
         {!isClaim &&
-          <div className='player__item-metric'>
+          <div className='metric table__cell'>
             ${player.value}
           </div>}
         {isClaim &&
-          <div className='player__item-name'>
+          <div className='player__item-name table__cell'>
             {claim.drop && <PlayerNameExpanded playerId={claim.drop} />}
           </div>}
         {isClaim &&
-          <div className='player__item-metric'>
+          <div className='metric table__cell'>
             {claim.bid && `$${claim.bid}`}
           </div>}
-        <div className='player__item-metric'>
+        <div className='metric table__cell'>
           {(player.getIn(['vorp', '0', vbaseline]) || 0).toFixed(1)}
         </div>
-        <div className='player__item-metric'>
+        <div className='metric table__cell'>
           {player.getIn(['lineups', 'starts'])}
         </div>
-        <div className='player__item-metric'>
+        <div className='metric table__cell'>
           {player.getIn(['lineups', 'sp'], 0).toFixed(1) || '--'}
         </div>
-        <div className='player__item-metric'>
+        <div className='metric table__cell'>
           {player.get(['lineups', 'bp'], 0).toFixed(1) || '--'}
         </div>
-        <div className='player__item-action'>
-          {!!isHosted && <IconButton small text onClick={this.handleContextClick} icon='more' />}
-        </div>
-        {reorder && <DragHandle />}
       </div>
     )
   }
