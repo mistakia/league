d96f03a6ffc4aac797814da9f032f19469c5f528
mistakia <1823355+mistakia@users.noreply.github.com>
2020-09-04 14:43:19 -0400

    feat: show league positional value

---
+++ app/views/components/dashboard-league-positional-value/dashboard-league-positional-value.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
+import { constants } from '@common'
+
+export default class DashboardLeaguePositionalValue extends React.Component {
+  render = () => {
+    const { summary, teams } = this.props
+
+    const series = []
+    for (const position of constants.positions) {
+      const data = []
+      for (const tid in summary.rosters) {
+        const rounded = parseInt(summary.rosters[tid][position].toFixed(1), 0)
+        data.push(rounded)
+      }
+      series.push({ name: position, data })
+    }
+
+    const teamNames = []
+    for (const tid in summary.rosters) {
+      const id = parseInt(tid, 10)
+      const name = teams.getIn([id, 'name'])
+      teamNames.push(name)
+    }
+
+    const options = {
+      chart: {
+        type: 'bar',
+        backgroundColor: 'transparent'
+      },
+      title: {
+        text: ''
+      },
+      xAxis: {
+        categories: teamNames
+      },
+      colors: ['#ff3f3f', '#3f9f3f', '#ffbb3f', '#3f3fff', '#9f9f9f', '#3f3f3f'],
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'Value'
+        }
+      },
+      legend: {
+        reversed: true
+      },
+      plotOptions: {
+        series: {
+          stacking: 'normal'
+        }
+      },
+      credits: {
+        enabled: false
+      },
+      series
+    }
+
+    return (
+      <div className='dashboard__section-side'>
+        <div className='dashboard__section-side-body'>
+          <HighchartsReact
+            highcharts={Highcharts}
+            options={options}
+          />
+        </div>
+      </div>
+    )
+  }
+}
---
+++ app/views/components/dashboard-league-positional-value/index.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux'
+import { createSelector } from 'reselect'
+
+import { getCurrentTeamRosterPositionalValue } from '@core/rosters'
+import { getCurrentLeague } from '@core/leagues'
+import { getTeamsForCurrentLeague } from '@core/teams'
+
+import DashboardLeaguePositionalValue from './dashboard-league-positional-value'
+
+const mapStateToProps = createSelector(
+  getCurrentTeamRosterPositionalValue,
+  getTeamsForCurrentLeague,
+  getCurrentLeague,
+  (summary, teams, league) => ({ summary, teams, league })
+)
+
+export default connect(
+  mapStateToProps
+)(DashboardLeaguePositionalValue)
