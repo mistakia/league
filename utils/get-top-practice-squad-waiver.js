c0c7770691a371744cd225bb1a1cd4b47a5e5ca2
mistakia <1823355+mistakia@users.noreply.github.com>
2020-09-08 15:08:25 -0400

    chore: fix practice waivers during in-season waiver & exclude locked players from waivers

--- utils/get-top-practice-squad-waiver.js
+++ utils/get-top-practice-squad-waiver.js
@@ -1,4 +1,4 @@
-const moment = require('moment')
+const moment = require('moment-timezone')
 
 const db = require('../db')
 const { constants } = require('../common')
@@ -8,6 +8,10 @@ module.exports = async (leagueId) => {
   const league = await getLeague(leagueId)
   const days = (league.nteams * 3) + 1 // total picks + waiver day
   if (!league.ddate || moment().isBefore(moment(league.ddate, 'X').add(days, 'day'))) {
+    return undefined
+  }
+
+  if (constants.season.isRegularSeason && constants.season.isWaiverPeriod) {
     return undefined
   }
 
@@ -36,11 +40,30 @@ module.exports = async (leagueId) => {
       order: 'asc'
     }])
 
+  if (constants.season.isRegularSeason) {
+    query
+      .select('schedule.date')
+      .join('player', 'waivers.player', 'player.player')
+      .joinRaw('left join schedule on player.cteam = schedule.v or player.cteam = schedule.h')
+      .where('schedule.wk', constants.season.week)
+      .where('schedule.seas', constants.season.year)
+  }
+
   if (playerIds.length) {
     query.whereNotIn('waivers.player', playerIds)
   }
 
   const waivers = await query
+
+  if (constants.season.isRegularSeason) {
+    const now = moment()
+    const filtered = waivers.filter(player => {
+      const gameStart = moment.tz(player.date, 'M/D/YYYY H:m', 'America/New_York')
+      return now.isBefore(gameStart)
+    })
+
+    return filtered.length ? filtered[0] : undefined
+  }
 
   return waivers.length ? waivers[0] : undefined
 }
