5ed5d393ecb64e0fa193479df69557c55d8ead1e
mistakia <1823355+mistakia@users.noreply.github.com>
2020-09-10 12:47:01 -0400

    feat: show players game on player expanded component

--- app/core/schedule/index.js
+++ app/core/schedule/index.js
@@ -1,4 +1,9 @@
-export { getSchedule, getByeByTeam, getGameForWeekByTeam } from './selectors'
+export {
+  getSchedule,
+  getByeByTeam,
+  getGameForWeekByTeam,
+  getGameForWeekByPlayerId
+} from './selectors'
 export { scheduleSagas } from './sagas'
 export { scheduleReducer } from './reducer'
 export { scheduleActions, getScheduleActions } from './actions'
--- app/views/components/player-name-expanded/index.js
+++ app/views/components/player-name-expanded/index.js
@@ -2,12 +2,14 @@ import { connect } from 'react-redux'
 import { createSelector } from 'reselect'
 
 import { getPlayerById, playerActions } from '@core/players'
+import { getGameForWeekByPlayerId } from '@core/schedule'
 
 import PlayerNameExpanded from './player-name-expanded'
 
 const mapStateToProps = createSelector(
   getPlayerById,
-  (player) => ({ player })
+  getGameForWeekByPlayerId,
+  (player, game) => ({ player, game })
 )
 
 const mapDispatchToProps = {
