7bd2954e19ef48c4944c08d23c7ba8227cbb8064
mistakia <1823355+mistakia@users.noreply.github.com>
2020-09-19 11:27:01 -0400

    feat: show scoreboard player game yardline

---
+++ app/views/components/scoreboard-player-game-status/scoreboard-player-game-status.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './scoreboard-player-game-status.styl'
+
+export default class ScoreboardPlayerGameStatus extends React.Component {
+  render = () => {
+    const { status } = this.props
+    const classNames = ['scoreboard__player-game-status']
+    if (status && status.hasPossession) classNames.push('possession')
+    if (status && status.isRedzone) classNames.push('redzone')
+    return (
+      <div className={classNames.join(' ')}>
+        <div className='scoreboard__player-game-status-yardline-container'>
+          <div
+            className='scoreboard__player-game-status-yardline'
+            style={{
+              width: status ? `${status.yardline}%` : '0%'
+            }}
+          />
+        </div>
+      </div>
+    )
+  }
+}
