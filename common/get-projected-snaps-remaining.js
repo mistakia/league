0f8350316dd315d312495081893a3676eb573ee6
mistakia <1823355+mistakia@users.noreply.github.com>
2020-09-22 09:05:35 -0400

    feat: calculate projected snaps remaining (wip)

---
+++ common/get-projected-snaps-remaining.js
@@ -0,0 +1,13 @@
+const getProjectedSnapsRemaining = ({
+  quarter,
+  clockTime,
+  down,
+  yardsToGo
+}) => {
+  // count plays
+  // calculate time elapsed
+  // calculate play rate
+  // projected remaining plays (remaining time / play rate)
+}
+
+export default getProjectedSnapsRemaining
