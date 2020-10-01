c2ae98f81805dac2742edff0896a2d1f4466154d
mistakia <1823355+mistakia@users.noreply.github.com>
2020-10-01 17:00:43 -0400

    feat: show opponent on players view

---
+++ app/views/components/player-row-opponent/player-row-opponent.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './player-row-opponent.styl'
+
+export default class PlayerRowOpponent extends React.Component {
+  render = () => {
+    const { game, team } = this.props
+
+    if (!game) {
+      return <div className='player__row-opponent' />
+    }
+
+    const isHome = game.h === team
+    const opp = isHome ? game.v : game.h
+
+    return (
+      <div className='player__row-opponent'>
+        {(isHome ? 'v' : '@') + opp}
+      </div>
+    )
+  }
+}
