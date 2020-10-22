a7a19a2d946f0988ecab3ec6a0663a523f283ce9
mistakia <1823355+mistakia@users.noreply.github.com>
2020-10-22 10:13:12 -0400

    chore: complete database backup

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -2,6 +2,15 @@
 set -e
 set -x
 
+FULL=false
+
+while getopts 'f' opt; do
+    case $opt in
+        f) FULL=true ;;
+        *) echo 'Error in command line parsing' >&2
+    esac
+done
+
 DUMP_DIR="/root/backups"
 DB_NAME="league_production"
 DB_FILE="/root/.mysql/mysqldump.cnf"
@@ -9,15 +18,24 @@ DB_TABLES="draft leagues matchups poaches rosters rosters_players teams trades t
 DATE_FORMAT="%Y-%m-%d_%H-%M"
 
 file_name="$(date +$DATE_FORMAT)"
-sql_file="$file_name.sql"
-gz_file="$file_name.tar.gz"
+if $FULL; then
+    backup_type="full"
+else
+    backup_type="user"
+fi
+sql_file="$file_name-$backup_type.sql"
+gz_file="$file_name-$backup_type.tar.gz"
 
 # make sure that the folder exists
 mkdir -p $DUMP_DIR
 cd $DUMP_DIR
 
 # run mysqlbackup, tar gz and delete sql file
-mysqldump --defaults-extra-file=$DB_FILE $DB_NAME $DB_TABLES > $sql_file
+if $FULL; then
+    mysqldump --defaults-extra-file=$DB_FILE $DB_NAME > $sql_file
+else
+    mysqldump --defaults-extra-file=$DB_FILE $DB_NAME $DB_TABLES > $sql_file
+fi
 tar -zvcf $gz_file $sql_file
 rm $sql_file
 
