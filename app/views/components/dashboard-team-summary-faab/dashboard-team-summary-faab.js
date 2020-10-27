1835da97a621ceb6f9825cdd592ccae690b21a2c
mistakia <1823355+mistakia@users.noreply.github.com>
2020-10-27 19:58:00 -0400

    feat: show all teams waiver, faab, and salary space on dashboard

---
+++ app/views/components/dashboard-team-summary-faab/dashboard-team-summary-faab.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Accordion from '@material-ui/core/Accordion'
+import AccordionDetails from '@material-ui/core/AccordionDetails'
+import AccordionSummary from '@material-ui/core/AccordionSummary'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+import Rank from '@components/rank'
+
+export default class DashboardTeamSummaryFAAB extends React.Component {
+  render = () => {
+    const { team, teams, rank } = this.props
+
+    const items = []
+    for (const team of teams.valueSeq()) {
+      items.push(
+        <tr key={team.uid}>
+          <td>{team.name}</td>
+          <td>${team.faab}</td>
+        </tr>
+      )
+    }
+
+    return (
+      <Accordion TransitionProps={{ unmountOnExit: true }}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Grid container>
+            <Grid item xs={8}>
+              Free Agency Budget
+            </Grid>
+            <Grid item xs={4}>
+              ${team.faab} (<Rank rank={rank} size={teams.size} />)
+            </Grid>
+          </Grid>
+        </AccordionSummary>
+        <AccordionDetails>
+          <table>
+            <tbody>
+              {items}
+            </tbody>
+          </table>
+        </AccordionDetails>
+      </Accordion>
+    )
+  }
+}
---
+++ app/views/components/dashboard-team-summary-waiver-order/dashboard-team-summary-waiver-order.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Accordion from '@material-ui/core/Accordion'
+import AccordionDetails from '@material-ui/core/AccordionDetails'
+import AccordionSummary from '@material-ui/core/AccordionSummary'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+import Rank from '@components/rank'
+import { nth } from '@common'
+
+export default class DashboardTeamSummaryWaiverOrder extends React.Component {
+  render = () => {
+    const { team, teams } = this.props
+
+    const items = []
+    for (const team of teams.valueSeq()) {
+      items.push(
+        <tr key={team.uid}>
+          <td>{team.name}</td>
+          <td>{team.wo}{nth(team.wo)}</td>
+        </tr>
+      )
+    }
+
+    return (
+      <Accordion TransitionProps={{ unmountOnExit: true }}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Grid container>
+            <Grid item xs={8}>
+              Waiver Order
+            </Grid>
+            <Grid item xs={4}>
+              <Rank rank={team.wo} size={teams.size} />
+            </Grid>
+          </Grid>
+        </AccordionSummary>
+        <AccordionDetails>
+          <table>
+            <tbody>
+              {items}
+            </tbody>
+          </table>
+        </AccordionDetails>
+      </Accordion>
+    )
+  }
+}
