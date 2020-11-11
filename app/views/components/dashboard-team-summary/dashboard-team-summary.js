692f7d924100cb228ddee9152e6faeec5e7e6db2
mistakia <1823355+mistakia@users.noreply.github.com>
2020-11-11 11:26:03 -0500

    feat: dashboard record & playoff odds summary

--- app/views/components/dashboard-team-summary/dashboard-team-summary.js
+++ app/views/components/dashboard-team-summary/dashboard-team-summary.js
@@ -3,11 +3,15 @@ import React from 'react'
 import DashboardTeamSummaryFAAB from '@components/dashboard-team-summary-faab'
 import DashboardTeamSummarySalary from '@components/dashboard-team-summary-salary'
 import DashboardTeamSummaryWaiverOrder from '@components/dashboard-team-summary-waiver-order'
+import DashboardTeamSummaryRecord from '@components/dashboard-team-summary-record'
+import DashboardTeamSummaryPlayoffOdds from '@components/dashboard-team-summary-playoff-odds'
 
 const DashboardTeamSummary = () => (
   <div className='dashboard__section-side'>
     <div className='dashboard__section-side-title'>Summary</div>
     <div className='dashboard__section-side-body'>
+      <DashboardTeamSummaryRecord />
+      <DashboardTeamSummaryPlayoffOdds />
       <DashboardTeamSummaryFAAB />
       <DashboardTeamSummarySalary />
       <DashboardTeamSummaryWaiverOrder />
