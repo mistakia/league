285c387936fc4f92a7e6f4e14cfe8a374c5ecd1b
mistakia <1823355+mistakia@users.noreply.github.com>
2021-02-14 19:14:51 -0500

    chore: update constants

--- common/season.js
+++ common/season.js
@@ -2,10 +2,10 @@ import moment from 'moment-timezone'
 
 export default class Season {
   constructor () {
-    this.offseason = moment.tz('2020-02-02', 'America/New_York') // Super Bowl
-    this.start = moment.tz('2020-09-01', 'America/New_York') // Two Tuesdays before first game
-    this.end = moment.tz('2021-02-07', 'America/New_York') // super bowl
-    this.openingDay = moment.tz('2020-09-10', 'America/New_York') // first game
+    this.offseason = moment.tz('2021-02-07', 'America/New_York') // Super Bowl
+    this.start = moment.tz('2021-08-31', 'America/New_York') // Two Tuesdays before first game
+    this.end = moment.tz('2022-02-06', 'America/New_York') // super bowl
+    this.openingDay = moment.tz('2021-09-09', 'America/New_York') // first game
     this.finalWeek = 16
     this.nflFinalWeek = 17
   }
