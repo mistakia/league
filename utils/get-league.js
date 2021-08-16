e1f147d14497b444feb5e9c1511f0195b3cd12f0
mistakia <1823355+mistakia@users.noreply.github.com>
2021-08-15 21:54:29 -0400

    fix: check for extension deadline on tags api

--- utils/get-league.js
+++ utils/get-league.js
@@ -1,6 +1,13 @@
 const db = require('../db')
+const { constants } = require('../common')
 
 module.exports = async (leagueId) => {
-  const leagues = await db('leagues').where({ uid: leagueId })
+  const leagues = await db('leagues')
+    .leftJoin('seasons', 'leagues.uid', 'seasons.lid')
+    .where(function() {
+      this.where('year', constants.season.year)
+      this.orWhereNull('year')
+    })
+    .where({ uid: leagueId })
   return leagues[0]
 }
