cb08a2d8fd4a3de5e659f671b5217430667e90e9
mistakia <1823355+mistakia@users.noreply.github.com>
2021-08-23 03:43:52 -0400

    style: settings page

--- app/views/pages/settings/settings.js
+++ app/views/pages/settings/settings.js
@@ -6,8 +6,8 @@ import EditableLeague from '@components/editable-league'
 import SettingsTeam from '@components/settings-team'
 import PageLayout from '@layouts/page'
 import EditableTeams from '@components/editable-teams'
-import SettingsProjections from '@components/settings-projections'
-import SettingsValue from '@components/settings-value'
+// import SettingsProjections from '@components/settings-projections'
+// import SettingsValue from '@components/settings-value'
 import SettingsNotifications from '@components/settings-notifications'
 
 import './settings.styl'
@@ -17,12 +17,12 @@ export default class SettingsPage extends React.Component {
     const { userId, leagueId, teamId, isHosted } = this.props
 
     const body = (
-      <Container maxWidth='lg' classes={{ root: 'settings' }}>
+      <Container classes={{ root: 'settings' }}>
         <div>
           <EditableLeague lid={leagueId} />
           {teamId && <SettingsTeam tid={teamId} />}
-          <SettingsValue />
-          <SettingsProjections />
+          {/* <SettingsValue />
+              <SettingsProjections /> */}
           {userId && isHosted && <SettingsNotifications />}
         </div>
         <EditableTeams />
