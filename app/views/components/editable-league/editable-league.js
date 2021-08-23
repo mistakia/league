3279795b34c291f6ffe8453277ed44ce9a6a0bd8
mistakia <1823355+mistakia@users.noreply.github.com>
2021-08-23 04:01:59 -0400

    fix: dayjs format typo

--- app/views/components/editable-league/editable-league.js
+++ app/views/components/editable-league/editable-league.js
@@ -46,7 +46,9 @@ export default class EditableLeague extends React.Component {
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <div className='settings__section-title'>League</div>
           <div className='settings__section-description'>
-            {`${(isCommish ? 'Edit ' : '')}Scoring / Starting Lineup / Roster Limits`}
+            {`${
+              isCommish ? 'Edit ' : ''
+            }Scoring / Starting Lineup / Roster Limits`}
           </div>
         </AccordionSummary>
         <AccordionDetails>
