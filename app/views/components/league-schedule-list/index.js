e0070c31980d9b2feb98a67234e71dab49013e38
mistakia <1823355+mistakia@users.noreply.github.com>
2021-09-05 17:23:00 -0400

    feat: add league schedule to mobile menu

---
+++ app/views/components/league-schedule-list/index.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux'
+import { createSelector } from 'reselect'
+
+import { getTeamEvents } from '@core/teams'
+import { getLeagueEvents } from '@core/leagues'
+
+import LeagueScheduleList from './league-schedule-list'
+
+const mapStateToProps = createSelector(
+  getTeamEvents,
+  getLeagueEvents,
+  (teamEvents, leagueEvents) => {
+    const events = teamEvents
+      .concat(leagueEvents)
+      .sort((a, b) => a.date.unix() - b.date.unix())
+
+    return { events }
+  }
+)
+
+export default connect(mapStateToProps)(LeagueScheduleList)
