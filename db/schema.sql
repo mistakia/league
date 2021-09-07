9f1497646dca4419386384b4150769c83477776a
mistakia <1823355+mistakia@users.noreply.github.com>
2021-09-07 00:19:15 -0400

    refactor: remove market_salary_adj

--- db/schema.sql
+++ db/schema.sql
@@ -1713,7 +1713,6 @@ CREATE TABLE `league_player_projection_values` (
   `vorp` decimal(5,2) DEFAULT NULL,
   `vorp_adj` decimal(5,2) DEFAULT NULL,
   `market_salary` decimal(5,2) DEFAULT NULL,
-  `market_salary_adj` decimal(5,2) DEFAULT NULL,
   KEY `player` (`player`),
   UNIQUE KEY `player_value` (`player`, `lid`, `week`, `year`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
