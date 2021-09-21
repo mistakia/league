eff02aff3fe6ae7af244bbef09832a16f6cba89f
mistakia <1823355+mistakia@users.noreply.github.com>
2021-09-21 02:24:18 -0400

    chore: update constants

--- common/constants.js
+++ common/constants.js
@@ -79,7 +79,7 @@ export const scoring = {
 }
 export const years = getAvailableYears()
 export const byeWeeks = [6, 7, 8, 9, 10, 11, 12, 13, 14]
-export const weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+export const weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 export const fantasyWeeks = [
   1,
   2,
