6612230e070919738b0e4c98705b7e18e438ecd2
mistakia <1823355+mistakia@users.noreply.github.com>
2021-11-07 12:33:34 -0500

    feat: update cov reserve eligibility check

--- common/is-reserve-cov-eligible.js
+++ common/is-reserve-cov-eligible.js
@@ -1,3 +1,3 @@
 export default function isReserveCovEligible(player) {
-  return player.status === 'Reserve/COVID-19'
+  return player.status === 'Reserve/COVID-19' || player.injury_status === 'COV'
 }
