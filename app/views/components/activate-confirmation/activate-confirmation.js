481ac7c04ae4c5a1734bdc1a7230f8501a494e34
mistakia <1823355+mistakia@users.noreply.github.com>
2021-11-29 17:19:17 -0500

    fix: activate confirmation for practice squad

--- app/views/components/activate-confirmation/activate-confirmation.js
+++ app/views/components/activate-confirmation/activate-confirmation.js
@@ -84,6 +84,13 @@ export default class ActivateConfirmation extends React.Component {
       )
     }
 
+    const isReservePlayer = player.slot === constants.slots.IR
+    let noBenchSpaceMessage =
+      'No active roster space available, make room by releasing a player'
+    if (isReservePlayer)
+      noBenchSpaceMessage +=
+        ' or moving any reserve eligible players to reserve.'
+
     return (
       <Dialog open onClose={this.props.onClose}>
         <DialogTitle>Activate Player</DialogTitle>
@@ -92,25 +99,24 @@ export default class ActivateConfirmation extends React.Component {
             {`${player.fname} ${player.lname} (${player.pos}) will be placed on the active roster. If the player was not previously activated, activating them will make them ineligible for the practice squad.`}
           </DialogContentText>
           {!this._hasBenchSpace && (
-            <DialogContentText>
-              No active roster space available, make room by releasing a player
-              or moving any reserve eligible players to reserve.
-            </DialogContentText>
+            <DialogContentText>{noBenchSpaceMessage}</DialogContentText>
           )}
           <div className='waiver__claim-inputs'>
-            {!this._hasBenchSpace && Boolean(reserveItems.length) && (
-              <FormControl size='small' variant='outlined'>
-                <InputLabel id='reserve-label'>Reserve</InputLabel>
-                <Select
-                  labelId='reserve-label'
-                  error={this.state.missing}
-                  value={this.state.reserve}
-                  onChange={this.handleSelectReserve}
-                  label='Reserve'>
-                  {reserveItems}
-                </Select>
-              </FormControl>
-            )}
+            {isReservePlayer &&
+              !this._hasBenchSpace &&
+              Boolean(reserveItems.length) && (
+                <FormControl size='small' variant='outlined'>
+                  <InputLabel id='reserve-label'>Reserve</InputLabel>
+                  <Select
+                    labelId='reserve-label'
+                    error={this.state.missing}
+                    value={this.state.reserve}
+                    onChange={this.handleSelectReserve}
+                    label='Reserve'>
+                    {reserveItems}
+                  </Select>
+                </FormControl>
+              )}
           </div>
           <div className='waiver__claim-inputs'>
             {!this._hasBenchSpace && (
