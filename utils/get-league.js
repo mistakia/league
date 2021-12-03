7b4629c801efbe1d868808cb14ac90fc3c11b2ea
mistakia <1823355+mistakia@users.noreply.github.com>
2021-12-03 14:09:51 -0500

    fix: default league when logged out

--- utils/get-league.js
+++ utils/get-league.js
@@ -1,7 +1,12 @@
 const db = require('../db')
-const { constants } = require('../common')
+const { constants, createDefaultLeague } = require('../common')
 
 module.exports = async (leagueId) => {
+  if (!leagueId) {
+    const league = createDefaultLeague({ userId: 0 })
+    return { uid: 0, ...league }
+  }
+
   const leagues = await db('leagues')
     .leftJoin('seasons', function () {
       this.on('leagues.uid', '=', 'seasons.lid')
