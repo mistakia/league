c308f5ff4eefaa1892688102c25391f0d7ba4179
mistakia <1823355+mistakia@users.noreply.github.com>
2021-12-15 01:26:51 -0500

    style: trade menu

--- app/views/components/trade-menu/index.js
+++ app/views/components/trade-menu/index.js
@@ -2,12 +2,14 @@ import { connect } from 'react-redux'
 import { createSelector } from 'reselect'
 
 import { getTrade, tradeActions } from '@core/trade'
+import { getApp } from '@core/app'
 
 import TradeMenu from './trade-menu'
 
-const mapStateToProps = createSelector(getTrade, (trade) => ({
-  trades: trade.items,
-  selectedTradeId: trade.selectedTradeId
+const mapStateToProps = createSelector(getTrade, getApp, (trade, app) => ({
+  trades: trade.items.sort((a, b) => b.uid - a.uid),
+  selectedTradeId: trade.selectedTradeId,
+  teamId: app.teamId
 }))
 
 const mapDispatchToProps = {
