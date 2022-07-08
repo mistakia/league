230fafa295c559b35c2e84adf2956baba19db024
mistakia <1823355+mistakia@users.noreply.github.com>
2022-07-08 16:16:34 -0400

    fix: transition bid conditional release player values

--- app/views/components/transition-confirmation/index.js
+++ app/views/components/transition-confirmation/index.js
@@ -1,24 +1,19 @@
 import { connect } from 'react-redux'
 import { createSelector } from 'reselect'
 
-import { getCurrentLeague, isBeforeExtensionDeadline } from '@core/leagues'
 import { getCurrentPlayers, rosterActions } from '@core/rosters'
 import { getCutlistTotalSalary, getPlayers } from '@core/players'
 
 import TransitionConfirmation from './transition-confirmation'
 
 const mapStateToProps = createSelector(
-  getCurrentLeague,
   getCurrentPlayers,
   getCutlistTotalSalary,
   getPlayers,
-  isBeforeExtensionDeadline,
-  (league, team, cutlistTotalSalary, players, isBeforeExtensionDeadline) => ({
-    league,
+  (team, cutlistTotalSalary, players) => ({
     team,
     cutlistTotalSalary,
-    cutlist: players.get('cutlist'),
-    isBeforeExtensionDeadline
+    cutlist: players.get('cutlist')
   })
 )
 
