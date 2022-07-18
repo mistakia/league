1c290986188ac3bbbc4be7dddb1e32d5d2f36806
mistakia <1823355+mistakia@users.noreply.github.com>
2022-07-18 17:22:08 -0400

    refactor: memoize selectors

--- app/core/leagues/selectors.js
+++ app/core/leagues/selectors.js
@@ -11,10 +11,13 @@ export function getLeagues(state) {
   return state.get('leagues').toList()
 }
 
-export function getCurrentLeague(state) {
-  const { leagueId } = getApp(state)
-  return state.get('leagues').get(leagueId).toJS()
-}
+export const getCurrentLeague = createSelector(
+  (state) => state.getIn(['app', 'leagueId']),
+  (state) => state.get('leagues'),
+  (leagueId, leagues) => {
+    return leagues.get(leagueId).toJS()
+  }
+)
 
 export function getCurrentLeagueTeamIds(state) {
   const { leagueId } = getApp(state)
@@ -24,157 +27,6 @@ export function getCurrentLeagueTeamIds(state) {
 export function getLeagueById(state, { lid }) {
   const leagues = state.get('leagues')
   return leagues.get(lid) || new League()
-}
-
-export function getLeagueEvents(state) {
-  const league = getCurrentLeague(state)
-  const events = []
-  const now = dayjs()
-
-  if (league.ext_date) {
-    const ext_date = dayjs.unix(league.ext_date)
-    if (now.isBefore(ext_date)) {
-      events.push({
-        detail: 'Extension Deadline',
-        date: ext_date
-      })
-    }
-  }
-
-  if (league.tran_start) {
-    const tran_start = dayjs.unix(league.tran_start)
-    if (now.isBefore(tran_start)) {
-      events.push({
-        detail: 'Restricted FA Begins',
-        date: tran_start
-      })
-    }
-  }
-
-  if (league.tran_end) {
-    const tran_end = dayjs.unix(league.tran_end)
-    if (now.isBefore(tran_end)) {
-      events.push({
-        detail: 'Restricted FA Ends',
-        date: tran_end
-      })
-    }
-  }
-
-  if (league.draft_start) {
-    const draft_start = dayjs.unix(league.draft_start)
-    if (now.isBefore(draft_start)) {
-      events.push({
-        detail: 'Rookie Draft Begins',
-        date: draft_start
-      })
-    }
-
-    const lastPick = getLastPick(state)
-    if (lastPick) {
-      const draftDates = getDraftDates({
-        start: league.draft_start,
-        type: league.draft_type,
-        min: league.draft_hour_min,
-        max: league.draft_hour_max,
-        picks: lastPick.pick
-      })
-
-      if (now.isBefore(draftDates.draftEnd)) {
-        events.push({
-          detail: 'Rookie Draft Ends',
-          date: draftDates.draftEnd
-        })
-      }
-
-      if (now.isBefore(draftDates.waiverEnd)) {
-        events.push({
-          detail: 'Rookie Waivers Clear',
-          date: draftDates.waiverEnd
-        })
-      }
-    }
-  }
-
-  const firstDayOfRegularSeason = constants.season.start.add('1', 'week')
-  if (now.isBefore(firstDayOfRegularSeason)) {
-    events.push({
-      detail: 'Regular Season Begins',
-      date: firstDayOfRegularSeason
-    })
-  }
-
-  const firstWaiverDate = constants.season.start
-    .add('1', 'week')
-    .day(3)
-    .hour(15)
-  if (now.isBefore(firstWaiverDate)) {
-    events.push({
-      detail: 'Veteran Waivers Clear',
-      date: firstWaiverDate
-    })
-  } else if (constants.season.isRegularSeason) {
-    const waiverDate = constants.season.now.day(3).hour(15).minute(0)
-    const nextWaiverDate = now.isBefore(waiverDate)
-      ? waiverDate
-      : waiverDate.add('1', 'week')
-
-    events.push({
-      detail: 'Waivers Processed',
-      date: nextWaiverDate
-    })
-  }
-
-  if (now.isBefore(constants.season.openingDay)) {
-    events.push({
-      detail: 'NFL Opening Day',
-      date: constants.season.openingDay
-    })
-  }
-
-  if (league.adate) {
-    const faPeriod = getFreeAgentPeriod(league.adata)
-    const date = dayjs.unix(league.adate)
-    if (now.isBefore(date)) {
-      if (now.isBefore(faPeriod.start)) {
-        events.push({
-          detail: 'Free Agency Period Begins',
-          date: faPeriod.start
-        })
-      }
-
-      events.push({
-        detail: 'Auction',
-        date
-      })
-    }
-
-    if (now.isBefore(faPeriod.end)) {
-      events.push({
-        detail: 'Free Agency Period Ends',
-        data: faPeriod.end
-      })
-    }
-  }
-
-  if (league.tddate) {
-    const date = dayjs.unix(league.tddate)
-    if (now.isBefore(date)) {
-      events.push({
-        detail: 'Trade Deadline',
-        date
-      })
-    }
-  }
-
-  if (now.isBefore(constants.season.end)) {
-    events.push({
-      detail: 'Offseason Begins',
-      date: constants.season.end
-    })
-  }
-
-  return events.sort((a, b) => a.date.unix() - b.date.unix())
 }
 
 export function isBeforeExtensionDeadline(state) {
@@ -204,5 +56,158 @@ export const getTeamsForCurrentLeague = createSelector(
   getTeams,
   (teamIds, teams) => {
     return teamIds.map((tid) => teams.get(tid))
+  }
+)
+
+export const getLeagueEvents = createSelector(
+  getCurrentLeague,
+  getLastPick,
+  (league, lastPick) => {
+    const events = []
+    const now = dayjs()
+
+    if (league.ext_date) {
+      const ext_date = dayjs.unix(league.ext_date)
+      if (now.isBefore(ext_date)) {
+        events.push({
+          detail: 'Extension Deadline',
+          date: ext_date
+        })
+      }
+    }
+
+    if (league.tran_start) {
+      const tran_start = dayjs.unix(league.tran_start)
+      if (now.isBefore(tran_start)) {
+        events.push({
+          detail: 'Restricted FA Begins',
+          date: tran_start
+        })
+      }
+    }
+
+    if (league.tran_end) {
+      const tran_end = dayjs.unix(league.tran_end)
+      if (now.isBefore(tran_end)) {
+        events.push({
+          detail: 'Restricted FA Ends',
+          date: tran_end
+        })
+      }
+    }
+
+    if (league.draft_start) {
+      const draft_start = dayjs.unix(league.draft_start)
+      if (now.isBefore(draft_start)) {
+        events.push({
+          detail: 'Rookie Draft Begins',
+          date: draft_start
+        })
+      }
+
+      if (lastPick) {
+        const draftDates = getDraftDates({
+          start: league.draft_start,
+          type: league.draft_type,
+          min: league.draft_hour_min,
+          max: league.draft_hour_max,
+          picks: lastPick.pick
+        })
+
+        if (now.isBefore(draftDates.draftEnd)) {
+          events.push({
+            detail: 'Rookie Draft Ends',
+            date: draftDates.draftEnd
+          })
+        }
+
+        if (now.isBefore(draftDates.waiverEnd)) {
+          events.push({
+            detail: 'Rookie Waivers Clear',
+            date: draftDates.waiverEnd
+          })
+        }
+      }
+    }
+
+    const firstDayOfRegularSeason = constants.season.start.add('1', 'week')
+    if (now.isBefore(firstDayOfRegularSeason)) {
+      events.push({
+        detail: 'Regular Season Begins',
+        date: firstDayOfRegularSeason
+      })
+    }
+
+    const firstWaiverDate = constants.season.start
+      .add('1', 'week')
+      .day(3)
+      .hour(15)
+    if (now.isBefore(firstWaiverDate)) {
+      events.push({
+        detail: 'Veteran Waivers Clear',
+        date: firstWaiverDate
+      })
+    } else if (constants.season.isRegularSeason) {
+      const waiverDate = constants.season.now.day(3).hour(15).minute(0)
+      const nextWaiverDate = now.isBefore(waiverDate)
+        ? waiverDate
+        : waiverDate.add('1', 'week')
+
+      events.push({
+        detail: 'Waivers Processed',
+        date: nextWaiverDate
+      })
+    }
+
+    if (now.isBefore(constants.season.openingDay)) {
+      events.push({
+        detail: 'NFL Opening Day',
+        date: constants.season.openingDay
+      })
+    }
+
+    if (league.adate) {
+      const faPeriod = getFreeAgentPeriod(league.adata)
+      const date = dayjs.unix(league.adate)
+      if (now.isBefore(date)) {
+        if (now.isBefore(faPeriod.start)) {
+          events.push({
+            detail: 'Free Agency Period Begins',
+            date: faPeriod.start
+          })
+        }
+
+        events.push({
+          detail: 'Auction',
+          date
+        })
+      }
+
+      if (now.isBefore(faPeriod.end)) {
+        events.push({
+          detail: 'Free Agency Period Ends',
+          data: faPeriod.end
+        })
+      }
+    }
+
+    if (league.tddate) {
+      const date = dayjs.unix(league.tddate)
+      if (now.isBefore(date)) {
+        events.push({
+          detail: 'Trade Deadline',
+          date
+        })
+      }
+    }
+
+    if (now.isBefore(constants.season.end)) {
+      events.push({
+        detail: 'Offseason Begins',
+        date: constants.season.end
+      })
+    }
+
+    return events.sort((a, b) => a.date.unix() - b.date.unix())
   }
 )
