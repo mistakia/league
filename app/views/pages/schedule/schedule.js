9aa7bdcac4585617c1287f8f409910d09207c9a6
mistakia <1823355+mistakia@users.noreply.github.com>
2022-07-21 22:47:00 -0400

    style: schedule page

--- app/views/pages/schedule/schedule.js
+++ app/views/pages/schedule/schedule.js
@@ -16,18 +16,21 @@ export default class SchedulePage extends React.Component {
 
     const sections = []
     const groups = groupBy(matchups, 'week')
-    Object.values(groups).forEach((matchups, index) => {
+    for (const [index, [week, matchups]] of Object.entries(
+      Object.entries(groups)
+    )) {
       const items = []
       for (const matchup of matchups) {
         items.push(<Matchup key={matchup.uid} matchup={matchup} />)
       }
       const section = (
         <div key={index} className='schedule__section'>
-          {items}
+          <div className='schedule__section-week'>Week {week}</div>
+          <div className='schedule__section-matchups'>{items}</div>
         </div>
       )
       sections.push(section)
-    })
+    }
 
     const body = (
       <Container maxWidth='md'>
