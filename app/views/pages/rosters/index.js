7d5842363d5bc100b566a85fbbe4dce0bda82334
mistakia <1823355+mistakia@users.noreply.github.com>
2022-07-25 17:48:13 -0400

    style: rosters page

--- app/views/pages/rosters/index.js
+++ app/views/pages/rosters/index.js
@@ -14,14 +14,20 @@ const mapStateToProps = createSelector(
   getTeamsForCurrentLeague,
   (rosters, league, teams) => {
     let ps_count_max = 0
+    let bench_count_max = 0
     for (const roster of rosters.values()) {
       const ps_count = roster.players.filter(
         (r) => r.slot === constants.slots.PS || r.slot === constants.slots.PSP
       )
       ps_count_max = Math.max(ps_count.size, ps_count_max)
+
+      const bench_count = roster.players.filter(
+        (r) => r.slot === constants.slots.BENCH
+      )
+      bench_count_max = Math.max(bench_count.size, bench_count_max)
     }
 
-    return { rosters, league, teams, ps_count_max }
+    return { rosters, league, teams, ps_count_max, bench_count_max }
   }
 )
 
