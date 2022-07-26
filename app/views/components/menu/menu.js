ed039a26a5b922118b8bf78b68b51f9dcdf1dcde
mistakia <1823355+mistakia@users.noreply.github.com>
2022-07-26 00:54:42 -0400

    style: mobile menu button

--- app/views/components/menu/menu.js
+++ app/views/components/menu/menu.js
@@ -4,7 +4,6 @@ import ImmutablePropTypes from 'react-immutable-proptypes'
 import { NavLink } from 'react-router-dom'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import SwipeableDrawer from '@mui/material/SwipeableDrawer'
 import List from '@mui/material/List'
@@ -16,6 +15,7 @@ import Avatar from '@mui/material/Avatar'
 import ExpandLess from '@mui/icons-material/ExpandLess'
 import ExpandMore from '@mui/icons-material/ExpandMore'
 import Collapse from '@mui/material/Collapse'
+import Fab from '@mui/material/Fab'
 
 import LeagueSchedule from '@components/league-schedule'
 import LeagueScheduleList from '@components/league-schedule-list'
@@ -87,131 +87,143 @@ export default class Menu extends React.Component {
     }
 
     return (
-      <div className='main__menu'>
-        <AppBar color='transparent' elevation={0}>
-          <Toolbar variant='dense'>
-            <IconButton
-              edge='start'
-              className='main__menu-button'
-              color='inherit'
-              aria-label='menu'
-              onClick={this.handleOpen}
-            >
-              <MenuIcon />
-            </IconButton>
-            {isLoggedIn && <LeagueSchedule />}
-            {isLoggedIn && <NavLink to='/dashboard'>Roster</NavLink>}
-            {isLoggedIn && constants.season.isRegularSeason && (
-              <NavLink to='/lineups'>Lineup</NavLink>
-            )}
-            <NavLink to='/players'>Players</NavLink>
-            {isLoggedIn && Boolean(constants.season.week) && (
-              <NavLink to='/scoreboard'>Scoreboard</NavLink>
-            )}
-            {isLoggedIn && <NavLink to='/league'>League</NavLink>}
-            {isLoggedIn && <NavLink to='/trade'>Trade</NavLink>}
-            {/* {isLoggedIn && constants.season.week > 0 && (
-                <NavLink to='/props'>Props</NavLink>
-                )} */}
-            {isLoggedIn && !week && <NavLink to='/draft'>Draft</NavLink>}
-            {isLoggedIn && !week && <NavLink to='/auction'>Auction</NavLink>}
-            <NavLink to='/settings'>Settings</NavLink>
-            <NavLink to='/resources'>Resources</NavLink>
-            <NavLink to='/glossary'>Glossary</NavLink>
-            {!isLoggedIn && <NavLink to='/login'>Login/Register</NavLink>}
-          </Toolbar>
-        </AppBar>
-        <SwipeableDrawer
-          anchor='left'
-          open={this.state.open}
-          onOpen={this.handleOpen}
-          onClose={this.handleClose}
-          classes={{
-            paper: 'main__menu-paper'
-          }}
+      <>
+        <Fab
+          color='primary'
+          variant='extended'
+          className='main__menu-button'
+          onClick={this.handleOpen}
         >
-          <List>{header}</List>
-          <Divider />
-          <List>
-            {isLoggedIn && (
-              <ListItem button onClick={this.handleClick('/dashboard')}>
-                <ListItemText primary='Roster' />
-              </ListItem>
-            )}
-            {isLoggedIn && (
-              <ListItem button onClick={this.handleClick('/lineups')}>
-                <ListItemText primary='Lineups' />
-              </ListItem>
-            )}
-            <ListItem button onClick={this.handleClick('/players')}>
-              <ListItemText primary='Players' />
-            </ListItem>
-            {isLoggedIn && (
-              <ListItem button onClick={this.handleClick('/scoreboard')}>
-                <ListItemText primary='Scoreboard' />
-              </ListItem>
-            )}
-            {isLoggedIn && (
-              <ListItem button onClick={this.handleClick('/trade')}>
-                <ListItemText primary='Trade' />
-              </ListItem>
-            )}
-            {isLoggedIn && !week && (
-              <ListItem button onClick={this.handleClick('/auction')}>
-                <ListItemText primary='Auction' />
-              </ListItem>
-            )}
-            {isLoggedIn && !week && (
-              <ListItem button onClick={this.handleClick('/draft')}>
-                <ListItemText primary='Draft' />
-              </ListItem>
-            )}
-          </List>
-          {isLoggedIn && <Divider />}
-          {isLoggedIn && (
+          <MenuIcon sx={{ mr: 1 }} />
+          Menu
+        </Fab>
+        <div className='main__menu'>
+          <AppBar color='transparent' elevation={0}>
+            <Toolbar variant='dense'>
+              {isLoggedIn && <LeagueSchedule />}
+              {isLoggedIn && <NavLink to='/dashboard'>Roster</NavLink>}
+              {isLoggedIn && constants.season.isRegularSeason && (
+                <NavLink to='/lineups'>Lineup</NavLink>
+              )}
+              <NavLink to='/players'>Players</NavLink>
+              {isLoggedIn && Boolean(constants.season.week) && (
+                <NavLink to='/scoreboard'>Scoreboard</NavLink>
+              )}
+              {isLoggedIn && <NavLink to='/league'>League</NavLink>}
+              {isLoggedIn && <NavLink to='/trade'>Trade</NavLink>}
+              {/* {isLoggedIn && constants.season.week > 0 && (
+                  <NavLink to='/props'>Props</NavLink>
+                  )} */}
+              {isLoggedIn && !week && <NavLink to='/draft'>Draft</NavLink>}
+              {isLoggedIn && !week && <NavLink to='/auction'>Auction</NavLink>}
+              <NavLink to='/settings'>Settings</NavLink>
+              <NavLink to='/resources'>Resources</NavLink>
+              <NavLink to='/glossary'>Glossary</NavLink>
+              {!isLoggedIn && <NavLink to='/login'>Login/Register</NavLink>}
+            </Toolbar>
+          </AppBar>
+          <SwipeableDrawer
+            anchor='left'
+            open={this.state.open}
+            onOpen={this.handleOpen}
+            onClose={this.handleClose}
+            classes={{
+              paper: 'main__menu-paper'
+            }}
+          >
+            <List>{header}</List>
+            <Divider />
             <List>
-              <ListItem
-                button
-                onClick={this.handleClick('/league/transactions')}
-              >
-                <ListItemText primary='Transactions' />
+              {isLoggedIn && (
+                <ListItem button onClick={this.handleClick('/dashboard')}>
+                  <ListItemText primary='Roster' />
+                </ListItem>
+              )}
+              {isLoggedIn && (
+                <ListItem button onClick={this.handleClick('/lineups')}>
+                  <ListItemText primary='Lineups' />
+                </ListItem>
+              )}
+              <ListItem button onClick={this.handleClick('/players')}>
+                <ListItemText primary='Players' />
               </ListItem>
-              <ListItem button onClick={this.handleClick('/league/waivers')}>
-                <ListItemText primary='Waivers' />
+              {isLoggedIn && (
+                <ListItem button onClick={this.handleClick('/scoreboard')}>
+                  <ListItemText primary='Scoreboard' />
+                </ListItem>
+              )}
+              {isLoggedIn && (
+                <ListItem button onClick={this.handleClick('/trade')}>
+                  <ListItemText primary='Trade' />
+                </ListItem>
+              )}
+              {isLoggedIn && !week && (
+                <ListItem button onClick={this.handleClick('/auction')}>
+                  <ListItemText primary='Auction' />
+                </ListItem>
+              )}
+              {isLoggedIn && !week && (
+                <ListItem button onClick={this.handleClick('/draft')}>
+                  <ListItemText primary='Draft' />
+                </ListItem>
+              )}
+            </List>
+            {isLoggedIn && <Divider />}
+            {isLoggedIn && (
+              <List>
+                <ListItem
+                  button
+                  onClick={this.handleClick('/league/transactions')}
+                >
+                  <ListItemText primary='Transactions' />
+                </ListItem>
+                <ListItem button onClick={this.handleClick('/league/waivers')}>
+                  <ListItemText primary='Waivers' />
+                </ListItem>
+                <ListItem button onClick={this.handleClick('/league/rosters')}>
+                  <ListItemText primary='Rosters' />
+                </ListItem>
+                <ListItem
+                  button
+                  onClick={this.handleClick('/league/standings')}
+                >
+                  <ListItemText primary='Standings' />
+                </ListItem>
+                <ListItem button onClick={this.handleClick('/league/stats')}>
+                  <ListItemText primary='Stats' />
+                </ListItem>
+                <ListItem button onClick={this.handleClick('/league/schedule')}>
+                  <ListItemText primary='Schedule' />
+                </ListItem>
+              </List>
+            )}
+            <Divider />
+            <List>
+              <ListItem button onClick={this.handleClick('/settings')}>
+                <ListItemText primary='Settings' />
               </ListItem>
-              <ListItem button onClick={this.handleClick('/league/rosters')}>
-                <ListItemText primary='Rosters' />
+              <ListItem button onClick={this.handleClick('/resources')}>
+                <ListItemText primary='Resources' />
               </ListItem>
-              <ListItem button onClick={this.handleClick('/league/standings')}>
-                <ListItemText primary='Standings' />
-              </ListItem>
-              <ListItem button onClick={this.handleClick('/league/stats')}>
-                <ListItemText primary='Stats' />
-              </ListItem>
-              <ListItem button onClick={this.handleClick('/league/schedule')}>
-                <ListItemText primary='Schedule' />
+              <ListItem button onClick={this.handleClick('/glossary')}>
+                <ListItemText primary='Glossary' />
               </ListItem>
             </List>
-          )}
-          <Divider />
-          <List>
-            <ListItem button onClick={this.handleClick('/settings')}>
-              <ListItemText primary='Settings' />
-            </ListItem>
-            <ListItem button onClick={this.handleClick('/resources')}>
-              <ListItemText primary='Resources' />
-            </ListItem>
-            <ListItem button onClick={this.handleClick('/glossary')}>
-              <ListItemText primary='Glossary' />
-            </ListItem>
-          </List>
-        </SwipeableDrawer>
-      </div>
+            <Divider />
+            <List>
+              <ListItem button onClick={this.props.logout}>
+                <ListItemText primary='Logout' />
+              </ListItem>
+            </List>
+          </SwipeableDrawer>
+        </div>
+      </>
     )
   }
 }
 
 Menu.propTypes = {
   isLoggedIn: PropTypes.bool,
-  team: ImmutablePropTypes.record
+  team: ImmutablePropTypes.record,
+  logout: PropTypes.func
 }
