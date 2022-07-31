b8331d6b9b18964c8426af6fd11a622e36f9c89c
mistakia <1823355+mistakia@users.noreply.github.com>
2022-07-31 15:27:16 -0400

    feat: import players from nfl

--- common/format-height.mjs
+++ common/format-height.mjs
@@ -11,13 +11,19 @@ export default function (str) {
     return null
   }
 
-  if (!str.includes("'")) {
+  if (str.includes("'")) {
+    const s = str.split("'")
+    const feet = parseInt(s[0], 10)
+    const inches = parseInt(s[1], 10) || 0
+
+    return feet * 12 + inches
+  } else if (str.includes('-')) {
+    const s = str.split('-')
+    const feet = parseInt(s[0], 10)
+    const inches = parseInt(s[1], 10) || 0
+
+    return feet * 12 + inches
+  } else {
     return parseInt(str, 10)
   }
-
-  const s = str.split("'")
-  const feet = parseInt(s[0], 10)
-  const inches = parseInt(s[1], 10) || 0
-
-  return feet * 12 + inches
 }
