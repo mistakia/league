84a7d48403421457049c76c7e924cb194358084f
mistakia <1823355+mistakia@users.noreply.github.com>
2022-08-02 18:42:09 -0400

    style: set fixed length for selected player table values

--- app/views/components/player-selected-row/player-selected-row.js
+++ app/views/components/player-selected-row/player-selected-row.js
@@ -3,75 +3,75 @@ import PropTypes from 'prop-types'
 
 import PercentileMetric from '@components/percentile-metric'
 
-const defenseStats = (stats, percentiles = {}) => (
+const defenseStats = (stats, percentiles = {}, fixed = 0) => (
   <div className='row__group'>
     <div className='row__group-body'>
-      <PercentileMetric stats={stats} percentiles={percentiles} type='dpa' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='dya' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='dsk' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='dint' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='dff' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='drf' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='dtno' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='dfds' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='dblk' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='dsf' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='dtpr' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='dtd' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='prtd' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='krtd' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='dpa' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='dya' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='dsk' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='dint' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='dff' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='drf' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='dtno' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='dfds' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='dblk' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='dsf' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='dtpr' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='dtd' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='prtd' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='krtd' />
     </div>
   </div>
 )
 
-const kickerStats = (stats, percentiles = {}) => (
+const kickerStats = (stats, percentiles = {}, fixed = 0) => (
   <div className='row__group'>
     <div className='row__group-body'>
-      <PercentileMetric stats={stats} percentiles={percentiles} type='xpm' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='fgm' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='fg19' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='fg29' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='fg39' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='fg49' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='fg50' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='xpm' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='fgm' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='fg19' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='fg29' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='fg39' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='fg49' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='fg50' />
     </div>
   </div>
 )
 
-const playerStats = (stats, percentiles = {}) => [
+const playerStats = (stats, percentiles = {}, fixed = 0) => [
   <div className='row__group' key={0}>
     <div className='row__group-body'>
-      <PercentileMetric stats={stats} percentiles={percentiles} type='py' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='tdp' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='ints' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='py' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='tdp' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='ints' />
     </div>
   </div>,
   <div className='row__group' key={1}>
     <div className='row__group-body'>
-      <PercentileMetric stats={stats} percentiles={percentiles} type='ra' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='ry' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='tdr' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='fuml' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='ra' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='ry' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='tdr' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='fuml' />
     </div>
   </div>,
   <div className='row__group' key={2}>
     <div className='row__group-body'>
-      <PercentileMetric stats={stats} percentiles={percentiles} type='trg' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='rec' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='recy' />
-      <PercentileMetric stats={stats} percentiles={percentiles} type='tdrec' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='trg' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='rec' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='recy' />
+      <PercentileMetric {...{ stats, percentiles, fixed }} type='tdrec' />
     </div>
   </div>
 ]
 
-const getStatRows = (pos, stats, percentiles) => {
+const getStatRows = (pos, stats, percentiles, fixed) => {
   switch (pos) {
     case 'DST':
-      return defenseStats(stats, percentiles)
+      return defenseStats(stats, percentiles, fixed)
     case 'K':
-      return kickerStats(stats, percentiles)
+      return kickerStats(stats, percentiles, fixed)
     default:
-      return playerStats(stats, percentiles)
+      return playerStats(stats, percentiles, fixed)
   }
 }
 
@@ -86,12 +86,13 @@ export default class PlayerSelectedRow extends React.Component {
       lead,
       pos,
       percentiles,
-      header
+      header,
+      fixed
     } = this.props
     const classNames = ['player__selected-row']
     if (className) classNames.push(className)
     if (header) classNames.push('header')
-    const rows = getStatRows(pos, stats, percentiles)
+    const rows = getStatRows(pos, stats, percentiles, fixed)
     return (
       <div className={classNames.join(' ')}>
         {lead || <div className='row__name'>{title}</div>}
@@ -112,5 +113,6 @@ PlayerSelectedRow.propTypes = {
   lead: PropTypes.element,
   pos: PropTypes.string,
   percentiles: PropTypes.object,
-  header: PropTypes.bool
+  header: PropTypes.bool,
+  fixed: PropTypes.number
 }
