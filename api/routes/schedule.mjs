a2dbaad6b0a5f6f9cfccfc6b4bb90b17d9d3cf09
mistakia <1823355+mistakia@users.noreply.github.com>
2022-08-03 19:12:40 -0400

    fix: typo, update nfl_games column

--- api/routes/schedule.mjs
+++ api/routes/schedule.mjs
@@ -18,7 +18,7 @@ router.get('/?', async (req, res) => {
     const games = await db('nfl_games')
       .select('seas', 'wk', 'date', 'time_est', 'v', 'h')
       .where('seas', constants.season.year)
-      .where('type', 'REG')
+      .where('seas_type', 'REG')
       .orderBy('wk', 'asc')
 
     for (const team of constants.nflTeams) {
