2f19e97fd7093f5b3201d180e8510bceedb66d75
mistakia <1823355+mistakia@users.noreply.github.com>
2022-08-04 00:32:56 -0400

    style: show spread for matchups

--- app/views/components/matchup/index.js
+++ app/views/components/matchup/index.js
@@ -2,12 +2,14 @@ import { connect } from 'react-redux'
 import { createSelector } from 'reselect'
 
 import { getTeamsForCurrentLeague } from '@core/teams'
+import { getRosters } from '@core/rosters'
 
 import Matchup from './matchup'
 
 const mapDispatchToProps = createSelector(
   getTeamsForCurrentLeague,
-  (teams) => ({ teams })
+  getRosters,
+  (teams, rosters) => ({ teams, rosters })
 )
 
 export default connect(mapDispatchToProps)(Matchup)
