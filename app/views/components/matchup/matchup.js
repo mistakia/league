5e4518f614cf25c4a823aa12f14c725a93f37a59
mistakia <1823355+mistakia@users.noreply.github.com>
2022-08-04 00:32:56 -0400

    style: show spread for matchups

--- app/views/components/matchup/matchup.js
+++ app/views/components/matchup/matchup.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Map } from 'immutable'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 
+import { constants } from '@common'
 import TeamName from '@components/team-name'
 import TeamImage from '@components/team-image'
 
@@ -8,45 +10,58 @@ import './matchup.styl'
 
 export default class Matchup extends React.Component {
   render = () => {
-    const { matchup, teams } = this.props
+    const { matchup, teams, rosters } = this.props
 
     const home = teams.find((t) => t.uid === matchup.hid)
+    const home_roster = rosters.getIn(
+      [home.uid, constants.season.week],
+      new Map()
+    )
+    const home_baseline_projection = home_roster.getIn(
+      ['lineups', `${matchup.week}`, 'baseline_total'],
+      0
+    )
     const away = teams.find((t) => t.uid === matchup.aid)
+    const away_roster = rosters.getIn(
+      [away.uid, constants.season.week],
+      new Map()
+    )
+    const away_baseline_projection = away_roster.getIn(
+      ['lineups', `${matchup.week}`, 'baseline_total'],
+      0
+    )
+
+    const formatSpread = (value) => (value > 0 ? `+${value}` : value)
 
     return (
       <div className='matchup'>
-        <div className='matchup__body'>
-          <div className='matchup__away'>
-            <div
-              className='matchup__banner'
-              style={{
-                backgroundColor: `#${away.pc}`
-              }}
-            />
-            <div
-              className='matchup__line'
-              style={{
-                backgroundColor: `#${away.ac}`
-              }}
-            />
-            <TeamImage tid={matchup.aid} />
-            <TeamName tid={matchup.aid} />
+        <div className='matchup__head'>
+          <div className='matchup__col metric'>Spread</div>
+        </div>
+        <div className='matchup__away'>
+          <div
+            className='matchup__banner'
+            style={{
+              backgroundColor: `#${away.pc}`
+            }}
+          />
+          <TeamImage tid={matchup.aid} />
+          <TeamName tid={matchup.aid} />
+          <div className='matchup__col metric'>
+            {formatSpread(home_baseline_projection - away_baseline_projection)}
           </div>
-          <div className='matchup__home'>
-            <div
-              className='matchup__banner'
-              style={{
-                backgroundColor: `#${home.pc}`
-              }}
-            />
-            <div
-              className='matchup__line'
-              style={{
-                backgroundColor: `#${home.ac}`
-              }}
-            />
-            <TeamImage tid={matchup.hid} />
-            <TeamName tid={matchup.hid} />
+        </div>
+        <div className='matchup__home'>
+          <div
+            className='matchup__banner'
+            style={{
+              backgroundColor: `#${home.pc}`
+            }}
+          />
+          <TeamImage tid={matchup.hid} />
+          <TeamName tid={matchup.hid} />
+          <div className='matchup__col metric'>
+            {formatSpread(away_baseline_projection - home_baseline_projection)}
           </div>
         </div>
       </div>
@@ -56,5 +71,6 @@ export default class Matchup extends React.Component {
 
 Matchup.propTypes = {
   matchup: ImmutablePropTypes.record,
-  teams: ImmutablePropTypes.map
+  teams: ImmutablePropTypes.map,
+  rosters: ImmutablePropTypes.map
 }
