f32eed0e64787517284d24a1fff4b2e06fcd1ccc
mistakia <1823355+mistakia@users.noreply.github.com>
2022-08-07 21:28:21 -0400

    fix: process play stats in order

--- scripts/process-play-stats.mjs
+++ scripts/process-play-stats.mjs
@@ -385,13 +385,17 @@ const main = async () => {
 
     if (argv.all) {
       log('processing all plays')
-      const years = await db('nfl_plays').select('seas').groupBy('seas')
+      const years = await db('nfl_plays')
+        .select('seas')
+        .groupBy('seas')
+        .orderBy('seas', 'asc')
       for (const { seas } of years) {
         for (const seas_type of constants.seas_types) {
           const weeks = await db('nfl_plays')
             .select('wk')
             .where({ seas, seas_type })
             .groupBy('wk')
+            .orderBy('wk', 'asc')
           log(
             `processing plays for ${weeks.length} weeks in ${seas} (${seas_type})`
           )
