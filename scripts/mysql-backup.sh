ce21aeadd87e9dc6f1824987eaad5ceec5a5225c
mistakia <1823355+mistakia@users.noreply.github.com>
2022-08-10 19:36:33 -0400

    feat: use backup types

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -3,12 +3,18 @@ set -e
 set -x
 
 FULL=false
-LITE=false
+LOGS=false
+STATS=true
+CACHE=true
 
-while getopts 'fl' opt; do
+while getopts 'flsu' opt; do
     case $opt in
         f) FULL=true ;;
-        l) LITE=true ;;
+        c) CACHE=true ;;
+        l) LOGS=true ;;
+        s) STATS=true ;;
+        u) USER=true ;;
+
         *) echo 'Error in command line parsing' >&2
     esac
 done
@@ -16,15 +22,78 @@ done
 DUMP_DIR="/root/backups"
 DB_NAME="league_production"
 DB_FILE="/root/.mysql/mysqldump.cnf"
-DB_TABLES="draft leagues matchups playoffs poaches poach_releases rosters rosters_players seasons team_stats teams trades trade_releases trades_picks trades_players trades_transactions transactions users users_sources users_teams waivers waiver_releases league_cutlist transition_bids transition_releases"
-DB_LITE_TABLES="draft footballoutsiders gamelogs league_baselines league_player_projection_points league_player_projection_values league_team_forecast league_team_lineup_contribution_weeks league_team_lineup_contributions league_team_lineup_starters league_team_lineups leagues matchups nfl_games nfl_play_stats player player_changelog players playoffs poaches poach_releases rosters rosters_players seasons team_stats teams trades trade_releases trades_picks trades_players trades_transactions transactions users users_sources users_teams waivers waiver_releases league_cutlist transition_bids transition_releases"
+
+DB_USER_TABLES="
+draft
+league_cutlist
+leagues
+matchups
+playoffs
+poach_releases
+poaches
+rosters
+rosters_players
+schedule
+seasons
+team_stats
+teams
+trade_releases
+trades
+trades_picks
+trades_players
+trades_transactions
+transactions
+transition_bids
+transition_releases
+users
+users_sources
+users_teams
+waiver_releases
+waivers"
+
+DB_CACHE_TABLES="
+league_baselines
+league_player_projection_points
+league_player_projection_values
+league_team_forecast
+league_team_lineup_contribution_weeks
+league_team_lineup_contributions
+league_team_lineup_starters
+league_team_lineups
+ros_projections"
+
+DB_LOGS_TABLES="
+jobs
+player_changelog"
+
+DB_STATS_TABLES="
+footballoutsiders
+gamelogs
+keeptradecut_rankings
+nfl_games
+nfl_play_stats
+nfl_plays
+nflSnap
+player
+players
+players_status
+practice
+projections
+props
+rankings
+"
+
 DATE_FORMAT="%Y-%m-%d_%H-%M"
 
 file_name="$(date +$DATE_FORMAT)"
 if $FULL; then
     backup_type="full"
-elif $LITE; then
-    backup_type="lite"
+elif $LOGS; then
+    backup_type="logs"
+elif $STATS; then
+    backup_type="stats"
+elif $CACHE; then
+    backup_type="cache"
 else
     backup_type="user"
 fi
@@ -38,10 +107,14 @@ cd $DUMP_DIR
 # run mysqlbackup, tar gz and delete sql file
 if $FULL; then
     mysqldump --defaults-extra-file=$DB_FILE $DB_NAME > $sql_file
-elif $LITE; then
-    mysqldump --defaults-extra-file=$DB_FILE $DB_NAME $DB_LITE_TABLES > $sql_file
+elif $LOGS; then
+    mysqldump --defaults-extra-file=$DB_FILE $DB_NAME $DB_LOGS_TABLES > $sql_file
+elif $STATS; then
+    mysqldump --defaults-extra-file=$DB_FILE $DB_NAME $DB_STATS_TABLES > $sql_file
+elif $CACHE; then
+    mysqldump --defaults-extra-file=$DB_FILE $DB_NAME $DB_CACHE_TABLES > $sql_file
 else
-    mysqldump --defaults-extra-file=$DB_FILE $DB_NAME $DB_TABLES > $sql_file
+    mysqldump --defaults-extra-file=$DB_FILE $DB_NAME $DB_USER_TABLES > $sql_file
 fi
 tar -vcf $gz_file $sql_file
 rm $sql_file
