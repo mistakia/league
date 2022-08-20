d3a7547d9ff5d344ea9458f7de0547c5e77ba749
mistakia <1823355+mistakia@users.noreply.github.com>
2022-08-20 11:17:50 -0400

    feat: auction calculate adjusted market salary for nominated player

--- app/views/components/auction-nominated-player/auction-nominated-player.js
+++ app/views/components/auction-nominated-player/auction-nominated-player.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 
 import PlayerName from '@components/player-name'
@@ -6,31 +7,31 @@ import NFLTeamBye from '@components/nfl-team-bye'
 
 import './auction-nominated-player.styl'
 
-export default class AuctionNominatedPlayer extends React.Component {
-  render = () => {
-    const { playerMap } = this.props
-
-    return (
-      <div className='auction__nominated-player'>
-        <div className='nominated__player'>
-          <PlayerName pid={playerMap.get('pid')} headshot />
+export default function AuctionNominatedPlayer({
+  playerMap,
+  market_salary_adjusted
+}) {
+  return (
+    <div className='auction__nominated-player'>
+      <div className='nominated__player'>
+        <PlayerName pid={playerMap.get('pid')} headshot />
+      </div>
+      <div className='nominated__player-details'>
+        <div className='nominated__player-detail'>
+          Market: ${playerMap.getIn(['market_salary', '0'], 0)}
         </div>
-        <div className='nominated__player-details'>
-          <div className='nominated__player-detail'>
-            Market: ${playerMap.getIn(['market_salary', '0'], 0)}
-          </div>
-          <div className='nominated__player-detail'>
-            Adjusted: ${playerMap.get('market_salary_adj', 0)}
-          </div>
-          <div className='nominated__player-detail'>
-            Bye: <NFLTeamBye team={playerMap.get('team')} />
-          </div>
+        <div className='nominated__player-detail'>
+          Adjusted: ${market_salary_adjusted}
+        </div>
+        <div className='nominated__player-detail'>
+          Bye: <NFLTeamBye team={playerMap.get('team')} />
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 AuctionNominatedPlayer.propTypes = {
-  playerMap: ImmutablePropTypes.map
+  playerMap: ImmutablePropTypes.map,
+  market_salary_adjusted: PropTypes.number
 }
