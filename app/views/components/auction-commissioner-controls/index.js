2f580e16ca89e7f52567558bd954b64b1e1f2a36
mistakia <1823355+mistakia@users.noreply.github.com>
2022-08-20 22:04:45 -0400

    feat: toggle pause on team disconnect

--- app/views/components/auction-commissioner-controls/index.js
+++ app/views/components/auction-commissioner-controls/index.js
@@ -6,12 +6,15 @@ import { auctionActions, getAuction } from '@core/auction'
 import AuctionCommissionerControls from './auction-commissioner-controls'
 
 const mapStateToProps = createSelector(getAuction, (auction) => ({
-  isPaused: auction.isPaused
+  isPaused: auction.isPaused,
+  pause_on_team_disconnect: auction.pause_on_team_disconnect
 }))
 
 const mapDispatchToProps = {
   pause: auctionActions.pause,
-  resume: auctionActions.resume
+  resume: auctionActions.resume,
+  toggle_pause_on_team_disconnect:
+    auctionActions.toggle_pause_on_team_disconnect
 }
 
 export default connect(
