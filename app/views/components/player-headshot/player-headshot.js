a77dcb78c1fe7fa9212716ad513fec8c2062e99d
mistakia <1823355+mistakia@users.noreply.github.com>
2022-08-22 23:54:00 -0400

    feat: fix player headshot for teams

--- app/views/components/player-headshot/player-headshot.js
+++ app/views/components/player-headshot/player-headshot.js
@@ -11,13 +11,23 @@ export default function PlayerHeadshot({
   width = 48,
   square = false
 }) {
+  const isTeam = playerMap.get('pos') === 'DST'
   const height = Math.round((width * 70) / 96)
-  const espn_id = playerMap.get('espn_id')
-  const src = espn_id
-    ? `https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/${espn_id}.png&w=${
-        width * 2
-      }&h=${height * 2}&cb=1`
-    : null
+
+  let src
+  if (isTeam) {
+    const pid = playerMap.get('pid')
+    src = `https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/${pid}.png&h=${
+      height * 2
+    }&w=${width * 2}`
+  } else {
+    const espn_id = playerMap.get('espn_id')
+    src = espn_id
+      ? `https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/${espn_id}.png&w=${
+          width * 2
+        }&h=${height * 2}&cb=1`
+      : null
+  }
 
   const classNames = ['player__headshot']
   if (square) classNames.push('square')
