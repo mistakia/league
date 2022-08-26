af39a677f3448b301b0a0ab16538e1f110a74b5a
mistakia <1823355+mistakia@users.noreply.github.com>
2022-08-26 16:45:00 -0400

    feat: add league title to toolbar

--- app/views/components/menu/index.js
+++ app/views/components/menu/index.js
@@ -3,14 +3,21 @@ import { createSelector } from 'reselect'
 
 import { getApp, appActions } from '@core/app'
 import { getCurrentTeam } from '@core/teams'
+import { getCurrentLeague } from '@core/leagues'
 
 import Menu from './menu'
 
-const mapStateToProps = createSelector(getApp, getCurrentTeam, (app, team) => ({
-  isLoggedIn: Boolean(app.userId),
-  leagueId: app.leagueId,
-  team
-}))
+const mapStateToProps = createSelector(
+  getApp,
+  getCurrentTeam,
+  getCurrentLeague,
+  (app, team, league) => ({
+    isLoggedIn: Boolean(app.userId),
+    leagueId: app.leagueId,
+    league,
+    team
+  })
+)
 
 const mapDispatchToProps = {
   logout: appActions.logout
