1b1b5a0a4951523bc111bfd051e262a02f099f65
mistakia <1823355+mistakia@users.noreply.github.com>
2022-08-29 18:07:08 -0400

    feat: add processed_at field to leagues

--- db/schema.sql
+++ db/schema.sql
@@ -686,6 +686,8 @@ CREATE TABLE `leagues` (
   `b_K` decimal(2,1) unsigned DEFAULT NULL, -- baseline k pts/game
   `b_DST` decimal(2,1) unsigned DEFAULT NULL, -- baseline dst pts/game
 
+  `processed_at` int(11) NOT NULL,
+
   UNIQUE KEY `uid` (`uid`),
   KEY `commishid` (`commishid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
