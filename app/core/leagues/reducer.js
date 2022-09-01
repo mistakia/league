5ceefe3cdc7c765a96252a0bdc2e90776598e085
mistakia <1823355+mistakia@users.noreply.github.com>
2022-09-01 16:02:41 -0400

    refactor: remove players isInitializing

--- app/core/leagues/reducer.js
+++ app/core/leagues/reducer.js
@@ -8,7 +8,7 @@ import { constants, createDefaultLeague } from '@common'
 
 const initialState = new Map().set(
   constants.DEFAULTS.LEAGUE_ID,
-  new League(createDefaultLeague())
+  new League(createDefaultLeague({ isLoaded: true }))
 )
 
 export function leaguesReducer(state = initialState, { payload, type }) {
