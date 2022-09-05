e1ff79c9d8ad13d63cbccb19ab30e79d8b380915
mistakia <1823355+mistakia@users.noreply.github.com>
2022-09-05 19:28:19 -0400

    feat: add round column to player table

--- db/schema.sql
+++ db/schema.sql
@@ -114,6 +114,7 @@ CREATE TABLE `player` (
   `arm` decimal(5,3) DEFAULT NULL,
   `hand` decimal(5,3) DEFAULT NULL,
   `dpos` int(3) NOT NULL DEFAULT '0', -- draft position
+  `round` tinyint(1) NOT NULL DEFAULT '0', -- draft round
   `col` varchar(255) NOT NULL,
   `dv` varchar(35) DEFAULT NULL,
   `start` int(4) NOT NULL,
