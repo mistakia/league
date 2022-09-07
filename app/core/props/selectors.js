64d4b9e42a815ec9b02755431aff69185cd3c172
mistakia <1823355+mistakia@users.noreply.github.com>
2022-09-07 13:27:06 -0400

    feat: import draft king player prop lines

--- app/core/props/selectors.js
+++ app/core/props/selectors.js
@@ -18,39 +18,39 @@ export function getFilteredProps(state) {
     const playerMap = getPlayerById(state, { pid: prop.pid })
     const proj = playerMap.getIn(['projection', `${prop.wk}`], {})
     switch (prop.type) {
-      case constants.oddTypes.GAME_PASSING:
+      case constants.player_prop_types.GAME_PASSING_YARDS:
         prop.proj = proj.py
         break
 
-      case constants.oddTypes.GAME_RECEIVING:
+      case constants.player_prop_types.GAME_RECEIVING_YARDS:
         prop.proj = proj.recy
         break
 
-      case constants.oddTypes.GAME_RUSHING:
+      case constants.player_prop_types.GAME_RUSHING_YARDS:
         prop.proj = proj.ry
         break
 
-      case constants.oddTypes.GAME_COMPLETIONS:
+      case constants.player_prop_types.GAME_PASSING_COMPLETIONS:
         prop.proj = proj.pc
         break
 
-      case constants.oddTypes.GAME_PASSING_TOUCHDOWNS:
+      case constants.player_prop_types.GAME_PASSING_TOUCHDOWNS:
         prop.proj = proj.tdp
         break
 
-      case constants.oddTypes.GAME_RECEPTIONS:
+      case constants.player_prop_types.GAME_RECEPTIONS:
         prop.proj = proj.rec
         break
 
-      case constants.oddTypes.GAME_INTERCEPTIONS:
+      case constants.player_prop_types.GAME_PASSING_INTERCEPTIONS:
         prop.proj = proj.ints
         break
 
-      case constants.oddTypes.GAME_CARRIES:
+      case constants.player_prop_types.GAME_RUSHING_ATTEMPTS:
         prop.proj = proj.ra
         break
 
-      case constants.oddTypes.GAME_TOUCHDOWNS:
+      case constants.player_prop_types.GAME_TOUCHDOWNS:
         prop.proj = proj.tdr + proj.tdrec
         break
 
