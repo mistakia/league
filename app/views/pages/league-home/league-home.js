8b0e5634b479b3f4a107d7ce22ebd091ab3a78a7
mistakia <1823355+mistakia@users.noreply.github.com>
2022-10-11 10:46:38 -0400

    fix: prevent processing poaching claims when players are locked

--- app/views/pages/league-home/league-home.js
+++ app/views/pages/league-home/league-home.js
@@ -164,11 +164,36 @@ export default function LeagueHomePage({
     }
   }
 
+  const getPoachProcessingTime = (submitted) => {
+    // if submitted between thursday 6pm and sunday 3pm — set to tuesday at 3pm
+    const { now } = constants.season
+    const start_window = (
+      now.day() < 3 ? now.subtract('1', 'week').day(5) : now.day(5)
+    )
+      .startOf('day')
+      .hour(18)
+    const end_window = (
+      now.day() < 3 ? now.day(0) : now.add('1', 'week').day(0)
+    )
+      .startOf('day')
+      .hour(15)
+
+    const submitted_timestamp = dayjs.unix(submitted)
+    if (submitted_timestamp.isBetween(start_window, end_window)) {
+      return (now.day() < 3 ? now.day(2) : now.add('1', 'week').day(2))
+        .startOf('day')
+        .hour(15)
+    } else {
+      // otherwise add 48 hours
+      return submitted_timestamp.add('48', 'hours')
+    }
+  }
+
   for (const poach of poaches) {
     const playerMap = poach.get('playerMap')
     if (!playerMap) continue
 
-    const processingTime = dayjs.unix(poach.submitted).add('48', 'hours')
+    const processingTime = getPoachProcessingTime(poach.submitted)
     notices.push(
       <Alert key={playerMap.get('pid')} severity='warning'>
         <div>
