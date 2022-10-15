033ef539dc1de9f01e7e5a9de9a09b8d189c1cc2
mistakia <1823355+mistakia@users.noreply.github.com>
2022-10-15 12:05:19 -0400

    fix: submit poach processing time message

---
+++ common/get-poach-processing-time.mjs
@@ -0,0 +1,32 @@
+import dayjs from 'dayjs'
+import isBetween from 'dayjs/plugin/isBetween.js'
+import * as constants from './constants.mjs'
+
+dayjs.extend(isBetween)
+
+export default function (submitted) {
+  if (constants.season.isOffseason) {
+    return submitted_timestamp.add('48', 'hours')
+  }
+
+  // if submitted between thursday 6pm and sunday 3pm — set to tuesday at 3pm
+  const { now } = constants.season
+  const start_window = (
+    now.day() < 3 ? now.subtract('1', 'week').day(5) : now.day(5)
+  )
+    .startOf('day')
+    .hour(18)
+  const end_window = (now.day() < 3 ? now.day(0) : now.add('1', 'week').day(0))
+    .startOf('day')
+    .hour(15)
+
+  const submitted_timestamp = dayjs.unix(submitted)
+  if (submitted_timestamp.isBetween(start_window, end_window)) {
+    return (now.day() < 3 ? now.day(2) : now.add('1', 'week').day(2))
+      .startOf('day')
+      .hour(15)
+  } else {
+    // otherwise add 48 hours
+    return submitted_timestamp.add('48', 'hours')
+  }
+}
