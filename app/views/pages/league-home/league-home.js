b46029f44c6dca85921dd2a8c2b0e6f8f3023cf5
mistakia <1823355+mistakia@users.noreply.github.com>
2022-10-15 12:05:19 -0400

    fix: submit poach processing time message

--- app/views/pages/league-home/league-home.js
+++ app/views/pages/league-home/league-home.js
@@ -21,7 +21,8 @@ import {
   constants,
   isReserveEligible,
   isReserveCovEligible,
-  getFreeAgentPeriod
+  getFreeAgentPeriod,
+  getPoachProcessingTime
 } from '@common'
 
 import './league-home.styl'
@@ -161,31 +162,6 @@ export default function LeagueHomePage({
           acquisitions or claims.
         </Alert>
       )
-    }
-  }
-
-  const getPoachProcessingTime = (submitted) => {
-    // if submitted between thursday 6pm and sunday 3pm — set to tuesday at 3pm
-    const { now } = constants.season
-    const start_window = (
-      now.day() < 3 ? now.subtract('1', 'week').day(5) : now.day(5)
-    )
-      .startOf('day')
-      .hour(18)
-    const end_window = (
-      now.day() < 3 ? now.day(0) : now.add('1', 'week').day(0)
-    )
-      .startOf('day')
-      .hour(15)
-
-    const submitted_timestamp = dayjs.unix(submitted)
-    if (submitted_timestamp.isBetween(start_window, end_window)) {
-      return (now.day() < 3 ? now.day(2) : now.add('1', 'week').day(2))
-        .startOf('day')
-        .hour(15)
-    } else {
-      // otherwise add 48 hours
-      return submitted_timestamp.add('48', 'hours')
     }
   }
 
