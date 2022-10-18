b0294c22a9edee74690341082ca1e409ced7522c
mistakia <1823355+mistakia@users.noreply.github.com>
2022-10-18 14:43:57 -0400

    feat: load percentiles

--- app/views/components/percentile-metric/index.js
+++ app/views/components/percentile-metric/index.js
@@ -1,1 +1,12 @@
-export { default } from './percentile-metric'
+import { connect } from 'react-redux'
+import { createSelector } from 'reselect'
+
+import { getPercentiles } from '@core/percentiles'
+
+import PercentileMetric from './percentile-metric'
+
+const mapStateToProps = createSelector(getPercentiles, (percentiles) => ({
+  percentiles
+}))
+
+export default connect(mapStateToProps)(PercentileMetric)
--- app/views/components/selected-player-matchup-table/selected-player-matchup-table.js
+++ app/views/components/selected-player-matchup-table/selected-player-matchup-table.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 import PropTypes from 'prop-types'
 
@@ -6,102 +6,120 @@ import PlayerSelectedRow from '@components/player-selected-row'
 import PlayerSelectedRowHeader from '@components/player-selected-row-header'
 import PlayerNameText from '@components/player-name-text'
 
-export default class SelectedPlayerMatchupTable extends React.Component {
-  render = () => {
-    const { gamelogs, position, opp, nfl_team_against_seasonlogs } = this.props
+export default function SelectedPlayerMatchupTable({
+  gamelogs,
+  position,
+  opp,
+  nfl_team_against_seasonlogs,
+  loadPercentiles
+}) {
+  if (!opp) {
+    return <div>BYE</div>
+  }
 
-    if (!opp) {
-      return <div>BYE</div>
+  const individual_percentile_key = `INDIVIDUAL_GAMELOG_${position}`
+
+  // load percentiles
+  useEffect(() => {
+    const percentile_keys = [individual_percentile_key]
+    nfl_team_against_seasonlogs.forEach((item) =>
+      percentile_keys.push(item.percentile_key)
+    )
+    for (const percentile_key of percentile_keys) {
+      loadPercentiles(percentile_key)
     }
+  }, [])
 
-    const rows = []
-    nfl_team_against_seasonlogs.forEach((item, index) => {
-      const lead = (
-        <>
-          <div className='table__cell sticky__column metric game__week' />
-          <div className='table__cell sticky__column sticky__two text'>
-            {item.title}
-          </div>
-          <div className='table__cell metric' />
-          <div className='table__cell metric'>
-            {(item.stats.pts || 0).toFixed(1)}
-          </div>
-          <div className='table__cell metric'>
-            {item.points_added ? item.points_added.toFixed(1) : '-'}
-          </div>
-          <div className='table__cell metric'>{item.stats.rnk || '-'}</div>
-        </>
-      )
-      rows.push(
-        <PlayerSelectedRow
-          key={`summary-${index}`}
-          stats={item.stats}
-          lead={lead}
-          pos={position}
-          header
-        />
-      )
-    })
-
-    gamelogs.forEach((gamelog, index) => {
-      const lead = (
-        <>
-          <div className='table__cell sticky__column metric game__week'>
-            {gamelog.week}
-          </div>
-          <div className='table__cell sticky__column sticky__two text'>
-            <PlayerNameText pid={gamelog.pid} />
-          </div>
-          <div className='table__cell metric'>{gamelog.tm}</div>
-          <div className='table__cell metric'>
-            {(gamelog.pts || 0).toFixed(1)}
-          </div>
-          <div className='table__cell metric'>
-            {gamelog.points_added ? gamelog.points_added.toFixed(1) : '-'}
-          </div>
-          <div className='table__cell metric'>{gamelog.pos_rnk || '-'}</div>
-        </>
-      )
-      rows.push(
-        <PlayerSelectedRow
-          key={index}
-          stats={gamelog}
-          lead={lead}
-          pos={position}
-        />
-      )
-    })
-
-    return (
+  const rows = []
+  nfl_team_against_seasonlogs.forEach((item, index) => {
+    const lead = (
       <>
-        <div className='selected__section-header sticky__column'>
-          <div className='row__group-head'>
-            {position}s vs {opp} Gamelogs
-          </div>
+        <div className='table__cell sticky__column metric game__week' />
+        <div className='table__cell sticky__column sticky__two text'>
+          {item.title}
         </div>
-        <div className='selected__section-header'>
-          <div className='table__cell sticky__column metric game__week'>Wk</div>
-          <div className='table__cell sticky__column sticky__two text'>
-            Player
-          </div>
-          <div className='table__cell metric'>Tm</div>
-          <div className='row__group'>
-            <div className='row__group-head'>Fantasy</div>
-            <div className='row__group-body'>
-              <div className='table__cell metric'>Pts</div>
-              <div className='table__cell metric'>Pts+</div>
-              <div className='table__cell metric'>Rnk</div>
-            </div>
-          </div>
-          <PlayerSelectedRowHeader pos={position} />
+        <div className='table__cell metric' />
+        <div className='table__cell metric'>
+          {(item.stats.pts || 0).toFixed(1)}
         </div>
-        {rows}
+        <div className='table__cell metric'>
+          {item.points_added ? item.points_added.toFixed(1) : '-'}
+        </div>
+        <div className='table__cell metric'>{item.stats.rnk || '-'}</div>
       </>
     )
-  }
+    rows.push(
+      <PlayerSelectedRow
+        key={`summary-${index}`}
+        percentile_key={item.percentile_key}
+        stats={item.stats}
+        lead={lead}
+        pos={position}
+        header
+      />
+    )
+  })
+
+  gamelogs.forEach((gamelog, index) => {
+    const lead = (
+      <>
+        <div className='table__cell sticky__column metric game__week'>
+          {gamelog.week}
+        </div>
+        <div className='table__cell sticky__column sticky__two text'>
+          <PlayerNameText pid={gamelog.pid} />
+        </div>
+        <div className='table__cell metric'>{gamelog.tm}</div>
+        <div className='table__cell metric'>
+          {(gamelog.pts || 0).toFixed(1)}
+        </div>
+        <div className='table__cell metric'>
+          {gamelog.points_added ? gamelog.points_added.toFixed(1) : '-'}
+        </div>
+        <div className='table__cell metric'>{gamelog.pos_rnk || '-'}</div>
+      </>
+    )
+    rows.push(
+      <PlayerSelectedRow
+        key={index}
+        percentile_key={individual_percentile_key}
+        stats={gamelog}
+        lead={lead}
+        pos={position}
+      />
+    )
+  })
+
+  return (
+    <>
+      <div className='selected__section-header sticky__column'>
+        <div className='row__group-head'>
+          {position}s vs {opp} Gamelogs
+        </div>
+      </div>
+      <div className='selected__section-header'>
+        <div className='table__cell sticky__column metric game__week'>Wk</div>
+        <div className='table__cell sticky__column sticky__two text'>
+          Player
+        </div>
+        <div className='table__cell metric'>Tm</div>
+        <div className='row__group'>
+          <div className='row__group-head'>Fantasy</div>
+          <div className='row__group-body'>
+            <div className='table__cell metric'>Pts</div>
+            <div className='table__cell metric'>Pts+</div>
+            <div className='table__cell metric'>Rnk</div>
+          </div>
+        </div>
+        <PlayerSelectedRowHeader pos={position} />
+      </div>
+      {rows}
+    </>
+  )
 }
 
 SelectedPlayerMatchupTable.propTypes = {
+  loadPercentiles: PropTypes.func,
   gamelogs: ImmutablePropTypes.list,
   position: PropTypes.string,
   opp: PropTypes.string,
