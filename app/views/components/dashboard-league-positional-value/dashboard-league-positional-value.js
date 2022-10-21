8da7208a5a3a04665bf5df5ac13dac2001509091
mistakia <1823355+mistakia@users.noreply.github.com>
2022-10-21 15:10:04 -0400

    feat: reorder league positional value chart when legend is toggled

--- app/views/components/dashboard-league-positional-value/dashboard-league-positional-value.js
+++ app/views/components/dashboard-league-positional-value/dashboard-league-positional-value.js
@@ -9,117 +9,172 @@ import { constants, sum } from '@common'
 
 HighchartsCustomEvents(Highcharts)
 
-export default class DashboardLeaguePositionalValue extends React.Component {
-  render = () => {
-    const { summary, teams } = this.props
-
-    const series = []
-    for (const position of constants.positions) {
-      const data = []
-      for (const { tid } of summary.sorted_tids) {
-        const rounded = parseInt(summary.rosters[tid][position].toFixed(1), 0)
-        data.push(rounded)
-      }
-      series.push({ name: position, data, borderColor: 'transparent' })
-    }
-
-    const draft_data = []
+export default function DashboardLeaguePositionalValue({ summary, teams }) {
+  const series = []
+  for (const position of constants.positions) {
+    const data = []
     for (const { tid } of summary.sorted_tids) {
-      const draft_value = summary.rosters[tid].DRAFT
-        ? summary.rosters[tid].DRAFT.toFixed(1)
-        : 0
-      const rounded = parseInt(draft_value, 10)
-      draft_data.push(rounded)
+      const rounded = parseInt(summary.rosters[tid][position].toFixed(1), 10)
+      data.push(rounded)
     }
-    series.push({
-      name: 'Draft Picks',
-      data: draft_data,
-      borderColor: 'transparent'
-    })
+    series.push({ name: position, data, borderColor: 'transparent' })
+  }
 
-    const teamNames = []
-    for (const { tid } of summary.sorted_tids) {
-      const id = parseInt(tid, 10)
-      const name = teams.getIn([id, 'name'])
-      teamNames.push(name)
-    }
+  const draft_data = []
+  for (const { tid } of summary.sorted_tids) {
+    const draft_value = summary.rosters[tid].DRAFT
+      ? summary.rosters[tid].DRAFT.toFixed(1)
+      : 0
+    const rounded = parseInt(draft_value, 10)
+    draft_data.push(rounded)
+  }
+  series.push({
+    name: 'Draft Picks',
+    data: draft_data,
+    borderColor: 'transparent'
+  })
 
-    const options = {
-      chart: {
-        type: 'bar',
-        backgroundColor: 'transparent'
-      },
-      title: {
-        text: ''
-      },
-      xAxis: {
-        categories: teamNames,
-        labels: {
-          events: {
-            mouseover: function (e) {
-              const chart = this.chart
-              if (chart.myLabel) {
-                chart.myLabel.destroy()
-                delete chart.myLabel
-              }
-              const values = chart.series
-                .filter((s) => s.visible)
-                .map((s) => s.yData[this.pos])
-              const value = sum(values)
-              chart.myLabel = chart.renderer
-                .label(value, e.layerX + 8, e.layerY, 'rectangle', true)
-                .css({
-                  color: '#FFFFFF'
-                })
-                .attr({
-                  fill: 'rgba(0, 0, 0, 0.75)',
-                  padding: 8,
-                  r: 4
-                })
-                .add()
-                .toFront()
-            },
-            mouseout: function (e) {
-              const chart = this.chart
-              if (chart.myLabel) {
-                chart.myLabel.destroy()
-                delete chart.myLabel
-              }
+  const teamNames = []
+  for (const { tid } of summary.sorted_tids) {
+    const id = parseInt(tid, 10)
+    const name = teams.getIn([id, 'name'])
+    teamNames.push(name)
+  }
+
+  const options = {
+    chart: {
+      type: 'bar',
+      backgroundColor: 'transparent'
+    },
+    title: {
+      text: ''
+    },
+    xAxis: {
+      categories: teamNames,
+      labels: {
+        animate: true,
+        events: {
+          mouseover: function (e) {
+            const chart = this.chart
+            if (chart.myLabel) {
+              chart.myLabel.destroy()
+              delete chart.myLabel
+            }
+            const values = chart.series
+              .filter((s) => s.visible)
+              .map((s) => s.yData[this.pos])
+            const value = sum(values)
+            chart.myLabel = chart.renderer
+              .label(value, e.layerX + 8, e.layerY, 'rectangle', true)
+              .css({
+                color: '#FFFFFF'
+              })
+              .attr({
+                fill: 'rgba(0, 0, 0, 0.75)',
+                padding: 8,
+                r: 4
+              })
+              .add()
+              .toFront()
+          },
+          mouseout: function (e) {
+            const chart = this.chart
+            if (chart.myLabel) {
+              chart.myLabel.destroy()
+              delete chart.myLabel
             }
           }
         }
-      },
-      colors: [
-        '#ff3f3f',
-        '#3f9f3f',
-        '#ffbb3f',
-        '#3f3fff',
-        '#9f9f9f',
-        '#3f3f3f',
-        '#800080'
-      ],
-      yAxis: {
-        min: 0,
-        title: {
-          text: 'Projected Points Added (Team Value)'
-        }
-      },
-      legend: {
-        reversed: true
-      },
-      plotOptions: {
-        series: {
-          stacking: 'normal'
-        }
-      },
-      credits: {
-        enabled: false
-      },
-      series
-    }
+      }
+    },
+    colors: [
+      '#ff3f3f',
+      '#3f9f3f',
+      '#ffbb3f',
+      '#3f3fff',
+      '#9f9f9f',
+      '#3f3f3f',
+      '#800080'
+    ],
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Projected Points Added (Team Value)'
+      }
+    },
+    legend: {
+      reversed: true
+    },
+    plotOptions: {
+      series: {
+        stacking: 'normal',
+        events: {
+          legendItemClick: function (event) {
+            const name = this.name
+            // visible flag not updated yet so use reverse
+            const visible = !this.visible
+            const teams = {}
+            for (const series of this.chart.series) {
+              if (
+                (series.name === name && !visible) ||
+                (series.name !== name && !series.visible)
+              )
+                continue
 
-    return <HighchartsReact highcharts={Highcharts} options={options} />
+              // sum values for each team
+              series.xData.forEach((x_index, index) => {
+                if (!teams[x_index]) {
+                  teams[x_index] = series.yData[index]
+                } else {
+                  teams[x_index] += series.yData[index]
+                }
+              })
+            }
+
+            // sort totals
+            const sorted_teams = Object.entries(teams).sort(
+              (a, b) => b[1] - a[1]
+            )
+
+            const series = []
+            for (const current_series of this.chart.series) {
+              const data = []
+              for (const item of sorted_teams) {
+                const team_index = item[0]
+                const series_value = current_series.yData[team_index]
+                data.push(series_value)
+              }
+              series.push({
+                name: current_series.name,
+                data,
+                borderColor: 'transparent'
+              })
+            }
+
+            const teamNames = []
+            for (const item of sorted_teams) {
+              const team_index = item[0]
+              const name = this.chart.xAxis[0].categories[team_index]
+              teamNames.push(name)
+            }
+
+            this.chart.update({
+              xAxis: {
+                categories: teamNames
+              },
+              series
+            })
+          }
+        }
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    series
   }
+
+  return <HighchartsReact highcharts={Highcharts} options={options} />
 }
 
 DashboardLeaguePositionalValue.propTypes = {
