d3b6f70e182eb547d4446061e91cc429a94e94c1
mistakia <1823355+mistakia@users.noreply.github.com>
2022-10-27 18:52:52 -0400

    feat: use multiple channels for player props

--- config.sample.js
+++ config.sample.js
@@ -18,7 +18,8 @@ module.exports = {
   draftkings_api_v5_url: '',
   caesars_api_v3_url: '',
   fanduel_api_url: '',
-  discord_props_channel_webhook_url: '',
+  discord_props_change_channel_webhook_url: '',
+  discord_props_open_channel_webhook_url: '',
   email: {
     api: '', // sendgrid api
     admin: '',
