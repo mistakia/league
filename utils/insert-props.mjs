0090461c6754265abfcdce201cd6f48133d7d899
mistakia <1823355+mistakia@users.noreply.github.com>
2022-11-03 18:02:12 -0400

    fix: issue with missing active field on props

--- utils/insert-props.mjs
+++ utils/insert-props.mjs
@@ -29,7 +29,7 @@ const handle_over_under_prop = async (prop) => {
 
   const last_prop = props_query[0]
   result.activated =
-    (!last_prop && active) || (active && last_prop.active !== active)
+    (!last_prop && active) || (last_prop.active !== null && active && last_prop.active !== active)
   result.deactivated = !active && last_prop && last_prop.active
 
   // if there is no last prop or if line/odds have changed, insert prop
