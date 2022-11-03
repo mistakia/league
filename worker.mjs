265dc1b3ac200a81df275333e190f93bc2ccf80b
mistakia <1823355+mistakia@users.noreply.github.com>
2022-11-03 13:33:42 -0400

    chore: logging

--- worker.mjs
+++ worker.mjs
@@ -4,6 +4,7 @@ import cron from 'node-cron'
 import import_live_plays from '#jobs/import-live-plays.mjs'
 import finalize_week from '#jobs/finalize-week.mjs'
 
+const log = debug('worker')
 debug.enable('worker,import-plays-nfl,import-plays-ngs,import-live-plays')
 
 // monday
@@ -20,3 +21,5 @@ cron.schedule('*/1 20-23 * 1,2,9-12 4', import_live_plays)
 
 // sunday
 cron.schedule('*/1 9-23 * 1,2,9-12 7', import_live_plays)
+
+log('worker jobs registered')
