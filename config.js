277703d69b53fe9e36f1146718081b6bb9391bfe
mistakia <1823355+mistakia@users.noreply.github.com>
2022-11-04 14:37:21 -0400

    chore: logging

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
-const { argv } = require('yargs')
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+
 const env = process.env.NODE_ENV || 'development'
 const configPath = argv.config || `./config.${env}`
 module.exports = require(configPath)
