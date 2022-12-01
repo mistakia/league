c88de583b4b448bb94d8f9c7f83094c1a6e0f4e6
mistakia <1823355+mistakia@users.noreply.github.com>
2022-12-01 14:01:13 -0500

    feat: version control game data

--- app/core/export/csv.js
+++ app/core/export/csv.js
@@ -1,19 +1,6 @@
 /* global Blob */
 
-function convertToCSV(objArray) {
-  const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray
-  let str = ''
-
-  for (let i = 0; i < array.length; i++) {
-    let line = ''
-    for (const index in array[i]) {
-      if (line !== '') line += ','
-      line += array[i][index]
-    }
-    str += line + '\r\n'
-  }
-  return str
-}
+import { convertToCSV } from '@common'
 
 export function csv({ headers, data, fileName = 'xo-football-export.csv' }) {
   if (headers) {
