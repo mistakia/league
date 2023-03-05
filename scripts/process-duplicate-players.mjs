273f74ae54bc997c00540bfdf8de7bc6842a8bfe
mistakia <1823355+mistakia@users.noreply.github.com>
2023-03-04 22:22:14 -0500

    feat: update process duplicate players

--- scripts/process-duplicate-players.mjs
+++ scripts/process-duplicate-players.mjs
@@ -16,6 +16,7 @@ const processDuplicatePlayers = async () => {
     .count('* as count')
     .groupBy('group_id')
     .having('count', '>', 1)
+    .orderBy('count', 'desc')
 
   log(`${duplicates.length} players had duplicates`)
 
@@ -54,43 +55,39 @@ const processDuplicatePlayers = async () => {
     const { formatted, dob, start } = duplicate_player_row
     const player_rows = await db('player').where({ formatted, dob, start })
 
-    // find earliest player
-    const sorted_player_rows = player_rows.sort((a, b) => a.pid - b.pid)
-    const first_player_row = sorted_player_rows[0]
+    const selected_player_row = player_rows[0]
 
-    const filtered_player_rows = sorted_player_rows
-      .slice(1)
-      .filter((player_row) => {
-        const differences = diff(first_player_row, player_row)
+    const filtered_player_rows = player_rows.slice(1).filter((player_row) => {
+      const differences = diff(selected_player_row, player_row)
 
-        const filtered_differences = differences.filter((difference) => {
-          if (difference.lhs && !difference.rhs) {
-            return false
-          }
+      const filtered_differences = differences.filter((difference) => {
+        if (difference.lhs && !difference.rhs) {
+          return false
+        }
 
-          if (difference.rhs && !difference.lhs) {
-            return false
-          }
+        if (difference.rhs && !difference.lhs) {
+          return false
+        }
 
-          if (accept_diff_props.includes(difference.path[0])) {
-            return false
-          }
-
-          return true
-        })
-
-        // ignore duplicate rows with unexpected differences
-        if (filtered_differences.length) {
-          ignore_count += 1
+        if (accept_diff_props.includes(difference.path[0])) {
           return false
         }
 
         return true
       })
 
+      // ignore duplicate rows with unexpected differences
+      if (filtered_differences.length) {
+        ignore_count += 1
+        return false
+      }
+
+      return true
+    })
+
     for (const player_row of filtered_player_rows) {
       await mergePlayer({
-        update_player_row: first_player_row,
+        update_player_row: selected_player_row,
         remove_player_row: player_row
       })
       deleted_count += 1
@@ -99,6 +96,16 @@ const processDuplicatePlayers = async () => {
 
   log(`deleted ${deleted_count} player rows`)
   log(`ignored ${ignore_count} duplicate player rows`)
+
+  // top 10 duplicate players
+  log(
+    duplicates
+      .slice(0, 10)
+      .map(
+        (player_row) =>
+          `select * from player where formatted = '${player_row.formatted}' and dob = '${player_row.dob}' and start = ${player_row.start};`
+      )
+  )
 }
 
 const main = async () => {
