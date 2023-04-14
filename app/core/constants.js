c96e74eeba8f70a69a42b05e990e491599a06eb7
mistakia <1823355+mistakia@users.noreply.github.com>
2023-04-14 14:22:53 -0400

    chore: get machine ip of API for local dev

--- app/core/constants.js
+++ app/core/constants.js
@@ -1,8 +1,13 @@
-/* global IS_DEV */
+/* global IS_DEV, MACHINE_IP */
+
+// MACHINE_IP is defined in webpack.config.dev.babel.mjs
+// it is used instead of localhost to allow the app to be accessed from
+// other devices on the same network (i.e. testing on a mobile phone)
+
 export const BASE_URL = IS_DEV
-  ? 'http://192.168.1.108:8082/api'
+  ? 'http://' + MACHINE_IP + ':8082/api'
   : 'https://xo.football/api'
-export const WS_URL = IS_DEV ? 'ws://192.168.1.108:8082' : 'wss://xo.football'
+export const WS_URL = IS_DEV ? 'ws://' + MACHINE_IP + ':8082' : 'wss://xo.football'
 export const DOCS_URL =
   'https://api.github.com/repos/mistakia/league/contents/docs'
 
