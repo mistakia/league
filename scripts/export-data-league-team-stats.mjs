7faa80ff1a5c0ee930fea5c422295bf2f2cfc858
mistakia <1823355+mistakia@users.noreply.github.com>
2023-05-05 12:16:39 -0400

    feat: export league matchups and team stats data for version tracking

---
+++ scripts/export-data-league-team-stats.mjs
@@ -0,0 +1,63 @@
+import debug from 'debug'
+import fs from 'fs-extra'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+// import yargs from 'yargs'
+// import { hideBin } from 'yargs/helpers'
+
+import db from '#db'
+import { convertToCSV } from '#common'
+import { isMain } from '#utils'
+
+// const argv = yargs(hideBin(process.argv)).argv
+const log = debug('export_league_team_stats')
+debug.enable('export_league_team_stats')
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const data_path = path.join(__dirname, '../data')
+
+const export_league_team_stats = async () => {
+  const data = await db('team_stats')
+
+  const header = {}
+  for (const field of Object.keys(data[0])) {
+    header[field] = field
+  }
+  const csv_data = [header, ...data]
+  const csv_data_string = JSON.stringify(csv_data)
+  const csv = convertToCSV(csv_data_string)
+
+  const json_file_path = `${data_path}/league_team_stats.json`
+  const csv_file_path = `${data_path}/league_team_stats.csv`
+
+  await fs.writeJson(json_file_path, data, { spaces: 2 })
+  log(`wrote json to ${json_file_path}`)
+
+  await fs.writeFile(csv_file_path, csv)
+  log(`wrote csv to ${csv_file_path}`)
+}
+
+const main = async () => {
+  let error
+  try {
+    await export_league_team_stats()
+  } catch (err) {
+    error = err
+    log(error)
+  }
+
+  // await db('jobs').insert({
+  //   type: constants.jobs.EXAMPLE,
+  //   succ: error ? 0 : 1,
+  //   reason: error ? error.message : null,
+  //   timestamp: Math.round(Date.now() / 1000)
+  // })
+
+  process.exit()
+}
+
+if (isMain(import.meta.url)) {
+  main()
+}
+
+export default export_league_team_stats
