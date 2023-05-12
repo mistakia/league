00891926a9d03b477e7a4a189c04a06e4d3aa0b0
mistakia <1823355+mistakia@users.noreply.github.com>
2023-05-12 09:28:11 -0400

    fix: santuary period calculation

--- common/is-santuary-period.mjs
+++ common/is-santuary-period.mjs
@@ -2,6 +2,13 @@ import { season } from './constants.mjs'
 import getFreeAgentPeriod from './get-free-agent-period.mjs'
 
 export default function isSantuaryPeriod(league) {
+  // If there is no extension date or its before, then it is santuary period
+  if (!league.ext_date) {
+    return true
+  } else if (season.now.isBefore(league.ext_date)) {
+    return true
+  }
+
   if (league.adate) {
     const faPeriod = getFreeAgentPeriod(league.adate)
     if (season.now.isBetween(faPeriod.start, faPeriod.end)) {
