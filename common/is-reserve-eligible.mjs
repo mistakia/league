1e9743c28ec1fa3b636ecb3c7094a4f33d56a53b
mistakia <1823355+mistakia@users.noreply.github.com>
2023-05-21 18:30:57 -0400

    fix: in offseason allow any injury status for injured reserve

--- common/is-reserve-eligible.mjs
+++ common/is-reserve-eligible.mjs
@@ -1,8 +1,11 @@
+import * as constants from './constants.mjs'
+
 export default function isReserveEligible({ status, injury_status } = {}) {
   return (
     (status && status !== 'Active') ||
     injury_status === 'PUP' ||
     injury_status === 'IR' ||
-    injury_status === 'Doubtful'
+    injury_status === 'Doubtful' ||
+    (injury_status && constants.season.week === 0)
   )
 }
