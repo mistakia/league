3019f3435d176686d04cf3c84c7da508ef550ea9
mistakia <1823355+mistakia@users.noreply.github.com>
2023-05-21 14:41:43 -0400

    feat: added percentiles to league team component

--- app/views/components/league-team/index.js
+++ app/views/components/league-team/index.js
@@ -5,8 +5,11 @@ import {
   getTeamById,
   getCurrentLeague,
   getRosterByTeamId,
-  getGroupedPlayersByTeamId
+  getGroupedPlayersByTeamId,
+  isRestrictedFreeAgencyPeriod,
+  isBeforeExtensionDeadline
 } from '@core/selectors'
+import { constants, calculatePercentiles, getExtensionAmount } from '@common'
 import { playerActions } from '@core/players'
 
 import LeagueTeam from './league-team'
@@ -16,12 +19,69 @@ const mapStateToProps = createSelector(
   getRosterByTeamId,
   getTeamById,
   getGroupedPlayersByTeamId,
-  (league, roster, team, players) => ({
+  isRestrictedFreeAgencyPeriod,
+  isBeforeExtensionDeadline,
+  (
     league,
     roster,
-    picks: team.picks,
-    players
-  })
+    team,
+    players,
+    isRestrictedFreeAgencyPeriod,
+    isBeforeExtensionDeadline
+  ) => {
+    const projectionType = constants.season.isRegularSeason ? 'ros' : '0'
+    const items = []
+    players.players
+      .forEach((p) => {
+        const value = p.get('value', 0)
+        const tag = p.get('tag')
+        const isRestrictedFreeAgent = tag === constants.tags.TRANSITION
+        const bid = p.get('bid', 0)
+        const extensions = p.get('extensions', 0)
+        const pos = p.get('pos')
+        const market_salary = p.getIn(['market_salary', '0'], 0)
+        const extendedSalary = getExtensionAmount({
+          pos,
+          tag: isBeforeExtensionDeadline ? tag : constants.tags.REGULAR,
+          extensions,
+          league,
+          value,
+          bid
+        })
+        const savings =
+          !isRestrictedFreeAgencyPeriod || bid || !isRestrictedFreeAgent
+            ? market_salary -
+              (isBeforeExtensionDeadline ? extendedSalary : bid || value)
+            : null
+
+        items.push({
+          savings,
+          market_salary,
+          market_salary_adj: p.get('market_salary_adj', 0),
+          projected_salary: p.getIn(['market_salary', projectionType], 0),
+          vorp_adj: p.getIn(['vorp_adj', projectionType], 0)
+        })
+      })
+
+    const percentiles = calculatePercentiles({
+      items,
+      stats: [
+        'savings',
+        'market_salary',
+        'market_salary_adj',
+        'projected_salary',
+        'vorp_adj'
+      ]
+    })
+
+    return {
+      league,
+      roster,
+      picks: team.picks,
+      players,
+      percentiles
+    }
+  }
 )
 
 const mapDispatchToProps = {
