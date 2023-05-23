11d8d0908a13d87452043454a815358c85e7745f
mistakia <1823355+mistakia@users.noreply.github.com>
2023-05-22 21:33:10 -0400

    feat: added player extension amount and tag savings to team roster page

--- app/views/components/league-team/index.js
+++ app/views/components/league-team/index.js
@@ -38,9 +38,11 @@ const mapStateToProps = createSelector(
       const bid = p.get('bid', 0)
       const extensions = p.get('extensions', 0)
       const pos = p.get('pos')
+      const slot = p.get('slot')
       const market_salary = p.getIn(['market_salary', '0'], 0)
       const extendedSalary = getExtensionAmount({
         pos,
+        slot,
         tag: isBeforeExtensionDeadline ? tag : constants.tags.REGULAR,
         extensions,
         league,
@@ -53,9 +55,37 @@ const mapStateToProps = createSelector(
             (isBeforeExtensionDeadline ? extendedSalary : bid || value)
           : null
 
+      let rookie_tag_savings = null
+      let franchise_tag_savings = null
+
+      if (isBeforeExtensionDeadline) {
+        const regular_extended_salary = getExtensionAmount({
+          pos,
+          slot,
+          tag: constants.tags.REGULAR,
+          extensions,
+          league,
+          value
+        })
+
+        const is_rookie = p.get('start') >= constants.year - 1
+        if (is_rookie) {
+          rookie_tag_savings =
+            Math.max(regular_extended_salary - value, 0) || null
+        }
+
+        franchise_tag_savings =
+          Math.max(
+            regular_extended_salary - league[`f${pos.toLowerCase()}`],
+            0
+          ) || null
+      }
+
       items.push({
         savings,
         market_salary,
+        rookie_tag_savings,
+        franchise_tag_savings,
         market_salary_adj: p.get('market_salary_adj', 0),
         projected_salary: p.getIn(['market_salary', projectionType], 0),
         vorp_adj: p.getIn(['vorp_adj', projectionType], 0)
@@ -67,6 +97,8 @@ const mapStateToProps = createSelector(
       stats: [
         'savings',
         'market_salary',
+        'rookie_tag_savings',
+        'franchise_tag_savings',
         'market_salary_adj',
         'projected_salary',
         'vorp_adj'
