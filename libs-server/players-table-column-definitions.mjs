fcdccdd4205b867d253ba0d070fe339758bd043f
mistakia <1823355+mistakia@users.noreply.github.com>
2023-07-10 21:00:25 -0400

    feat: update players table column definitions

--- libs-server/players-table-column-definitions.mjs
+++ libs-server/players-table-column-definitions.mjs
@@ -108,12 +108,149 @@ const league_player_projection_values_table_alias = ({ params = {} }) => {
   return `league_player_projection_values_${year}_week_${week}_${league_id}`
 }
 
+const player_projected_points_added = {
+  column_name: 'pts_added',
+  table_name: 'league_format_player_projection_values',
+  table_alias: league_format_player_projection_values_table_alias,
+  select_as: stat_in_year_week('points_added'),
+  join: league_format_player_projection_values_join
+}
+
+const player_projected_market_salary = {
+  column_name: 'market_salary',
+  table_name: 'league_format_player_projection_values',
+  table_alias: league_format_player_projection_values_table_alias,
+  select_as: stat_in_year_week('market_salary'),
+  join: league_format_player_projection_values_join
+}
+
+const player_projected_salary_adjusted_points_added = {
+  column_name: 'salary_adj_pts_added',
+  table_name: 'league_player_projection_values',
+  table_alias: league_player_projection_values_table_alias,
+  select_as: stat_in_year_week('salary_adjusted_points_added'),
+  join: league_player_projection_values_join
+}
+
+const player_projected_points = {
+  column_name: 'total',
+  table_name: 'scoring_format_player_projection_points',
+  table_alias: scoring_format_player_projection_points_table_alias,
+  select_as: stat_in_year_week('proj_fan_pts'),
+  join: scoring_format_player_projection_points_join
+}
+
+const player_projected_pass_atts = {
+  column_name: 'pa',
+  table_name: 'projections_index',
+  table_alias: projections_index_table_alias,
+  select_as: stat_in_year_week('proj_pass_atts'),
+  join: projections_index_join
+}
+
+const player_projected_pass_comps = {
+  column_name: 'pc',
+  table_name: 'projections_index',
+  table_alias: projections_index_table_alias,
+  select_as: stat_in_year_week('proj_pass_comps'),
+  join: projections_index_join
+}
+
+const player_projected_pass_yds = {
+  column_name: 'py',
+  table_name: 'projections_index',
+  table_alias: projections_index_table_alias,
+  select_as: stat_in_year_week('proj_pass_yds'),
+  join: projections_index_join
+}
+
+const player_projected_pass_tds = {
+  column_name: 'tdp',
+  table_name: 'projections_index',
+  table_alias: projections_index_table_alias,
+  select_as: stat_in_year_week('proj_pass_tds'),
+  join: projections_index_join
+}
+
+const player_projected_pass_ints = {
+  column_name: 'ints',
+  table_name: 'projections_index',
+  table_alias: projections_index_table_alias,
+  select_as: stat_in_year_week('proj_pass_ints'),
+  join: projections_index_join
+}
+
+const player_projected_rush_atts = {
+  column_name: 'ra',
+  table_name: 'projections_index',
+  table_alias: projections_index_table_alias,
+  select_as: stat_in_year_week('proj_rush_atts'),
+  join: projections_index_join
+}
+
+const player_projected_rush_yds = {
+  column_name: 'ry',
+  table_name: 'projections_index',
+  table_alias: projections_index_table_alias,
+  select_as: stat_in_year_week('proj_rush_yds'),
+  join: projections_index_join
+}
+
+const player_projected_rush_tds = {
+  column_name: 'tdr',
+  table_name: 'projections_index',
+  table_alias: projections_index_table_alias,
+  select_as: stat_in_year_week('proj_rush_tds'),
+  join: projections_index_join
+}
+
+const player_projected_fumbles_lost = {
+  column_name: 'fuml',
+  table_name: 'projections_index',
+  table_alias: projections_index_table_alias,
+  select_as: stat_in_year_week('proj_fumbles_lost'),
+  join: projections_index_join
+}
+
+const player_projected_targets = {
+  column_name: 'trg',
+  table_name: 'projections_index',
+  table_alias: projections_index_table_alias,
+  select_as: stat_in_year_week('proj_trg'),
+  join: projections_index_join
+}
+
+const player_projected_recs = {
+  column_name: 'rec',
+  table_name: 'projections_index',
+  table_alias: projections_index_table_alias,
+  select_as: stat_in_year_week('proj_rec'),
+  join: projections_index_join
+}
+
+const player_projected_rec_yds = {
+  column_name: 'recy',
+  table_name: 'projections_index',
+  table_alias: projections_index_table_alias,
+  select_as: stat_in_year_week('proj_rec_yds'),
+  join: projections_index_join
+}
+
+const player_projected_rec_tds = {
+  column_name: 'tdrec',
+  table_name: 'projections_index',
+  table_alias: projections_index_table_alias,
+  select_as: stat_in_year_week('proj_rec_tds'),
+  join: projections_index_join
+}
+
 export default {
   player_name: {
     table_name: 'player',
-    column_name: 'name'
+    select: ({ query }) => {
+      query.select('player.fname', 'player.lname')
+    }
   },
-  player_league_roster_status: {},
   player_league_salary: {
     column_name: 'value',
     table_name: 'transactions',
@@ -142,141 +279,10 @@ export default {
     }
   },
 
-  player_week_projected_market_salary: {
-    column_name: 'market_salary',
-    table_name: 'league_format_player_projection_values',
-    table_alias: league_format_player_projection_values_table_alias,
-    select_as: stat_in_year_week('market_salary'),
-    join: league_format_player_projection_values_join
-  },
-  player_week_projected_inflation_adjusted_market_salary: {
-    column_name: 'market_salary_adj',
-    table_name: 'league_player_projection_values',
-    table_alias: league_player_projection_values_table_alias,
-    select_as: stat_in_year_week('inflation_adjusted_market_salary'),
-    join: league_player_projection_values_join
-  },
-  player_week_projected_salary_adjusted_points_added: {
-    column_name: 'salary_adj_pts_added',
-    table_name: 'league_player_projection_values',
-    table_alias: league_player_projection_values_table_alias,
-    select_as: stat_in_year_week('salary_adjusted_points_added'),
-    join: league_player_projection_values_join
-  },
+  player_week_projected_market_salary: player_projected_market_salary,
+  player_season_projected_market_salary: player_projected_market_salary,
+  player_rest_of_season_projected_market_salary: player_projected_market_salary,
 
-  player_week_projected_points_added: {
-    column_name: 'pts_added',
-    table_name: 'league_format_player_projection_values',
-    table_alias: league_format_player_projection_values_table_alias,
-    select_as: stat_in_year_week('points_added'),
-    join: league_format_player_projection_values_join
-  },
-  player_week_projected_points: {
-    column_name: 'total',
-    table_name: 'scoring_format_player_projection_points',
-    table_alias: scoring_format_player_projection_points_table_alias,
-    select_as: stat_in_year_week('proj_fan_pts'),
-    join: scoring_format_player_projection_points_join
-  },
-  player_week_projected_pass_atts: {
-    column_name: 'pa',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_pass_atts'),
-    join: projections_index_join
-  },
-  player_week_projected_pass_comps: {
-    column_name: 'pc',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_pass_comps'),
-    join: projections_index_join
-  },
-  player_week_projected_pass_yds: {
-    column_name: 'py',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_pass_yds'),
-    join: projections_index_join
-  },
-  player_week_projected_pass_tds: {
-    column_name: 'tdp',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_pass_tds'),
-    join: projections_index_join
-  },
-  player_week_projected_pass_ints: {
-    column_name: 'ints',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_pass_ints'),
-    join: projections_index_join
-  },
-  player_week_projected_rush_atts: {
-    column_name: 'ra',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_rush_atts'),
-    join: projections_index_join
-  },
-  player_week_projected_rush_yds: {
-    column_name: 'ry',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_rush_yds'),
-    join: projections_index_join
-  },
-  player_week_projected_rush_tds: {
-    column_name: 'tdr',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_rush_tds'),
-    join: projections_index_join
-  },
-  player_week_projected_fumbles_lost: {
-    column_name: 'fuml',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_fumbles_lost'),
-    join: projections_index_join
-  },
-  player_week_projected_targets: {
-    column_name: 'trg',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_trg'),
-    join: projections_index_join
-  },
-  player_week_projected_recs: {
-    column_name: 'rec',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_rec'),
-    join: projections_index_join
-  },
-  player_week_projected_rec_yds: {
-    column_name: 'recy',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_rec_yds'),
-    join: projections_index_join
-  },
-  player_week_projected_rec_tds: {
-    column_name: 'tdrec',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_rec_tds'),
-    join: projections_index_join
-  },
-
-  player_season_projected_market_salary: {
-    column_name: 'market_salary',
-    table_name: 'league_format_player_projection_values',
-    table_alias: league_format_player_projection_values_table_alias,
-    select_as: stat_in_year_week('market_salary'),
-    join: league_format_player_projection_values_join
-  },
   player_season_projected_inflation_adjusted_market_salary: {
     column_name: 'market_salary_adj',
     table_name: 'league_player_projection_values',
@@ -284,121 +290,73 @@ export default {
     select_as: stat_in_year_week('inflation_adjusted_market_salary'),
     join: league_player_projection_values_join
   },
-  player_season_projected_points_added: {
-    column_name: 'pts_added',
-    table_name: 'league_format_player_projection_values',
-    table_alias: league_format_player_projection_values_table_alias,
-    select_as: stat_in_year_week('points_added'),
-    join: league_format_player_projection_values_join
-  },
-  player_season_projected_points: {
-    column_name: 'total',
-    table_name: 'scoring_format_player_projection_points',
-    table_alias: scoring_format_player_projection_points_table_alias,
-    select_as: stat_in_year_week('proj_fan_pts'),
-    join: scoring_format_player_projection_points_join
-  },
-  player_season_projected_salary_adjusted_points_added: {
-    column_name: 'salary_adj_pts_added',
-    table_name: 'league_player_projection_values',
-    table_alias: league_player_projection_values_table_alias,
-    select_as: stat_in_year_week('salary_adjusted_points_added'),
-    join: league_player_projection_values_join
-  },
-  player_season_projected_pass_atts: {
-    column_name: 'pa',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_pass_atts'),
-    join: projections_index_join
-  },
-  player_season_projected_pass_comps: {
-    column_name: 'pc',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_pass_comps'),
-    join: projections_index_join
-  },
-  player_season_projected_pass_yds: {
-    column_name: 'py',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_pass_yds'),
-    join: projections_index_join
-  },
-  player_season_projected_pass_tds: {
-    column_name: 'tdp',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_pass_tds'),
-    join: projections_index_join
-  },
-  player_season_projected_pass_ints: {
-    column_name: 'ints',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_pass_ints'),
-    join: projections_index_join
-  },
-  player_season_projected_rush_atts: {
-    column_name: 'ra',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_rush_atts'),
-    join: projections_index_join
-  },
-  player_season_projected_rush_yds: {
-    column_name: 'ry',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_rush_yds'),
-    join: projections_index_join
-  },
-  player_season_projected_rush_tds: {
-    column_name: 'tdr',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_rush_tds'),
-    join: projections_index_join
-  },
-  player_season_projected_fumbles_lost: {
-    column_name: 'fuml',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_fum_lost'),
-    join: projections_index_join
-  },
-  player_season_projected_targets: {
-    column_name: 'trg',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_trg'),
-    join: projections_index_join
-  },
-  player_season_projected_recs: {
-    column_name: 'rec',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_recs'),
-    join: projections_index_join
-  },
-  player_season_projected_rec_yds: {
-    column_name: 'recy',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_rec_yds'),
-    join: projections_index_join
-  },
-  player_season_projected_rec_tds: {
-    column_name: 'tdrec',
-    table_name: 'projections_index',
-    table_alias: projections_index_table_alias,
-    select_as: stat_in_year_week('proj_rec_tds'),
-    join: projections_index_join
-  },
 
-  player_rest_of_season_points_added: {},
-  player_rest_of_season_projected_points: {},
+  player_week_projected_salary_adjusted_points_added:
+    player_projected_salary_adjusted_points_added,
+  player_season_projected_salary_adjusted_points_added:
+    player_projected_salary_adjusted_points_added,
+  player_rest_of_season_projected_salary_adjusted_points_added:
+    player_projected_salary_adjusted_points_added,
+
+  player_week_projected_points_added: player_projected_points_added,
+  player_season_projected_points_added: player_projected_points_added,
+  player_rest_of_season_projected_points_added: player_projected_points_added,
+
+  player_week_projected_points: player_projected_points,
+  player_season_projected_points: player_projected_points,
+  player_rest_of_season_projected_points: player_projected_points,
+
+  player_week_projected_pass_atts: player_projected_pass_atts,
+  player_season_projected_pass_atts: player_projected_pass_atts,
+  player_rest_of_season_projected_pass_atts: player_projected_pass_atts,
+
+  player_week_projected_pass_comps: player_projected_pass_comps,
+  player_season_projected_pass_comps: player_projected_pass_comps,
+  player_rest_of_season_projected_pass_comps: player_projected_pass_comps,
+
+  player_week_projected_pass_yds: player_projected_pass_yds,
+  player_season_projected_pass_yds: player_projected_pass_yds,
+  player_rest_of_season_projected_pass_yds: player_projected_pass_yds,
+
+  player_week_projected_pass_tds: player_projected_pass_tds,
+  player_season_projected_pass_tds: player_projected_pass_tds,
+  player_rest_of_season_projected_pass_tds: player_projected_pass_tds,
+
+  player_week_projected_pass_ints: player_projected_pass_ints,
+  player_season_projected_pass_ints: player_projected_pass_ints,
+  player_rest_of_season_projected_pass_ints: player_projected_pass_ints,
+
+  player_week_projected_rush_atts: player_projected_rush_atts,
+  player_season_projected_rush_atts: player_projected_rush_atts,
+  player_rest_of_season_projected_rush_atts: player_projected_rush_atts,
+
+  player_week_projected_rush_yds: player_projected_rush_yds,
+  player_season_projected_rush_yds: player_projected_rush_yds,
+  player_rest_of_season_projected_rush_yds: player_projected_rush_yds,
+
+  player_week_projected_rush_tds: player_projected_rush_tds,
+  player_season_projected_rush_tds: player_projected_rush_tds,
+  player_rest_of_season_projected_rush_tds: player_projected_rush_tds,
+
+  player_week_projected_fumbles_lost: player_projected_fumbles_lost,
+  player_season_projected_fumbles_lost: player_projected_fumbles_lost,
+  player_rest_of_season_projected_fumbles_lost: player_projected_fumbles_lost,
+
+  player_week_projected_targets: player_projected_targets,
+  player_season_projected_targets: player_projected_targets,
+  player_rest_of_season_projected_targets: player_projected_targets,
+
+  player_week_projected_recs: player_projected_recs,
+  player_season_projected_recs: player_projected_recs,
+  player_rest_of_season_projected_recs: player_projected_recs,
+
+  player_week_projected_rec_yds: player_projected_rec_yds,
+  player_season_projected_rec_yds: player_projected_rec_yds,
+  player_rest_of_season_projected_rec_yds: player_projected_rec_yds,
+
+  player_week_projected_rec_tds: player_projected_rec_tds,
+  player_season_projected_rec_tds: player_projected_rec_tds,
+  player_rest_of_season_projected_rec_tds: player_projected_rec_tds,
 
   week_opponent_abbreviation: {},
   week_opponent_points_allowed_over_average: {}
