406b9dc271d70834f0e0460803b6f39871bf578c
mistakia <1823355+mistakia@users.noreply.github.com>
2023-07-24 01:36:07 -0400

    feat: user table view post and delete api endpoints

--- api/routes/table_views.mjs
+++ api/routes/table_views.mjs
@@ -1,5 +1,7 @@
 import express from 'express'
 
+import { validators } from '#libs-server'
+
 const router = express.Router()
 
 router.get('/?', async (req, res) => {
@@ -10,6 +12,119 @@ router.get('/?', async (req, res) => {
   } catch (error) {
     logger(error)
     res.status(500).send({ error: error.toString() })
+  }
+})
+
+router.post('/?', async (req, res) => {
+  const { log, db } = req.app.locals
+  try {
+    const { view_id, view_name, table_state, view_description } = req.body
+
+    if (!req.auth || !req.auth.userId) {
+      return res.status(401).send({ error: 'invalid userId' })
+    }
+
+    const user_id = req.auth ? req.auth.userId : null
+
+    if (!validators.view_name_validator(view_name)) {
+      return res.status(400).send({ error: 'invalid view_name' })
+    }
+
+    if (!validators.view_description_validator(view_description)) {
+      return res.status(400).send({ error: 'invalid view_description' })
+    }
+
+    if (!validators.table_state_validator(table_state)) {
+      return res.status(400).send({ error: 'invalid table_state' })
+    }
+
+    if (view_id) {
+      const view = await db('user_table_views')
+        .where({
+          view_id
+        })
+        .first()
+
+      if (!view) {
+        return res.status(400).send({ error: 'invalid view_id' })
+      }
+
+      if (view.user_id !== user_id) {
+        return res.status(401).send({ error: 'invalid userId' })
+      }
+
+      await db('database_table_views')
+        .where({
+          view_id,
+          user_id
+        })
+        .update({
+          view_name,
+          view_description,
+          table_state: JSON.stringify(table_state)
+        })
+    } else {
+      await db('user_table_views')
+        .insert({
+          view_name,
+          view_description,
+          table_state: JSON.stringify(table_state),
+          user_id
+        })
+        .onConflict(['view_name', 'user_id'])
+        .merge()
+    }
+
+    const view = await db('user_table_views')
+      .where({
+        view_name,
+        user_id
+      })
+      .first()
+
+    res.status(200).send(view)
+  } catch (err) {
+    log(err)
+    res.status(500).send({ error: err.toString() })
+  }
+})
+
+router.delete('/:view_id', async (req, res) => {
+  const { log, db } = req.app.locals
+  try {
+    const { view_id } = req.params
+
+    if (!req.auth || !req.auth.userId) {
+      return res.status(401).send({ error: 'invalid userId' })
+    }
+
+    const user_id = req.auth ? req.auth.userId : null
+
+    const view = await db('user_table_views')
+      .where({
+        view_id
+      })
+      .first()
+
+    if (!view) {
+      return res.status(400).send({ error: 'invalid view_id' })
+    }
+
+    if (view.user_id !== user_id) {
+      return res.status(401).send({ error: 'invalid userId' })
+    }
+
+    await db('user_table_views')
+      .where({
+        view_id,
+        user_id
+      })
+      .del()
+
+    res.status(200).send({ success: true })
+  } catch (err) {
+    log(err)
+    res.status(500).send({ error: err.toString() })
   }
 })
 
--- libs-server/validators.mjs
+++ libs-server/validators.mjs
@@ -33,3 +33,91 @@ const seas_type_schema = {
 }
 
 export const seas_type_validator = v.compile(seas_type_schema)
+
+const view_name_schema = {
+  $$root: true,
+  type: 'string',
+  min: 1,
+  max: 30
+}
+export const view_name_validator = v.compile(view_name_schema)
+
+const view_description_schema = {
+  $$root: true,
+  type: 'string',
+  min: 1,
+  max: 400
+}
+export const view_description_validator = v.compile(view_description_schema)
+
+const sort_schema = {
+  type: 'array',
+  items: {
+    type: 'object',
+    props: {
+      id: { type: 'string' },
+      desc: { type: 'boolean' }
+    }
+  }
+}
+export const sort_validator = v.compile(sort_schema)
+
+const columns_schema = {
+  type: 'array',
+  items: {
+    type: 'object',
+    props: {
+      column_name: { type: 'string' },
+      table_name: { type: 'string' }
+    }
+  }
+}
+export const columns_validator = v.compile(columns_schema)
+
+const where_operator_schema = {
+  type: 'string',
+  enum: [
+    '=',
+    '!=',
+    '>',
+    '>=',
+    '<',
+    '<=',
+    'LIKE',
+    'NOT LIKE',
+    'IS NULL',
+    'IS NOT NULL',
+    'IN',
+    'NOT IN'
+  ]
+}
+
+const where_schema = {
+  type: 'array',
+  items: {
+    type: 'object',
+    props: {
+      column_name: { type: 'string' },
+      operator: where_operator_schema,
+      value: { type: 'string' }
+    }
+  }
+}
+
+export const where_validator = v.compile(where_schema)
+
+const offset_schema = {
+  type: 'number',
+  positive: true,
+  integer: true,
+  $$root: true
+}
+export const offset_validator = v.compile(offset_schema)
+
+const table_state_schema = {
+  offset: offset_schema,
+  sort: sort_schema,
+  columns: columns_schema,
+  where: where_schema
+}
+export const table_state_validator = v.compile(table_state_schema)
