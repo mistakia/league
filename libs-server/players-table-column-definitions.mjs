1a29f323852d554364da40cb51114523564711c8
mistakia <1823355+mistakia@users.noreply.github.com>
2023-08-24 13:57:26 -0400

    fix: players table column definitions

--- libs-server/players-table-column-definitions.mjs
+++ libs-server/players-table-column-definitions.mjs
@@ -624,7 +624,7 @@ export default {
     select: ({ query, params = {} }) => {
       query.select(
         db.raw(
-          'CASE WHEN SUM(CASE WHEN nfl_plays.psr_pid = player.pid THEN 1 ELSE 0 END) > 0 THEN ROUND(100.0 * SUM(CASE WHEN nfl_plays.psr_pid = player.pid AND nfl_plays.psr = 1 THEN 1 ELSE 0 END) / SUM(CASE WHEN nfl_plays.psr_pid = player.pid THEN 1 ELSE 0 END), 2) ELSE 0 END AS qb_press_pct'
+          'CASE WHEN SUM(CASE WHEN nfl_plays.psr_pid = player.pid THEN 1 ELSE 0 END) > 0 THEN ROUND(100.0 * SUM(CASE WHEN nfl_plays.psr_pid = player.pid AND nfl_plays.qb_pressure = 1 THEN 1 ELSE 0 END) / SUM(CASE WHEN nfl_plays.psr_pid = player.pid THEN 1 ELSE 0 END), 2) ELSE 0 END AS qb_press_pct'
         )
       )
     },
