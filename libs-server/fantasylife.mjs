0efcadc9ca5be831b8a0432989723458d72394ed
mistakia <1823355+mistakia@users.noreply.github.com>
2023-09-07 13:44:08 -0400

    feat: import fantasylife projections scripts

---
+++ libs-server/fantasylife.mjs
@@ -0,0 +1,80 @@
+import fetch from 'node-fetch'
+import queryString from 'query-string'
+import debug from 'debug'
+
+import config from '#config'
+
+const log = debug('fantasylife')
+
+export const get_projections = async ({ table_id, query_params = {} } = {}) => {
+  if (!table_id) {
+    throw new Error('table_id is required')
+  }
+
+  const params = {
+    page: 1,
+    perPage: 500,
+    ...query_params
+  }
+
+  try {
+    const url = `${
+      config.fantasylife_api
+    }/datatables/table-contents/${table_id}/ajax?${queryString.stringify(
+      params
+    )}`
+    log(url)
+    const res = await fetch(url)
+    const data = await res.json()
+
+    if (!data.total) {
+      return null
+    }
+
+    const result = []
+
+    const player_field_id = data.fields.find(
+      (f) => f.label === 'Player' && f.type === 'player'
+    )?.key
+    const rush_att_field_id = data.fields.find((f) => f.label === 'Ru Att')?.key
+    const rush_yds_field_id = data.fields.find((f) => f.label === 'Ru Yds')?.key
+    const rush_tds_field_id = data.fields.find((f) => f.label === 'Ru TDs')?.key
+    const targets_field_id = data.fields.find((f) => f.label === 'Tgt')?.key
+    const rec_field_id = data.fields.find((f) => f.label === 'Rec')?.key
+    const rec_yds_field_id = data.fields.find((f) => f.label === 'Rec Yds')?.key
+    const rec_tds_field_id = data.fields.find((f) => f.label === 'Rec TDs')?.key
+    const pass_att_field_id = data.fields.find((f) => f.label === 'P Att')?.key
+    const pass_comp_field_id = data.fields.find(
+      (f) => f.label === 'P Comp'
+    )?.key
+    const pass_yds_field_id = data.fields.find((f) => f.label === 'P Yds')?.key
+    const pass_tds_field_id = data.fields.find((f) => f.label === 'P TDs')?.key
+    const pass_ints_field_id = data.fields.find((f) => f.label === 'INT')?.key
+
+    for (const item of data.items) {
+      result.push({
+        fantasylife_player_id: item[player_field_id]?.data.fa_id,
+        position: item[player_field_id]?.data.position.name,
+        name: item[player_field_id]?.data.name,
+        nfl_team: item[player_field_id]?.data.team.abbr,
+        rush_att: Number(item[rush_att_field_id]) || null,
+        rush_yds: Number(item[rush_yds_field_id]) || null,
+        rush_tds: Number(item[rush_tds_field_id]) || null,
+        targets: Number(item[targets_field_id]) || null,
+        rec: Number(item[rec_field_id]) || null,
+        rec_yds: Number(item[rec_yds_field_id]) || null,
+        rec_tds: Number(item[rec_tds_field_id]) || null,
+        pass_att: Number(item[pass_att_field_id]) || null,
+        pass_comp: Number(item[pass_comp_field_id]) || null,
+        pass_yds: Number(item[pass_yds_field_id]) || null,
+        pass_tds: Number(item[pass_tds_field_id]) || null,
+        pass_ints: Number(item[pass_ints_field_id]) || null
+      })
+    }
+
+    return result
+  } catch (err) {
+    log(err)
+    return null
+  }
+}
---
+++ scripts/import-fantasylife-projections.mjs
@@ -0,0 +1,135 @@
+import debug from 'debug'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+import db from '#db'
+import { constants } from '#libs-shared'
+import { isMain, fantasylife, getPlayer } from '#libs-server'
+
+const argv = yargs(hideBin(process.argv)).argv
+const log = debug('import-fantasylife-projections')
+debug.enable('import-fantasylife-projections,fantasylife,get-player')
+
+const format_projection = (item) => ({
+  pa: item.pass_att,
+  pc: item.pass_comp,
+  py: item.pass_yds,
+  ints: item.pass_ints,
+  tdp: item.pass_tds,
+  ra: item.rush_att,
+  ry: item.rush_yds,
+  tdr: item.rush_tds,
+  trg: item.targets,
+  rec: item.rec,
+  recy: item.rec_yds,
+  tdrec: item.rec_tds
+})
+
+const import_fantasylife_projections = async ({
+  table_id,
+  dry_run = false
+} = {}) => {
+  if (!table_id) {
+    throw new Error('table_id is required')
+  }
+
+  if (constants.season.week > constants.season.nflFinalWeek) {
+    return
+  }
+
+  const timestamp = new Date()
+  const inserts = []
+  const missing = []
+  const position_ids = [
+    196, // QB
+    208 // FLEX
+    // TODO 211  // DST
+  ]
+
+  for (const position_id of position_ids) {
+    const query_params = {
+      data_filter_data: `{"58":190,"61":${position_id}}`,
+      scoring_system_data: '{"id":185}'
+    }
+    const data = await fantasylife.get_projections({ table_id, query_params })
+    log(data[0])
+
+    if (!data) {
+      continue
+    }
+
+    for (const item of data) {
+      let player_row
+      const params = {
+        name: item.name,
+        pos: item.position,
+        team: item.nfl_team
+      }
+
+      try {
+        player_row = await getPlayer(params)
+        if (!player_row) {
+          missing.push(params)
+          continue
+        }
+      } catch (err) {
+        log(err)
+        missing.push(params)
+        continue
+      }
+
+      const data = format_projection(item)
+      inserts.push({
+        pid: player_row.pid,
+        year: constants.season.year,
+        week: constants.season.week,
+        sourceid: constants.sources.FANTASYLIFE,
+        ...data
+      })
+    }
+  }
+
+  log(`Could not locate ${missing.length} players`)
+  missing.forEach((m) =>
+    log(`could not find player: ${m.name} / ${m.pos} / ${m.team}`)
+  )
+
+  if (dry_run) {
+    log(inserts[0])
+    return
+  }
+
+  if (inserts.length) {
+    log(`Inserting ${inserts.length} projections into database`)
+    await db('projections_index').insert(inserts).onConflict().merge()
+    await db('projections').insert(inserts.map((i) => ({ ...i, timestamp })))
+  }
+}
+
+const main = async () => {
+  let error
+  try {
+    await import_fantasylife_projections({
+      table_id: argv.table_id,
+      dry_run: argv.dry
+    })
+  } catch (err) {
+    error = err
+    log(error)
+  }
+
+  // await db('jobs').insert({
+  //   type: constants.jobs.EXAMPLE,
+  //   succ: error ? 0 : 1,
+  //   reason: error ? error.message : null,
+  //   timestamp: Math.round(Date.now() / 1000)
+  // })
+
+  process.exit()
+}
+
+if (isMain(import.meta.url)) {
+  main()
+}
+
+export default import_fantasylife_projections
