40bbe14bb768944a9cc9e2a00d6793c1a4cc7f28
mistakia <1823355+mistakia@users.noreply.github.com>
2023-09-12 00:19:32 -0400

    fix: import nflfastr plays

--- scripts/import-plays-nflfastr.mjs
+++ scripts/import-plays-nflfastr.mjs
@@ -10,11 +10,11 @@ import { hideBin } from 'yargs/helpers'
 
 import db from '#db'
 import { constants, fixTeam } from '#libs-shared'
-import { isMain, readCSV, getPlay } from '#libs-server'
+import { isMain, readCSV, getPlay, update_play } from '#libs-server'
 
 const argv = yargs(hideBin(process.argv)).argv
 const log = debug('import-nflfastr-plays')
-debug.enable('import-nflfastr-plays')
+debug.enable('import-nflfastr-plays,update-play')
 
 /* const unzip = (source, destination) =>
  *   new Promise((resolve, reject) => {
@@ -30,132 +30,166 @@ debug.enable('import-nflfastr-plays')
  *   })
  *  */
 
-const formatPlay = (play) => ({
-  ydl_100: parseInt(play.yardline_100, 10) || null,
+const format_number = (num) => {
+  if (num === null || num === undefined) {
+    return null
+  }
 
-  drive_seq: parseInt(play.fixed_drive, 10) || null,
+  const n = Number(num)
+
+  if (Number.isNaN(n)) {
+    return null
+  }
+
+  if (Number.isInteger(n)) {
+    return n
+  }
+
+  return n.toFixed(12)
+}
+
+const format_play = (play) => ({
+  ydl_100: format_number(play.yardline_100),
+
+  drive_seq: format_number(play.fixed_drive),
   drive_result: play.fixed_drive_result,
   drive_top: play.drive_time_of_possession,
-  drive_fds: parseInt(play.drive_first_downs, 10) || null,
-  drive_inside20: Boolean(play.drive_inside20),
-  drive_score: Boolean(play.drive_ended_with_score),
-  drive_start_qtr: parseInt(play.drive_quarter_start, 10) || null,
-  drive_end_qtr: parseInt(play.drive_quarter_end, 10) || null,
-  drive_yds_penalized: parseInt(play.drive_yards_penalized, 10) || null,
+  drive_fds: format_number(play.drive_first_downs),
+  drive_inside20: format_number(Boolean(play.drive_inside20)),
+  drive_score: format_number(Boolean(play.drive_ended_with_score)),
+  drive_start_qtr: format_number(play.drive_quarter_start),
+  drive_end_qtr: format_number(play.drive_quarter_end),
+  drive_yds_penalized: format_number(play.drive_yards_penalized),
   drive_start_transition: play.drive_start_transition,
   drive_end_transition: play.drive_end_transition,
   drive_game_clock_start: play.drive_game_clock_start,
   drive_game_clock_end: play.drive_game_clock_end,
   drive_start_ydl: play.drive_start_yard_line,
   drive_end_ydl: play.drive_end_yard_line,
-  drive_start_play_id: play.drive_play_id_started || null,
-  drive_end_play_id: play.drive_play_id_ended || null,
+  drive_start_play_id: format_number(play.drive_play_id_started),
+  drive_end_play_id: format_number(play.drive_play_id_ended),
 
-  series_seq: parseInt(play.series, 10) || null,
-  series_suc: Boolean(play.series_success),
+  series_seq: format_number(play.series),
+  series_suc: format_number(Boolean(play.series_success)),
   series_result: play.series_result,
 
   game_clock_end: play.end_clock_time,
-  sec_rem_qtr: parseInt(play.quarter_seconds_remaining, 10) || null,
-  sec_rem_half: parseInt(play.half_seconds_remaining, 10) || null,
-  sec_rem_gm: parseInt(play.game_seconds_remaining, 10) || null,
+  sec_rem_qtr: format_number(play.quarter_seconds_remaining),
+  sec_rem_half: format_number(play.half_seconds_remaining),
+  sec_rem_gm: format_number(play.game_seconds_remaining),
 
-  fum: Boolean(play.fumble),
-  incomp: Boolean(play.incomplete_pass),
-  touchback: Boolean(play.touchback),
-  safety: Boolean(play.safety),
-  special: Boolean(play.special),
-  oob: Boolean(play.out_of_bounds),
-  tfl: Boolean(play.tackled_for_loss),
-  rush: Boolean(play.rush_attempt),
-  pass: Boolean(play.pass_attempt),
-  solo_tk: Boolean(play.solo_tackle),
-  assist_tk: Boolean(play.assist_tackle),
+  fum: format_number(Boolean(play.fumble)),
+  incomp: format_number(Boolean(play.incomplete_pass)),
+  touchback: format_number(Boolean(play.touchback)),
+  safety: format_number(Boolean(play.safety)),
+  special: format_number(Boolean(play.special)),
+  oob: format_number(Boolean(play.out_of_bounds)),
+  tfl: format_number(Boolean(play.tackled_for_loss)),
+  rush: format_number(Boolean(play.rush_attempt)),
+  pass: format_number(Boolean(play.pass_attempt)),
+  solo_tk: format_number(Boolean(play.solo_tackle)),
+  assist_tk: format_number(Boolean(play.assist_tackle)),
 
   pen_team: play.penalty_team ? fixTeam(play.penalty_team) : null,
-  pen_yds: parseInt(play.penalty_yards, 10) || null,
+  pen_yds: format_number(play.penalty_yards),
 
   pass_td: play.pass_touchdown ? play.pass_touchdown === '1' : null,
   rush_td: play.rush_touchdown ? play.rush_touchdown === '1' : null,
 
-  pass_yds: parseInt(play.passing_yards, 10) || null,
-  recv_yds: parseInt(play.receiving_yards, 10) || null,
-  rush_yds: parseInt(play.rushing_yards, 10) || null,
+  pass_yds: format_number(play.passing_yards),
+  recv_yds: format_number(play.receiving_yards),
+  rush_yds: format_number(play.rushing_yards),
 
-  qbd: Boolean(play.qb_dropback),
-  qbk: Boolean(play.qb_kneel),
-  qbs: Boolean(play.qb_spike),
+  qbd: format_number(Boolean(play.qb_dropback)),
+  qbk: format_number(Boolean(play.qb_kneel)),
+  qbs: format_number(Boolean(play.qb_spike)),
 
   run_location: play.run_location,
   run_gap: play.run_gap,
 
-  fd_rush: Boolean(play.first_down_rush),
-  fd_pass: Boolean(play.first_down_pass),
-  fd_penalty: Boolean(play.first_down_penalty),
+  fd_rush: format_number(Boolean(play.first_down_rush)),
+  fd_pass: format_number(Boolean(play.first_down_pass)),
+  fd_penalty: format_number(Boolean(play.first_down_penalty)),
 
-  third_down_converted: Boolean(play.third_down_converted),
-  third_down_failed: Boolean(play.third_down_failed),
-  fourth_down_converted: Boolean(play.fourth_down_converted),
-  fourth_down_failed: Boolean(play.fourth_down_failed),
+  third_down_converted: format_number(Boolean(play.third_down_converted)),
+  third_down_failed: format_number(Boolean(play.third_down_failed)),
+  fourth_down_converted: format_number(Boolean(play.fourth_down_converted)),
+  fourth_down_failed: format_number(Boolean(play.fourth_down_failed)),
 
-  ep: parseFloat(play.ep) || null,
-  epa: parseFloat(play.epa) || null,
-  ep_succ: Boolean(play.success) || null,
+  ep: format_number(play.ep),
+  epa: format_number(play.epa),
+  ep_succ: format_number(Boolean(play.success)),
 
-  total_home_epa: parseFloat(play.total_home_epa) || null,
-  total_away_epa: parseFloat(play.total_away_epa) || null,
-  total_home_rush_epa: parseFloat(play.total_home_rush_epa) || null,
-  total_away_rush_epa: parseFloat(play.total_away_rush_epa) || null,
-  total_home_pass_epa: parseFloat(play.total_home_pass_epa) || null,
-  total_away_pass_epa: parseFloat(play.total_away_pass_epa) || null,
+  total_home_epa: format_number(play.total_home_epa),
+  total_away_epa: format_number(play.total_away_epa),
+  total_home_rush_epa: format_number(play.total_home_rush_epa),
+  total_away_rush_epa: format_number(play.total_away_rush_epa),
+  total_home_pass_epa: format_number(play.total_home_pass_epa),
+  total_away_pass_epa: format_number(play.total_away_pass_epa),
 
-  qb_epa: parseFloat(play.qb_epa) || null,
-  air_epa: parseFloat(play.air_epa) || null,
-  yac_epa: parseFloat(play.yac_epa) || null,
-  comp_air_epa: parseFloat(play.comp_air_epa) || null,
-  comp_yac_epa: parseFloat(play.comp_yac_epa) || null,
-  xyac_epa: parseFloat(play.xyac_epa) || null,
+  qb_epa: format_number(play.qb_epa),
+  air_epa: format_number(play.air_epa),
+  yac_epa: format_number(play.yac_epa),
+  comp_air_epa: format_number(play.comp_air_epa),
+  comp_yac_epa: format_number(play.comp_yac_epa),
+  xyac_epa: format_number(play.xyac_epa),
 
-  total_home_comp_air_epa: parseFloat(play.total_home_comp_air_epa) || null,
-  total_away_comp_air_epa: parseFloat(play.total_away_comp_air_epa) || null,
-  total_home_comp_yac_epa: parseFloat(play.total_home_comp_yac_epa) || null,
-  total_away_comp_yac_epa: parseFloat(play.total_away_comp_yac_epa) || null,
-  total_home_raw_air_epa: parseFloat(play.total_home_raw_air_epa) || null,
-  total_away_raw_air_epa: parseFloat(play.total_away_raw_air_epa) || null,
-  total_home_raw_yac_epa: parseFloat(play.total_home_raw_yac_epa) || null,
-  total_away_raw_yac_epa: parseFloat(play.total_away_raw_yac_epa) || null,
+  total_home_comp_air_epa:
+    format_number(play.total_home_comp_air_epa),
+  total_away_comp_air_epa:
+    format_number(play.total_away_comp_air_epa),
+  total_home_comp_yac_epa:
+    format_number(play.total_home_comp_yac_epa),
+  total_away_comp_yac_epa:
+    format_number(play.total_away_comp_yac_epa),
+  total_home_raw_air_epa:
+    format_number(play.total_home_raw_air_epa),
+  total_away_raw_air_epa:
+    format_number(play.total_away_raw_air_epa),
+  total_home_raw_yac_epa:
+    format_number(play.total_home_raw_yac_epa),
+  total_away_raw_yac_epa:
+    format_number(play.total_away_raw_yac_epa),
 
-  wp: parseFloat(play.wp) || null,
-  wpa: parseFloat(play.wpa) || null,
-  home_wp: parseFloat(play.home_wp) || null,
-  away_wp: parseFloat(play.away_wp) || null,
-  vegas_wpa: parseFloat(play.vegas_wpa) || null,
-  vegas_home_wpa: parseFloat(play.vegas_home_wpa) || null,
-  home_wp_post: parseFloat(play.home_wp_post) || null,
-  away_wp_post: parseFloat(play.away_wp_post) || null,
-  vegas_wp: parseFloat(play.vegas_wp) || null,
-  vegas_home_wp: parseFloat(play.vegas_home_wp) || null,
-  total_home_rush_wpa: parseFloat(play.total_home_rush_wpa) || null,
-  total_away_rush_wpa: parseFloat(play.total_away_rush_wpa) || null,
-  total_home_pass_wpa: parseFloat(play.total_home_pass_wpa) || null,
-  total_away_pass_wpa: parseFloat(play.total_away_pass_wpa) || null,
-  air_wpa: parseFloat(play.air_wpa) || null,
-  yac_wpa: parseFloat(play.yac_wpa) || null,
-  comp_air_wpa: parseFloat(play.comp_air_wpa) || null,
-  comp_yac_wpa: parseFloat(play.comp_yac_wpa) || null,
-  total_home_comp_air_wpa: parseFloat(play.total_home_comp_air_wpa) || null,
-  total_away_comp_air_wpa: parseFloat(play.total_away_comp_air_wpa) || null,
-  total_home_comp_yac_wpa: parseFloat(play.total_home_comp_yac_wpa) || null,
-  total_away_comp_yac_wpa: parseFloat(play.total_away_comp_yac_wpa) || null,
-  total_home_raw_air_wpa: parseFloat(play.total_home_raw_air_wpa) || null,
-  total_away_raw_air_wpa: parseFloat(play.total_away_raw_air_wpa) || null,
-  total_home_raw_yac_wpa: parseFloat(play.total_home_raw_yac_wpa) || null,
-  total_away_raw_yac_wpa: parseFloat(play.total_away_raw_yac_wpa) || null,
+  wp: format_number(play.wp),
+  wpa: format_number(play.wpa),
+  home_wp: format_number(play.home_wp),
+  away_wp: format_number(play.away_wp),
+  vegas_wpa: format_number(play.vegas_wpa),
+  vegas_home_wpa: format_number(play.vegas_home_wpa),
+  home_wp_post: format_number(play.home_wp_post),
+  away_wp_post: format_number(play.away_wp_post),
+  vegas_wp: format_number(play.vegas_wp),
+  vegas_home_wp: format_number(play.vegas_home_wp),
+  total_home_rush_wpa: format_number(play.total_home_rush_wpa),
+  total_away_rush_wpa: format_number(play.total_away_rush_wpa),
+  total_home_pass_wpa: format_number(play.total_home_pass_wpa),
+  total_away_pass_wpa: format_number(play.total_away_pass_wpa),
+  air_wpa: format_number(play.air_wpa),
+  yac_wpa: format_number(play.yac_wpa),
+  comp_air_wpa: format_number(play.comp_air_wpa),
+  comp_yac_wpa: format_number(play.comp_yac_wpa),
+  total_home_comp_air_wpa:
+    format_number(play.total_home_comp_air_wpa),
+  total_away_comp_air_wpa:
+    format_number(play.total_away_comp_air_wpa),
+  total_home_comp_yac_wpa:
+    format_number(play.total_home_comp_yac_wpa),
+  total_away_comp_yac_wpa:
+    format_number(play.total_away_comp_yac_wpa),
+  total_home_raw_air_wpa:
+    format_number(play.total_home_raw_air_wpa),
+  total_away_raw_air_wpa:
+    format_number(play.total_away_raw_air_wpa),
+  total_home_raw_yac_wpa:
+    format_number(play.total_home_raw_yac_wpa),
+  total_away_raw_yac_wpa:
+    format_number(play.total_away_raw_yac_wpa),
 
-  xyac_mean_yds: parseFloat(play.xyac_mean_yardage) || null,
-  xyac_median_yds: parseFloat(play.xyac_median_yardage) || null,
-  xyac_succ_prob: parseFloat(play.xyac_success) || null,
-  xyac_fd_prob: parseFloat(play.xyac_fd) || null,
+  xyac_mean_yds: format_number(play.xyac_mean_yardage),
+  xyac_median_yds: format_number(play.xyac_median_yardage),
+  xyac_succ_prob: format_number(play.xyac_success),
+  xyac_fd_prob: format_number(play.xyac_fd),
 
   ep_att: Boolean(play.extra_point_attempt),
   two_att: Boolean(play.two_point_attempt),
@@ -164,42 +198,42 @@ const formatPlay = (play) => ({
   punt_att: Boolean(play.punt_attempt),
 
   fg_result: play.field_goal_result,
-  kick_distance: parseInt(play.kick_distance, 10) || null,
+  kick_distance: format_number(play.kick_distance),
   ep_result: play.extra_point_result,
   tp_result: play.two_point_conv_result,
   punt_blocked: Boolean(play.punt_blocked),
 
-  home_to_rem: parseInt(play.home_timeouts_remaining, 10) || null,
-  away_to_rem: parseInt(play.away_timeouts_remaining, 10) || null,
-  pos_to_rem: parseInt(play.posteam_timeouts_remaining, 10) || null,
-  def_to_rem: parseInt(play.defteam_timeouts_remaining, 10) || null,
+  home_to_rem: format_number(play.home_timeouts_remaining),
+  away_to_rem: format_number(play.away_timeouts_remaining),
+  pos_to_rem: format_number(play.posteam_timeouts_remaining),
+  def_to_rem: format_number(play.defteam_timeouts_remaining),
   to: Boolean(play.timeout),
   to_team: play.timeout_team ? fixTeam(play.timeout_team) : null,
 
-  home_score: parseInt(play.total_home_score, 10) || null,
-  away_score: parseInt(play.total_away_score, 10) || null,
-  pos_score: parseInt(play.posteam_score, 10) || null,
-  def_score: parseInt(play.defteam_score, 10) || null,
-  score_diff: parseInt(play.score_differential, 10) || null,
-  pos_score_post: parseInt(play.posteam_score_post, 10) || null,
-  def_score_post: parseInt(play.defteam_score_post, 10) || null,
-  score_diff_post: parseInt(play.score_differential_post, 10) || null,
+  home_score: format_number(play.total_home_score),
+  away_score: format_number(play.total_away_score),
+  pos_score: format_number(play.posteam_score),
+  def_score: format_number(play.defteam_score),
+  score_diff: format_number(play.score_differential),
+  pos_score_post: format_number(play.posteam_score_post),
+  def_score_post: format_number(play.defteam_score_post),
+  score_diff_post: format_number(play.score_differential_post),
 
-  no_score_prob: parseFloat(play.no_score_prob) || null,
-  opp_fg_prob: parseFloat(play.opp_fg_prob) || null,
-  opp_safety_prob: parseFloat(play.opp_safety_prob) || null,
-  opp_td_prob: parseFloat(play.opp_td_prob) || null,
-  fg_prob: parseFloat(play.fg_prob) || null,
-  safety_prob: parseFloat(play.safety_prob) || null,
-  td_prob: parseFloat(play.td_prob) || null,
-  extra_point_prob: parseFloat(play.extra_point_prob) || null,
-  two_conv_prob: parseFloat(play.two_conv_prob) || null,
+  no_score_prob: format_number(play.no_score_prob),
+  opp_fg_prob: format_number(play.opp_fg_prob),
+  opp_safety_prob: format_number(play.opp_safety_prob),
+  opp_td_prob: format_number(play.opp_td_prob),
+  fg_prob: format_number(play.fg_prob),
+  safety_prob: format_number(play.safety_prob),
+  td_prob: format_number(play.td_prob),
+  extra_point_prob: format_number(play.extra_point_prob),
+  two_conv_prob: format_number(play.two_conv_prob),
 
-  xpass_prob: parseFloat(play.xpass) || null,
-  pass_oe: parseFloat(play.pass_oe) || null,
+  xpass_prob: format_number(play.xpass),
+  pass_oe: format_number(play.pass_oe),
 
-  cp: parseFloat(play.cp) || null,
-  cpoe: parseFloat(play.cpoe) || null
+  cp: format_number(play.cp),
+  cpoe: format_number(play.cpoe)
 })
 
 const run = async () => {
@@ -207,6 +241,7 @@ const run = async () => {
   const filename = `play_by_play_${year}.csv`
   const path = `${os.tmpdir()}/${filename}`
   const url = `https://github.com/nflverse/nflverse-data/releases/download/pbp/${filename}`
+  const force_import = argv.force
 
   if (argv.d || !fs.existsSync(path)) {
     // download file
@@ -238,11 +273,17 @@ const run = async () => {
     const dbPlay = await getPlay(opts)
 
     if (dbPlay) {
-      const play = formatPlay(item)
-      // log(`${dbPlay.esbid} ${dbPlay.playId}`)
-      await db('nfl_plays').update(play).where({
-        esbid: dbPlay.esbid,
-        playId: dbPlay.playId
+      const play = format_play(item)
+
+      // temp ignore discrepanies in current year data
+      if (year === constants.season.year) {
+        delete play.game_clock_end
+      }
+
+      await update_play({
+        play_row: dbPlay,
+        update: play,
+        ignore_conflicts: force_import
       })
     } else {
       log(`${item.game_id} - ${item.play_id} - ${item.desc}`)
