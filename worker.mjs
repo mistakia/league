2e99ab1af9130b6c348ffc4fbb6a7029abdef652
mistakia <1823355+mistakia@users.noreply.github.com>
2023-09-11 20:45:35 -0400

    debug: node cron tasks not running

--- worker.mjs
+++ worker.mjs
@@ -1,9 +1,9 @@
 import debug from 'debug'
-import cron from 'node-cron'
+import schedule from 'node-schedule'
 
 import import_live_plays from '#jobs/import-live-plays.mjs'
-import import_live_odds from '#jobs/import-live-odds.mjs'
-import finalize_week from '#jobs/finalize-week.mjs'
+// import import_live_odds from '#jobs/import-live-odds.mjs'
+// import finalize_week from '#jobs/finalize-week.mjs'
 
 const log = debug('worker')
 debug.enable(
@@ -15,29 +15,29 @@ log(`env: ${process.env.NODE_ENV}`)
 // REGULAR SEASON
 
 // monday
-cron.schedule('*/1 20-23 * 1,2,9-12 1', import_live_plays)
+schedule.scheduleJob('*/1 20-23 * 1,2,9-12 1', import_live_plays)
 
 // tuesday
-cron.schedule('0 0 * 1,2,9-12 2', finalize_week)
+// cron.schedule('0 0 * 1,2,9-12 2', finalize_week)
 
 // wednesday
-cron.schedule('0 0 * 1,2,9-12 3', finalize_week)
+// cron.schedule('0 0 * 1,2,9-12 3', finalize_week)
 
 // thursday
-cron.schedule('*/1 20-23 * 1,2,9-12 4', import_live_plays)
+// cron.schedule('*/1 20-23 * 1,2,9-12 4', import_live_plays)
 
 // sunday
-cron.schedule('*/1 9-23 * 1,2,9-12 7', import_live_plays)
+// cron.schedule('*/1 9-23 * 1,2,9-12 7', import_live_plays)
 
 // monday through thursday and sunday
-cron.schedule('*/10 * * 1,2,9-12 1-4,7', import_live_odds)
+// cron.schedule('*/10 * * 1,2,9-12 1-4,7', import_live_odds)
 
 // friday through saturday
-cron.schedule('*/5 * * 1,2,9-12 5-6', import_live_odds)
+// cron.schedule('*/5 * * 1,2,9-12 5-6', import_live_odds)
 
 // OFF SEASON
 
 // every 10 minutes
-cron.schedule('*/10 * * 3,4,5,6,7,8 *', import_live_odds)
+// cron.schedule('*/10 * * 3,4,5,6,7,8 *', import_live_odds)
 
 log('all jobs registered')
