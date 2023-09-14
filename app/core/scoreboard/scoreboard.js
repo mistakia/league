b32b65bd713188999f713ef587088a5e5748b192
mistakia <1823355+mistakia@users.noreply.github.com>
2023-09-14 15:31:59 -0400

    fix: scoreboard matchup past projections & performance improvements

--- app/core/scoreboard/scoreboard.js
+++ app/core/scoreboard/scoreboard.js
@@ -1,8 +1,27 @@
 import { Record } from 'immutable'
 
+import { Matchup, createMatchup } from '@core/matchups/matchup'
+
 export const Scoreboard = new Record({
   tid: null,
   points: null,
   projected: null,
-  minutes: null
+  minutes: null,
+  matchup: new Matchup()
 })
+
+export const createScoreboard = ({
+  tid,
+  points,
+  projected,
+  minutes,
+  matchup
+} = {}) => {
+  return new Scoreboard({
+    tid,
+    points,
+    projected,
+    minutes,
+    matchup: createMatchup(matchup)
+  })
+}
