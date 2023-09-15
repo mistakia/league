26b7396d0a0261ed318519c4e36b3482ffa09cc9
mistakia <1823355+mistakia@users.noreply.github.com>
2023-09-15 11:39:14 -0400

    feat: live scoring on schedule page

--- app/views/components/matchup/matchup.js
+++ app/views/components/matchup/matchup.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 
-import { get_string_from_object } from '@libs-shared'
+import { constants, get_string_from_object } from '@libs-shared'
 import TeamName from '@components/team-name'
 import TeamImage from '@components/team-image'
 
 import './matchup.styl'
 
-export default function Matchup({ matchup, teams }) {
+export default function Matchup({ matchup, teams, scoreboard }) {
   const navigate = useNavigate()
   const home = teams.find((t) => t.uid === matchup.hid) || {}
   const away = teams.find((t) => t.uid === matchup.aid) || {}
@@ -19,10 +19,28 @@ export default function Matchup({ matchup, teams }) {
   const formatSpread = (value) =>
     value === 0 ? 'EVEN' : value > 0 ? `+${value}` : value
 
+  const is_current_week =
+    matchup.week === constants.season.week &&
+    matchup.year === constants.season.year
+  const is_final = Boolean(matchup.ap && matchup.hp)
+    const home_score =
+    !is_final && is_current_week ? scoreboard.home.points : matchup.hp
+  const away_score =
+    !is_final && is_current_week ? scoreboard.away.points : matchup.ap
+  const home_proj =
+    !is_final && is_current_week
+      ? Math.round(scoreboard.home.projected)
+      : matchup.home_projection
+  const away_proj =
+    !is_final && is_current_week
+      ? Math.round(scoreboard.away.projected)
+      : matchup.away_projection
+
   return (
     <div className='matchup cursor' onClick={handleClick}>
       <div className='matchup__head'>
         <div className='matchup__col metric spread'>Spread</div>
+        <div className='matchup__col metric proj'>Proj</div>
         <div className='matchup__col metric score' />
       </div>
       <div
@@ -42,7 +60,8 @@ export default function Matchup({ matchup, teams }) {
         <div className='matchup__col metric spread'>
           {formatSpread(matchup.home_projection - matchup.away_projection)}
         </div>
-        <div className='matchup__col metric score'>{matchup.ap || '-'}</div>
+        <div className='matchup__col metric proj'>{away_proj}</div>
+        <div className='matchup__col metric score'>{away_score || '-'}</div>
         {matchup.ap > matchup.hp && (
           <div className='matchup__winner-arrow-left' />
         )}
@@ -64,7 +83,8 @@ export default function Matchup({ matchup, teams }) {
         <div className='matchup__col metric spread'>
           {formatSpread(matchup.away_projection - matchup.home_projection)}
         </div>
-        <div className='matchup__col metric score'>{matchup.hp || '-'}</div>
+        <div className='matchup__col metric proj'>{home_proj}</div>
+        <div className='matchup__col metric score'>{home_score || '-'}</div>
         {matchup.hp > matchup.ap && (
           <div className='matchup__winner-arrow-left' />
         )}
@@ -75,5 +95,6 @@ export default function Matchup({ matchup, teams }) {
 
 Matchup.propTypes = {
   matchup: ImmutablePropTypes.record,
-  teams: ImmutablePropTypes.map
+  teams: ImmutablePropTypes.map,
+  scoreboard: ImmutablePropTypes.record
 }
