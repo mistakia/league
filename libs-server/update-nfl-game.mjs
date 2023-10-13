3078916825850c6d64290cbd1750eb4244d4d6e8
mistakia <1823355+mistakia@users.noreply.github.com>
2023-10-13 00:29:16 -0400

    feat: import nfl games data from `nflverse/nfldata`

---
+++ libs-server/update-nfl-game.mjs
@@ -0,0 +1,114 @@
+import diff from 'deep-diff'
+import debug from 'debug'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+import isMain from './is-main.mjs'
+import db from '#db'
+
+const argv = yargs(hideBin(process.argv)).argv
+const log = debug('update-nfl-game')
+debug.enable('update-nfl-game')
+
+const excluded_column_names = ['esbid', 'updated']
+
+const update_nfl_game = async ({
+  game_row,
+  esbid,
+  update,
+  ignore_conflicts = false
+}) => {
+  if (!game_row && esbid) {
+    game_row = await db('nfl_games').where({ esbid }).first()
+  }
+
+  if (!game_row) {
+    return 0
+  }
+
+  const differences = diff(game_row, update)
+
+  const edits = differences.filter((d) => d.kind === 'E')
+  if (!edits.length) {
+    return 0
+  }
+
+  let changes = 0
+  for (const edit of edits) {
+    const column_name = edit.path[0]
+
+    if (excluded_column_names.includes(column_name)) {
+      log(`not allowed to update ${column_name}`)
+      continue
+    }
+
+    if (!ignore_conflicts && edit.lhs) {
+      log(
+        `conflict with existing value for ${column_name}, esbid: ${game_row.esbid}, existing: ${edit.lhs}, new: ${edit.rhs}`
+      )
+      continue
+    }
+
+    changes += 1
+    log(
+      `Updating game: ${game_row.esbid}, Field: ${column_name}, Value: ${edit.rhs}`
+    )
+
+    const prev = edit.lhs
+    if (prev) {
+      await db('nfl_games_changelog')
+        .insert({
+          esbid: game_row.esbid,
+          column_name,
+          prev,
+          new: edit.rhs,
+          timestamp: Math.round(Date.now() / 1000)
+        })
+        .onConflict()
+        .ignore()
+    }
+
+    await db('nfl_games')
+      .update({
+        [column_name]: edit.rhs
+      })
+      .where({
+        esbid: game_row.esbid
+      })
+  }
+
+  return changes
+}
+
+export default update_nfl_game
+
+const main = async () => {
+  let error
+  try {
+    if (!argv.esbid) {
+      log('missing --esbid')
+      process.exit()
+    }
+
+    const ignore = ['_', '$0', 'esbid']
+    const keys = Object.keys(argv).filter((key) => !ignore.includes(key))
+    const update = {}
+    keys.forEach((key) => {
+      update[key] = argv[key]
+    })
+
+    const changes = await update_nfl_game({
+      esbid: argv.esbid,
+      update
+    })
+    log(`game ${argv.esbid} updated, changes: ${changes}`)
+    process.exit()
+  } catch (err) {
+    error = err
+    log(error)
+  }
+}
+
+if (isMain(import.meta.url)) {
+  main()
+}
---
+++ scripts/import-nfl-games-nflverse-nfldata.mjs
@@ -0,0 +1,160 @@
+import fs from 'fs'
+import os from 'os'
+import { pipeline } from 'stream'
+import { promisify } from 'util'
+import fetch from 'node-fetch'
+import debug from 'debug'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+import db from '#db'
+import { constants } from '#libs-shared'
+import { isMain, readCSV, update_nfl_game, getPlayer } from '#libs-server'
+
+const argv = yargs(hideBin(process.argv)).argv
+const log = debug('import-nfl-games-nflverse')
+debug.enable('import-nfl-games-nflverse,update-nfl-game,get-player')
+
+const format_number = (num) => {
+  if (num === null || num === undefined || num === '') {
+    return null
+  }
+
+  const n = Number(num)
+
+  if (Number.isNaN(n)) {
+    return null
+  }
+
+  if (Number.isInteger(n)) {
+    return n
+  }
+
+  return Number(n.toFixed(12))
+}
+
+const format_game = (game) => ({
+  nflverse_game_id: game.game_id,
+  esbid: game.old_game_id,
+  gsisid: format_number(game.gsis),
+  pfr_game_id: game.pfr,
+  pff_game_id: game.pff,
+  espn_game_id: game.espn,
+  ftn_game_id: game.ftn,
+
+  // total: game.total,
+  // year: game.season,
+  // seas_type: game.game_type,
+  // week: game.week
+
+  away_rest: format_number(game.away_rest),
+  home_rest: format_number(game.home_rest),
+  away_moneyline: format_number(game.away_moneyline),
+  home_moneyline: format_number(game.home_moneyline),
+
+  spread_line: format_number(game.spread_line),
+  total_line: format_number(game.total_line),
+
+  roof: game.roof || null,
+  surf: game.surface || null,
+  temp: format_number(game.temp),
+  wind: format_number(game.wind),
+
+  stad: game.stadium || null,
+
+  away_coach: game.away_coach || null,
+  home_coach: game.home_coach || null,
+
+  referee: game.referee || null
+})
+
+const import_nfl_games_nflverse_nfldata = async ({
+  force_import = false,
+  force_download = false
+} = {}) => {
+  const current_date = new Date().toISOString().split('T')[0]
+  const file_name = `nflverse_nfldata_games_${current_date}.csv`
+  const path = `${os.tmpdir()}/${file_name}`
+  const url = 'https://github.com/nflverse/nfldata/raw/master/data/games.csv'
+
+  if (force_download || !fs.existsSync(path)) {
+    log(`downloading ${url}`)
+    const stream_pipeline = promisify(pipeline)
+    const response = await fetch(url)
+    if (!response.ok)
+      throw new Error(`unexpected response ${response.statusText}`)
+
+    await stream_pipeline(response.body, fs.createWriteStream(`${path}`))
+  } else {
+    log(`file exists: ${path}`)
+  }
+
+  const game_not_matched = []
+  const data = await readCSV(path, {
+    mapValues: ({ header, index, value }) => (value === 'NA' ? null : value)
+  })
+
+  for (const item of data) {
+    const db_game = await db('nfl_games')
+      .where({ esbid: item.old_game_id })
+      .first()
+
+    if (db_game) {
+      const game = format_game(item)
+
+      if (item.away_qb_id) {
+        const away_qb_player = await getPlayer({ gsisid: item.away_qb_id })
+
+        if (away_qb_player) {
+          game.away_qb_pid = away_qb_player.pid
+        }
+      }
+
+      if (item.home_qb_id) {
+        const home_qb_player = await getPlayer({ gsisid: item.home_qb_id })
+
+        if (home_qb_player) {
+          game.home_qb_pid = home_qb_player.pid
+        }
+      }
+
+      await update_nfl_game({
+        game_row: db_game,
+        update: game,
+        ignore_conflicts: force_import
+      })
+    } else {
+      log(`game not matched: ${item.old_game_id} - ${item.game_id}`)
+      game_not_matched.push(item)
+    }
+  }
+
+  log(`${game_not_matched.length} games not matched`)
+}
+
+const main = async () => {
+  let error
+  try {
+    const force_import = argv.force
+    const force_download = argv.d
+    await import_nfl_games_nflverse_nfldata({ force_import, force_download })
+  } catch (err) {
+    error = err
+    console.log(error)
+  }
+
+  await db('jobs').insert({
+    type: constants.jobs.IMPORT_GAMES_NFLVERSE,
+    succ: error ? 0 : 1,
+    reason: error ? error.message : null,
+    timestamp: Math.round(Date.now() / 1000)
+  })
+
+  process.exit()
+}
+
+if (isMain(import.meta.url)) {
+  main()
+}
+
+export default import_nfl_games_nflverse_nfldata
