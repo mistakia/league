6ec2214dd02261c49141cfad9fd690ed88cf0fa4
mistakia <1823355+mistakia@users.noreply.github.com>
2023-10-16 15:44:26 -0400

    feat: download fanduel wagers within a certain date range

--- scripts/import-fanduel-wagers.mjs
+++ scripts/import-fanduel-wagers.mjs
@@ -21,7 +21,8 @@ const import_fanduel_wagers = async ({
   authorization,
   is_settled = false,
   fanduel_state = 'va',
-  placed_after
+  placed_after,
+  placed_before
 } = {}) => {
   if (!placed_after) {
     placed_after = dayjs().subtract(1, 'week')
@@ -30,6 +31,7 @@ const import_fanduel_wagers = async ({
   log({
     is_settled,
     placed_after: placed_after.format(),
+    placed_before: placed_before ? placed_before.format() : null,
     fanduel_state
   })
 
@@ -37,14 +39,17 @@ const import_fanduel_wagers = async ({
     fanduel_state,
     is_settled,
     authorization,
-    placed_after
+    placed_after,
+    placed_before
   })
   log(`loaded ${wagers.length} wagers`)
 
   await fs.ensureDir(data_path)
   const json_file_path = `${data_path}/fanduel_wagers_${fanduel_state}_${placed_after.format(
     'YYYY'
-  )}_${placed_after.format('MM')}_${placed_after.format('DD')}.json`
+  )}_${placed_after.format('MM')}_${placed_after.format('DD')}_${
+    placed_before ? placed_before.format('YYYY_MM_DD') : 'present'
+  }.json`
   await fs.writeJson(json_file_path, wagers, { spaces: 2 })
   log(`wrote json to ${json_file_path}`)
 }
@@ -54,16 +59,20 @@ const main = async () => {
   try {
     const auth = argv.auth
     const fanduel_state = argv.state
-    const is_settled = Boolean(argv.is_settled)
+    const is_settled = argv.is_settled === 'true' || argv.is_settled === true
 
     const placed_after = argv.placed_after
       ? dayjs(argv.placed_after, 'YYYY-MM-DD')
+      : null
+    const placed_before = argv.placed_before
+      ? dayjs(argv.placed_before, 'YYYY-MM-DD')
       : null
     await import_fanduel_wagers({
       authorization: auth,
       fanduel_state,
       is_settled,
-      placed_after
+      placed_after,
+      placed_before
     })
   } catch (err) {
     error = err
