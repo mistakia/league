b72851527598ac317c31829c8d08e54946dc46e2
mistakia <1823355+mistakia@users.noreply.github.com>
2024-05-12 22:31:35 -0400

    refactor: convert `update-player-id` to executable cli script

--- libs-server/update-player-id.mjs
+++ libs-server/update-player-id.mjs
@@ -1,11 +1,24 @@
 import debug from 'debug'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 
 import db from '#db'
 import config from '#config'
+import isMain from './is-main.mjs'
 
+const argv = yargs(hideBin(process.argv)).argv
 const log = debug('update-player-id')
+debug.enable('update-player-id')
 
-export default async function ({ current_pid, new_pid }) {
+const update_player_id = async function ({ current_pid, new_pid }) {
+  if (!current_pid) {
+    throw new Error('current_pid is required')
+  }
+
+  if (!new_pid) {
+    throw new Error('new_pid is required')
+  }
+
   const player_rows = await db('player').where({ pid: new_pid })
   const player_row = player_rows[0]
 
@@ -34,4 +47,19 @@ export default async function ({ current_pid, new_pid }) {
     log(`${table_name} ${column_name} rows updated: ${rows_updated.info}`)
     await db(table_name).where(column_name, current_pid).del()
   }
+}
+
+export default update_player_id
+
+if (isMain(import.meta.url)) {
+  const main = async () => {
+    await update_player_id({
+      current_pid: argv.current_pid,
+      new_pid: argv.new_pid
+    })
+
+    process.exit(0)
+  }
+
+  main()
 }
