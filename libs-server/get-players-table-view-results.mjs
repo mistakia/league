39a6e4372ef599cec2514f55427181962a9e9744
mistakia <1823355+mistakia@users.noreply.github.com>
2024-06-16 23:51:59 -0400

    fix: group players table `with` statements by shared column params and pid column

--- libs-server/get-players-table-view-results.mjs
+++ libs-server/get-players-table-view-results.mjs
@@ -1,6 +1,58 @@
 import db from '#db'
 import players_table_column_definitions from './players-table-column-definitions.mjs'
 
+const add_play_by_play_with_statement = ({
+  query,
+  params = {},
+  with_table_name,
+  where_clauses = [],
+  select_strings = [],
+  pid_column
+}) => {
+  if (!with_table_name) {
+    throw new Error('with_table_name is required')
+  }
+
+  const conditions = [
+    { column: 'year', param: 'year', condition: params.year },
+    { column: 'week', param: 'week', condition: params.week },
+    { column: 'def', param: 'defense', condition: params.defense },
+    { column: 'off', param: 'offense', condition: params.offense },
+    { column: 'dwn', param: 'down', condition: params.down },
+    { column: 'qtr', param: 'quarter', condition: params.quarter }
+  ]
+
+  conditions.forEach((cond) => {
+    if (!Array.isArray(params[cond.param])) {
+      params[cond.param] = [params[cond.param]]
+    }
+  })
+
+  const with_query = db('nfl_plays')
+    .select(pid_column)
+    .whereNot('play_type', 'NOPL')
+    .where('seas_type', 'REG')
+    .groupBy(pid_column)
+
+  for (const select_string of select_strings) {
+    with_query.select(db.raw(select_string))
+  }
+
+  // column_params to filter plays
+  for (const condition of conditions) {
+    if (condition.condition) {
+      with_query.whereIn(condition.column, condition.condition)
+    }
+  }
+
+  // where_clauses to filter stats/metrics
+  for (const where_clause of where_clauses) {
+    with_query.havingRaw(where_clause)
+  }
+
+  query.with(with_table_name, with_query)
+}
+
 const get_column_index = ({ column_id, index, columns }) => {
   const columns_with_same_id = columns.filter(
     ({ column: c }) => (typeof c === 'string' ? c : c.column_id) === column_id
@@ -24,56 +76,6 @@ const get_table_name = ({ column_definition, column_params }) => {
   return column_definition.table_alias
     ? column_definition.table_alias({ params: column_params })
     : column_definition.table_name
-}
-
-const add_select_clause = ({
-  players_query,
-  column_definition,
-  table_name,
-  column_params,
-  column_index = 0
-}) => {
-  if (column_definition.select) {
-    column_definition.select({
-      query: players_query,
-      table_name,
-      params: column_params,
-      column_index
-    })
-  } else if (column_definition.select_as) {
-    const select_as = column_definition.select_as(column_params)
-    players_query.select(
-      `${table_name}.${column_definition.column_name} AS ${select_as}_${column_index}`
-    )
-  } else {
-    players_query.select(
-      `${table_name}.${column_definition.column_name} as ${column_definition.column_name}_${column_index}`
-    )
-  }
-}
-
-const join_table_if_needed = ({
-  players_query,
-  table_name,
-  column_definition,
-  column_params,
-  joined_table_index,
-  join_type = 'LEFT'
-}) => {
-  if (!joined_table_index[table_name]) {
-    if (column_definition.join) {
-      column_definition.join({
-        query: players_query,
-        table_name,
-        params: column_params,
-        join_type
-      })
-    } else if (table_name !== 'player' && table_name !== 'nfl_plays') {
-      players_query.leftJoin(table_name, `${table_name}.pid`, 'player.pid')
-    }
-
-    joined_table_index[table_name] = true
-  }
 }
 
 const get_where_string = ({ where_clause, column_definition, table_name }) => {
@@ -111,34 +113,27 @@ const get_where_string = ({ where_clause, column_definition, table_name }) => {
   return where_string
 }
 
-const add_where_clauses_for_table = ({
-  players_query,
-  where_clauses,
-  column_definition,
-  table_name,
+const get_select_string = ({
   column_params,
-  with_statement_index
+  column_index,
+  column_definition,
+  table_name
 }) => {
-  const where_clause_strings = []
-  for (const where_clause of where_clauses) {
-    const where_string = get_where_string({
-      where_clause,
-      column_definition,
-      table_name
-    })
-    where_clause_strings.push(where_string)
-  }
-
-  if (column_definition.with) {
-    column_definition.with({
-      query: players_query,
-      with_table_name: table_name,
+  if (column_definition.select) {
+    return column_definition.select({
+      table_name,
       params: column_params,
-      where_clauses: where_clause_strings
+      column_index
     })
-    with_statement_index[table_name] = true
+  } else if (column_definition.select_as) {
+    const select_as = column_definition.select_as(column_params)
+    return [
+      `\`${table_name}\`.\`${column_definition.column_name}\` AS \`${select_as}_${column_index}\``
+    ]
   } else {
-    players_query.whereRaw(where_clause_strings.join(' AND '))
+    return [
+      `\`${table_name}\`.\`${column_definition.column_name}\` as \`${column_definition.column_name}_${column_index}\``
+    ]
   }
 }
 
@@ -159,21 +154,134 @@ const add_sort_clause = ({
     )
   } else {
     players_query.orderByRaw(
-      `${table_name}.${column_definition.column_name}_${column_index} IS NULL`
+      `\`${table_name}\`.\`${column_definition.column_name}\` IS NULL`
     )
     players_query.orderByRaw(
-      `${table_name}.${column_definition.column_name}_${column_index} ${
+      `\`${table_name}\`.\`${column_definition.column_name}\` ${
         sort_clause.desc ? 'desc' : 'asc'
       }`
     )
   }
 }
 
-const get_grouped_where_clauses = ({
+const add_clauses_for_table = ({
+  players_query,
+  select_columns = [],
+  where_clauses = [],
+  table_name,
+  column_params = {}
+}) => {
+  const column_ids = []
+  const select_strings = []
+  let use_with = false
+
+  // the pid column and join_func should be the same among column definitions with the same table name/alias
+  let pid_column = null
+  let join_func = null
+
+  for (const { column_id, column_index } of select_columns) {
+    const column_definition = players_table_column_definitions[column_id]
+    const select_string = get_select_string({
+      column_params,
+      column_index,
+      column_definition,
+      table_name
+    })
+    for (const s of select_string) {
+      select_strings.push(s)
+    }
+
+    if (column_definition.join) {
+      join_func = column_definition.join
+    }
+
+    if (column_definition.use_with) {
+      use_with = true
+      pid_column = column_definition.pid_column
+      join_func = column_definition.join
+    }
+    column_ids.push(column_id)
+  }
+
+  const where_clause_strings = []
+  const unique_column_ids = new Set(column_ids)
+  for (const where_clause of where_clauses) {
+    const column_definition =
+      players_table_column_definitions[where_clause.column_id]
+    const where_string = get_where_string({
+      where_clause,
+      column_definition,
+      table_name
+    })
+    where_clause_strings.push(where_string)
+
+    if (column_definition.join) {
+      join_func = column_definition.join
+    }
+
+    // if use_with (play by play) a select string should be added to the with statement as its needed for the having clause (avoid duplicates for the same column)
+    if (
+      column_definition.use_with &&
+      !unique_column_ids.has(where_clause.column_id)
+    ) {
+      use_with = true
+      pid_column = column_definition.pid_column
+      join_func = column_definition.join
+      const select_string = get_select_string({
+        column_params,
+        column_index: 0,
+        column_definition,
+        table_name
+      })
+      for (const s of select_string) {
+        select_strings.push(s)
+      }
+    }
+  }
+
+  if (use_with) {
+    add_play_by_play_with_statement({
+      query: players_query,
+      params: column_params,
+      with_table_name: table_name,
+      where_clauses: where_clause_strings,
+      select_strings,
+      pid_column
+    })
+
+    // add select to players_query for each select_column in the with statement
+    for (const { column_id, column_index } of select_columns) {
+      const column_definition = players_table_column_definitions[column_id]
+      players_query.select(
+        `${table_name}.${column_definition.column_name} as ${column_definition.column_name}_${column_index}`
+      )
+    }
+  } else {
+    players_query.whereRaw(where_clause_strings.join(' AND '))
+    for (const select_string of select_strings) {
+      players_query.select(db.raw(select_string))
+    }
+  }
+
+  // join table if needed
+  if (join_func) {
+    join_func({
+      query: players_query,
+      table_name,
+      params: column_params,
+      join_type: where_clauses.length ? 'INNER' : 'LEFT'
+    })
+  } else if (table_name !== 'player' && table_name !== 'nfl_plays') {
+    players_query.leftJoin(table_name, `${table_name}.pid`, 'player.pid')
+  }
+}
+
+const get_grouped_clauses_by_table = ({
   where: where_clauses,
+  table_columns,
   players_table_column_definitions
 }) => {
-  const grouped_where_clauses = {}
+  const grouped_clauses_by_table = {}
 
   for (const where_clause of where_clauses) {
     const { column_id, params: column_params } = where_clause
@@ -187,17 +295,48 @@ const get_grouped_where_clauses = ({
       column_params
     })
 
-    if (!grouped_where_clauses[table_name]) {
-      grouped_where_clauses[table_name] = {
-        column_definition,
+    if (!grouped_clauses_by_table[table_name]) {
+      grouped_clauses_by_table[table_name] = {
         column_params,
-        where_clauses: []
+        where_clauses: [],
+        select_columns: []
       }
     }
-    grouped_where_clauses[table_name].where_clauses.push(where_clause)
+    grouped_clauses_by_table[table_name].where_clauses.push(where_clause)
   }
 
-  return grouped_where_clauses
+  for (const { column, index } of table_columns) {
+    const column_id = typeof column === 'string' ? column : column.column_id
+    const column_params = typeof column === 'string' ? {} : column.params
+    const column_definition = players_table_column_definitions[column_id]
+
+    // column_index among columns with the same column_id
+    const column_index = get_column_index({
+      column_id,
+      index,
+      columns: table_columns
+    })
+
+    if (!column_definition) {
+      continue
+    }
+    const table_name = get_table_name({ column_definition, column_params })
+
+    if (!grouped_clauses_by_table[table_name]) {
+      grouped_clauses_by_table[table_name] = {
+        column_params,
+        where_clauses: [],
+        select_columns: []
+      }
+    }
+
+    grouped_clauses_by_table[table_name].select_columns.push({
+      column_id,
+      column_index
+    })
+  }
+
+  return grouped_clauses_by_table
 }
 
 export default async function ({
@@ -219,76 +358,26 @@ export default async function ({
     })
   }
 
-  const grouped_where_clauses = get_grouped_where_clauses({
+  const grouped_clauses_by_table = get_grouped_clauses_by_table({
     where,
+    table_columns,
     players_table_column_definitions
   })
 
   // call joins for where/having first so they are inner joins
   for (const [
     table_name,
-    { column_definition, column_params, where_clauses }
-  ] of Object.entries(grouped_where_clauses)) {
-    add_where_clauses_for_table({
+    { column_params, where_clauses, select_columns }
+  ] of Object.entries(grouped_clauses_by_table)) {
+    add_clauses_for_table({
       players_query,
+      select_columns,
       where_clauses,
-      column_definition,
       table_name,
       column_params,
+      joined_table_index,
       with_statement_index
     })
-
-    join_table_if_needed({
-      players_query,
-      table_name,
-      column_definition,
-      column_params,
-      joined_table_index,
-      join_type: 'INNER'
-    })
-  }
-
-  for (const { column, index } of table_columns) {
-    const column_id = typeof column === 'string' ? column : column.column_id
-    const column_params = typeof column === 'string' ? {} : column.params
-    const column_definition = players_table_column_definitions[column_id]
-
-    // column_index among columns with the same column_id
-    const column_index = get_column_index({
-      column_id,
-      index,
-      columns: table_columns
-    })
-
-    if (!column_definition) {
-      continue
-    }
-
-    const table_name = get_table_name({ column_definition, column_params })
-    add_select_clause({
-      players_query,
-      column_definition,
-      table_name,
-      column_params,
-      column_index
-    })
-    join_table_if_needed({
-      players_query,
-      table_name,
-      column_definition,
-      column_params,
-      joined_table_index,
-      join_type: 'LEFT'
-    })
-
-    if (column_definition.with && !with_statement_index[table_name]) {
-      column_definition.with({
-        query: players_query,
-        with_table_name: table_name,
-        params: column_params
-      })
-      with_statement_index[table_name] = true
-    }
   }
 
   for (const sort_clause of sort) {
