e2a943732b95fc8c51b68c4e749a0cbec1798a1c
mistakia <1823355+mistakia@users.noreply.github.com>
2024-06-22 19:05:05 -0400

    refactor: status page loading indicator, show issues at the top, refactor to functional component

--- app/core/status/reducer.js
+++ app/core/status/reducer.js
@@ -1,11 +1,22 @@
-import { List } from 'immutable'
+import { List, Map } from 'immutable'
 
 import { statusActions } from './actions'
 
-export function statusReducer(state = new List(), { payload, type }) {
+const initialState = new Map({
+  is_loading: false,
+  jobs: new List()
+})
+
+export function statusReducer(state = initialState, { payload, type }) {
   switch (type) {
+    case statusActions.GET_STATUS_PENDING:
+      return state.set('is_loading', true)
+
+    case statusActions.GET_STATUS_FAILED:
+      return state.set('is_loading', false)
+
     case statusActions.GET_STATUS_FULFILLED:
-      return new List(payload.data)
+      return state.set('is_loading', false).set('jobs', new List(payload.data))
 
     default:
       return state
