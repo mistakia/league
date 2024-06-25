184e42d69bf0398ddb35d998bc12bfe89cc5adeb
mistakia <1823355+mistakia@users.noreply.github.com>
2024-06-24 23:45:29 -0400

    feat: update player table fields to include team share stats

--- test/libs-server.get-players-table-view-results.mjs
+++ test/libs-server.get-players-table-view-results.mjs
@@ -166,4 +166,52 @@ describe('LIBS SERVER get_players_table_view_results', () => {
       "with `f93346a51cbad8531491c4e5538f42384366d8d153b66c5e26a6b4d62d58aa5d` as (select `pg`.`pid`, ROUND(100.0 * SUM(CASE WHEN nfl_plays.trg_pid = pg.pid THEN nfl_plays.dot ELSE 0 END) / SUM(nfl_plays.dot), 2) AS air_yds_share_from_plays from `nfl_plays` inner join `player_gamelogs` as `pg` on `nfl_plays`.`esbid` = `pg`.`esbid` and `nfl_plays`.`off` = `pg`.`tm` where not `play_type` = 'NOPL' and `seas_type` = 'REG' and `trg_pid` is not null and `year` in (2023) group by `pg`.`pid` having air_yds_share_from_plays >= '25') select `player`.`pid`, `f93346a51cbad8531491c4e5538f42384366d8d153b66c5e26a6b4d62d58aa5d`.`air_yds_share_from_plays` as `air_yds_share_from_plays_0`, player.fname, player.lname from `player` inner join `f93346a51cbad8531491c4e5538f42384366d8d153b66c5e26a6b4d62d58aa5d` on `f93346a51cbad8531491c4e5538f42384366d8d153b66c5e26a6b4d62d58aa5d`.`pid` = `player`.`pid` group by `player`.`pid`, `player`.`lname`, `player`.`fname` order by `air_yds_share_from_plays_0` IS NULL, `air_yds_share_from_plays_0` desc limit 500"
     expect(query.toString()).to.equal(expected_query)
   })
+
+  it('should handle player_target_share_from_plays with a where clause', () => {
+    const query = get_players_table_view_results({
+      prefix_columns: ['player_name'],
+      columns: [
+        {
+          column_id: 'player_target_share_from_plays',
+          params: {
+            year: [2023],
+            dwn: [3]
+          }
+        },
+        {
+          column_id: 'player_targets_from_plays',
+          params: {
+            year: [2023],
+            dwn: [3]
+          }
+        },
+        {
+          column_id: 'player_target_share_from_plays',
+          params: {
+            year: [2023],
+            qtr: [1, 2]
+          }
+        }
+      ],
+      where: [
+        {
+          column_id: 'player_targets_from_plays',
+          operator: '>=',
+          value: '15',
+          params: {
+            year: [2023],
+            dwn: [3]
+          }
+        }
+      ],
+      sort: [
+        {
+          column_id: 'player_target_share_from_plays',
+          desc: true
+        }
+      ]
+    })
+    const expected_query = "with `2a75baacf96d2681b567e3bb61d0ea94fc46b9d94290ca11aa679cf35cdece17` as (select `trg_pid`, COUNT(*) AS trg_from_plays_0 from `nfl_plays` where not `play_type` = 'NOPL' and `seas_type` = 'REG' and `dwn` in (3) and `year` in (2023) group by `trg_pid` having trg_from_plays_0 >= '15'), `e9290825ef915b8776a391455230feab7b6cf7a607062b12529dbd65e0cf542f` as (select `pg`.`pid`, ROUND(100.0 * COUNT(CASE WHEN nfl_plays.trg_pid = pg.pid THEN 1 ELSE NULL END) / COUNT(*), 2) AS trg_share_from_plays from `nfl_plays` inner join `player_gamelogs` as `pg` on `nfl_plays`.`esbid` = `pg`.`esbid` and `nfl_plays`.`off` = `pg`.`tm` where not `play_type` = 'NOPL' and `seas_type` = 'REG' and `trg_pid` is not null and `dwn` in (3) and `year` in (2023) group by `pg`.`pid`), `db60da4fe342316de8c189603bb657c844ffad9b69a6ca1df4a1316276efd8c3` as (select `pg`.`pid`, ROUND(100.0 * COUNT(CASE WHEN nfl_plays.trg_pid = pg.pid THEN 1 ELSE NULL END) / COUNT(*), 2) AS trg_share_from_plays from `nfl_plays` inner join `player_gamelogs` as `pg` on `nfl_plays`.`esbid` = `pg`.`esbid` and `nfl_plays`.`off` = `pg`.`tm` where not `play_type` = 'NOPL' and `seas_type` = 'REG' and `trg_pid` is not null and `qtr` in (1, 2) and `year` in (2023) group by `pg`.`pid`) select `player`.`pid`, `2a75baacf96d2681b567e3bb61d0ea94fc46b9d94290ca11aa679cf35cdece17`.`trg_from_plays_0` as `trg_from_plays_0`, player.fname, player.lname, `e9290825ef915b8776a391455230feab7b6cf7a607062b12529dbd65e0cf542f`.`trg_share_from_plays` as `trg_share_from_plays_0`, `db60da4fe342316de8c189603bb657c844ffad9b69a6ca1df4a1316276efd8c3`.`trg_share_from_plays` as `trg_share_from_plays_1` from `player` inner join `2a75baacf96d2681b567e3bb61d0ea94fc46b9d94290ca11aa679cf35cdece17` on `2a75baacf96d2681b567e3bb61d0ea94fc46b9d94290ca11aa679cf35cdece17`.`trg_pid` = `player`.`pid` left join `e9290825ef915b8776a391455230feab7b6cf7a607062b12529dbd65e0cf542f` on `e9290825ef915b8776a391455230feab7b6cf7a607062b12529dbd65e0cf542f`.`pid` = `player`.`pid` left join `db60da4fe342316de8c189603bb657c844ffad9b69a6ca1df4a1316276efd8c3` on `db60da4fe342316de8c189603bb657c844ffad9b69a6ca1df4a1316276efd8c3`.`pid` = `player`.`pid` group by `player`.`pid`, `player`.`lname`, `player`.`fname` order by `trg_share_from_plays_0` IS NULL, `trg_share_from_plays_0` desc limit 500"
+    expect(query.toString()).to.equal(expected_query)
+  })
 })
