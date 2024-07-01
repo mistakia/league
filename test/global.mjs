89d273c1fb582203648855220f7e3bfc43656ce5
mistakia <1823355+mistakia@users.noreply.github.com>
2024-06-30 23:10:13 -0400

    fix: workflow test

--- test/global.mjs
+++ test/global.mjs
@@ -1,5 +1,14 @@
 import knex from '#db'
+import path, { dirname } from 'path'
+import fs from 'fs/promises'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const schema_file = path.resolve(__dirname, '../db/schema.postgres.sql')
 
 export async function mochaGlobalSetup() {
+  const sql = await fs.readFile(schema_file, 'utf8')
+  await knex.raw(sql)
+
   await knex.seed.run()
 }
