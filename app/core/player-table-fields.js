2bbac2ace3b51db1b26a472c5b2cd10cf85e68b4
mistakia <1823355+mistakia@users.noreply.github.com>
2024-07-04 01:07:55 -0400

    feat: add player table fields for league format seasonlogs and careerlogs

--- app/core/player-table-fields.js
+++ app/core/player-table-fields.js
@@ -35,6 +35,9 @@ const COLUMN_GROUPS = {
   AFTER_CATCH: { priority: 5 }
 }
 
+const active_year =
+  constants.season.week > 0 ? constants.season.year : constants.season.year - 1
+
 for (const [key, value] of Object.entries(COLUMN_GROUPS)) {
   value.column_group_id = key
 }
@@ -148,10 +151,9 @@ export function PlayerTableFields({
     ...field
   })
 
-  const from_scoring_format_logs = (field) => ({
+  const from_format_player_logs = (field) => ({
     data_type: table_constants.TABLE_DATA_TYPES.NUMBER,
     size: 70,
-    splits: ['year'],
     ...field
   })
 
@@ -175,28 +177,69 @@ export function PlayerTableFields({
     single: true
   }
 
+  const league_format_hash_param = {
+    label: 'League Format',
+    values: [
+      {
+        value:
+          '1985e1968b75707ebcab9da620176a0b218c5c1bd28d00cbbc4d1744a1631d0b',
+        label: '10 Team / SFLEX / 1 FLX / 0.5PPR / 4PTD (GENESIS LEAGUE)'
+      },
+      {
+        value:
+          '97b8d7d2b53c7401b93bfad1fbe97aeb1a582e376b2fcb34868a8628d7fbe48a',
+        label: '12 Team / SFLEX / 1 FLX / PPR / 4PTD'
+      }
+    ],
+    data_type: table_constants.TABLE_DATA_TYPES.SELECT,
+    default_value:
+      '1985e1968b75707ebcab9da620176a0b218c5c1bd28d00cbbc4d1744a1631d0b',
+    single: true
+  }
+
   const from_scoring_format_seasonlogs = (field) => ({
-    ...from_scoring_format_logs(field),
+    ...from_format_player_logs(field),
     column_groups: [COLUMN_GROUPS.FANTASY_POINTS, COLUMN_GROUPS.SEASON],
     column_params: {
       year: {
         values: constants.years,
         data_type: table_constants.TABLE_DATA_TYPES.SELECT,
-        default_value:
-          constants.season.week > 0
-            ? constants.season.year
-            : constants.season.year - 1,
+        default_value: active_year,
         single: true
       },
+      scoring_format_hash: scoring_format_hash_param
+    },
+    splits: ['year']
+  })
+
+  const from_scoring_format_careerlogs = (field) => ({
+    ...from_format_player_logs(field),
+    column_groups: [COLUMN_GROUPS.FANTASY_POINTS, COLUMN_GROUPS.CAREER],
+    column_params: {
       scoring_format_hash: scoring_format_hash_param
     }
   })
 
-  const from_scoring_format_careerlogs = (field) => ({
-    ...from_scoring_format_logs(field),
-    column_groups: [COLUMN_GROUPS.FANTASY_POINTS, COLUMN_GROUPS.CAREER],
+  const from_league_format_seasonlogs = (field) => ({
+    ...from_format_player_logs(field),
+    column_groups: [COLUMN_GROUPS.FANTASY_LEAGUE, COLUMN_GROUPS.SEASON],
     column_params: {
-      scoring_format_hash: scoring_format_hash_param
+      year: {
+        values: constants.years,
+        data_type: table_constants.TABLE_DATA_TYPES.SELECT,
+        default_value: active_year,
+        single: true
+      },
+      league_format_hash: league_format_hash_param
+    },
+    splits: ['year']
+  })
+
+  const from_league_format_careerlogs = (field) => ({
+    ...from_format_player_logs(field),
+    column_groups: [COLUMN_GROUPS.FANTASY_LEAGUE, COLUMN_GROUPS.CAREER],
+    column_params: {
+      league_format_hash: league_format_hash_param
     }
   })
 
@@ -1591,7 +1634,101 @@ export function PlayerTableFields({
         column_title: 'Top 36 Seasons (Career)',
         header_label: 'TOP 36',
         player_value_path: 'top_36_from_careerlogs'
-      })
+      }),
+
+    player_startable_games_from_seasonlogs: from_league_format_seasonlogs({
+      column_title: 'Startable Games (Season)',
+      header_label: 'SG',
+      player_value_path: 'startable_games_from_seasonlogs'
+    }),
+    player_points_added_from_seasonlogs: from_league_format_seasonlogs({
+      column_title: 'Points Added (Season)',
+      header_label: 'Pts+',
+      player_value_path: 'points_added_from_seasonlogs'
+    }),
+    player_points_added_per_game_from_seasonlogs: from_league_format_seasonlogs(
+      {
+        column_title: 'Points Added Per Game (Season)',
+        header_label: 'Pts+/G',
+        player_value_path: 'points_added_per_game_from_seasonlogs'
+      }
+    ),
+    player_points_added_rank_from_seasonlogs: from_league_format_seasonlogs({
+      column_title: 'Points Added Rank (Season)',
+      header_label: 'Pts+ Rnk',
+      player_value_path: 'points_added_rnk_from_seasonlogs'
+    }),
+    player_points_added_position_rank_from_seasonlogs:
+      from_league_format_seasonlogs({
+        column_title: 'Points Added Position Rank (Season)',
+        header_label: 'Pts+ Pos Rnk',
+        player_value_path: 'points_added_pos_rnk_from_seasonlogs'
+      }),
+
+    player_startable_games_from_careerlogs: from_league_format_careerlogs({
+      column_title: 'Startable Games (Career)',
+      header_label: 'SG',
+      player_value_path: 'startable_games_from_careerlogs'
+    }),
+    player_points_added_from_careerlogs: from_league_format_careerlogs({
+      column_title: 'Points Added (Career)',
+      header_label: 'Pts+',
+      player_value_path: 'points_added_from_careerlogs'
+    }),
+    player_points_added_per_game_from_careerlogs: from_league_format_careerlogs(
+      {
+        column_title: 'Points Added Per Game (Career)',
+        header_label: 'Pts+/G',
+        player_value_path: 'points_added_per_game_from_careerlogs'
+      }
+    ),
+    player_best_season_points_added_per_game_from_careerlogs:
+      from_league_format_careerlogs({
+        column_title: 'Best Season Points Added Per Game (Career)',
+        header_label: 'Best Pts+/G',
+        player_value_path: 'best_season_points_added_per_game_from_careerlogs'
+      }),
+    player_points_added_first_three_seasons_from_careerlogs:
+      from_league_format_careerlogs({
+        column_title: 'Points Added First 3 Seasons (Career)',
+        header_label: 'Pts+ 1st 3',
+        player_value_path: 'points_added_first_three_seas_from_careerlogs'
+      }),
+    player_points_added_first_four_seasons_from_careerlogs:
+      from_league_format_careerlogs({
+        column_title: 'Points Added First 4 Seasons (Career)',
+        header_label: 'Pts+ 1st 4',
+        player_value_path: 'points_added_first_four_seas_from_careerlogs'
+      }),
+    player_points_added_first_five_seasons_from_careerlogs:
+      from_league_format_careerlogs({
+        column_title: 'Points Added First 5 Seasons (Career)',
+        header_label: 'Pts+ 1st 5',
+        player_value_path: 'points_added_first_five_seas_from_careerlogs'
+      }),
+    player_points_added_first_season_from_careerlogs:
+      from_league_format_careerlogs({
+        column_title: 'Points Added First Season (Career)',
+        header_label: 'Pts+ Rookie',
+        player_value_path: 'points_added_first_seas_from_careerlogs'
+      }),
+    player_points_added_second_season_from_careerlogs:
+      from_league_format_careerlogs({
+        column_title: 'Points Added Second Season (Career)',
+        header_label: 'Pts+ 2nd Year',
+        player_value_path: 'points_added_second_seas_from_careerlogs'
+      }),
+    player_points_added_third_season_from_careerlogs:
+      from_league_format_careerlogs({
+        column_title: 'Points Added Third Season (Career)',
+        header_label: 'Pts+ 3rd Year',
+        player_value_path: 'points_added_third_seas_from_careerlogs'
+      }),
+    player_draft_rank_from_careerlogs: from_league_format_careerlogs({
+      column_title: 'Points Added Draft Class Rank',
+      header_label: 'Pts Draft Rnk',
+      player_value_path: 'draft_rank_from_careerlogs'
+    })
   }
 
   if (is_logged_in) {
