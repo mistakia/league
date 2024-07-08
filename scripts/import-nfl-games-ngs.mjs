d3616342fc2c60f5e7687611dd4ccd6e297b22a9
mistakia <1823355+mistakia@users.noreply.github.com>
2024-07-07 22:45:47 -0400

    fix: unique index key for ngs games import

--- scripts/import-nfl-games-ngs.mjs
+++ scripts/import-nfl-games-ngs.mjs
@@ -106,7 +106,7 @@ const run = async ({ year = constants.season.year }) => {
   }
 
   if (inserts.length) {
-    await db('nfl_games').insert(inserts).onConflict('esbid').merge()
+    await db('nfl_games').insert(inserts).onConflict(['v', 'h', 'week', 'year', 'seas_type']).merge()
     log(`saved data for ${inserts.length} games`)
   }
 }
