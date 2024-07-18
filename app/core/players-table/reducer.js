1dc16b07b974397c9b96784053e6711650ef592a
mistakia <1823355+mistakia@users.noreply.github.com>
2024-07-18 07:47:30 -0400

    fix: do not load empty players table

--- app/core/players-table/reducer.js
+++ app/core/players-table/reducer.js
@@ -1,9 +1,13 @@
 import { List } from 'immutable'
 
 import { playerActions } from '@core/players/actions'
+import { players_table_views_actions } from '@core/players-table-views/actions'
 
 export function players_table_reducer(state = new List(), { payload, type }) {
   switch (type) {
+    case players_table_views_actions.PLAYERS_TABLE_VIEW_CHANGED:
+      return new List()
+
     case playerActions.POST_PLAYERS_TABLE_VIEW_SEARCH_FULFILLED:
       return new List(payload.data)
     default:
