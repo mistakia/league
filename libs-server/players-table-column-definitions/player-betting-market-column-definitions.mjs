635ea90db3e76ed07e9e05c10c809aa66483fe12
mistakia <1823355+mistakia@users.noreply.github.com>
2024-08-13 20:02:11 -0400

    feat: added team game betting market column definitions

--- libs-server/players-table-column-definitions/player-betting-market-column-definitions.mjs
+++ libs-server/players-table-column-definitions/player-betting-market-column-definitions.mjs
@@ -26,7 +26,7 @@ const betting_markets_table_alias = ({ params = {} }) => {
   )
 }
 
-const betting_markets_with = ({
+const player_betting_market_with = ({
   query,
   params,
   with_table_name,
@@ -83,7 +83,7 @@ const betting_markets_with = ({
   })
 }
 
-const betting_markets_join = ({
+const player_betting_market_join = ({
   query,
   table_name,
   join_type = 'LEFT',
@@ -94,23 +94,123 @@ const betting_markets_join = ({
   query[join_func](table_name, `${table_name}.selection_pid`, 'player.pid')
 }
 
-const create_betting_market_field = ({ column_name, column_alias }) => ({
+const team_betting_market_join = ({
+  query,
+  table_name,
+  join_type = 'LEFT',
+  params = {}
+}) => {
+  const join_func = get_join_func(join_type)
+
+  query[join_func](table_name, function () {
+    this.on(`${table_name}.h`, '=', 'player.current_nfl_team').orOn(
+      `${table_name}.v`,
+      '=',
+      'player.current_nfl_team'
+    )
+  })
+}
+
+const team_betting_market_with = ({
+  query,
+  params,
+  with_table_name,
+  having_clauses,
+  splits
+}) => {
+  const time_type = Array.isArray(params.time_type)
+    ? params.time_type[0]
+    : params.time_type || bookmaker_constants.time_type.CLOSE
+  const market_type = Array.isArray(params.market_type)
+    ? params.market_type[0]
+    : params.market_type || bookmaker_constants.game_market_types.GAME_TOTAL
+  const source_id = Array.isArray(params.source_id)
+    ? params.source_id[0]
+    : params.source_id || bookmaker_constants.bookmakers.DRAFTKINGS
+
+  const year = Array.isArray(params.year)
+    ? params.year[0]
+    : params.year || constants.season.year
+  let week = Array.isArray(params.week) ? params.week[0] : params.week
+
+  if (!week) {
+    week = Math.max(1, constants.season.week)
+  }
+
+  const markets_cte = `${with_table_name}_markets`
+
+  query.with(markets_cte, (qb) => {
+    qb.select(
+      'source_id',
+      'source_market_id',
+      'time_type',
+      'nfl_games.h',
+      'nfl_games.v'
+    )
+      .from('prop_markets_index')
+      .where('market_type', market_type)
+      .andWhere('time_type', time_type)
+      .andWhere('prop_markets_index.year', year)
+      .andWhere('source_id', source_id)
+
+    if (week) {
+      qb.join('nfl_games', function () {
+        this.on(`nfl_games.esbid`, '=', `prop_markets_index.esbid`)
+        this.andOn(`nfl_games.year`, '=', `prop_markets_index.year`)
+        this.andOn(`nfl_games.week`, '=', db.raw('?', [week]))
+      })
+    }
+  })
+
+  query.with(with_table_name, (qb) => {
+    qb.select('pms.selection_pid', 'pms.selection_metric_line', 'm.h', 'm.v')
+      .from(`${markets_cte} as m`)
+      .join('prop_market_selections_index as pms', function () {
+        this.on('pms.source_id', '=', 'm.source_id')
+          .andOn('pms.source_market_id', '=', 'm.source_market_id')
+          .andOn('pms.time_type', '=', 'm.time_type')
+      })
+  })
+}
+
+const create_player_betting_market_field = ({ column_name, column_alias }) => ({
   column_name,
   select_as: () => `${column_alias}_betting_market`,
   main_where: ({ table_name }) => `${table_name}.${column_name}`,
   table_alias: betting_markets_table_alias,
-  join: betting_markets_join,
-  with: betting_markets_with
+  join: player_betting_market_join,
+  with: player_betting_market_with
+})
+
+const create_team_betting_market_field = ({ column_name, column_alias }) => ({
+  column_name,
+  select_as: () => `${column_alias}_betting_market`,
+  main_where: ({ table_name }) => `${table_name}.${column_name}`,
+  table_alias: betting_markets_table_alias,
+  join: team_betting_market_join,
+  with: team_betting_market_with
 })
 
 export default {
-  player_season_prop_line_from_betting_markets: create_betting_market_field({
-    column_name: 'selection_metric_line',
-    column_alias: 'season_prop_line'
-  }),
+  player_season_prop_line_from_betting_markets:
+    create_player_betting_market_field({
+      column_name: 'selection_metric_line',
+      column_alias: 'season_prop_line'
+    }),
 
-  player_game_prop_line_from_betting_markets: create_betting_market_field({
+  player_game_prop_line_from_betting_markets:
+    create_player_betting_market_field({
+      column_name: 'selection_metric_line',
+      column_alias: 'game_prop_line'
+    }),
+
+  team_game_prop_line_from_betting_markets: create_team_betting_market_field({
     column_name: 'selection_metric_line',
     column_alias: 'game_prop_line'
+  }),
+
+  team_game_prop_odds_from_betting_markets: create_team_betting_market_field({
+    column_name: 'selection_odds',
+    column_alias: 'game_prop_odds'
   })
 }
