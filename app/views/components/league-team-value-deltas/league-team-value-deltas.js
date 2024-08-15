0da5bc879a6b30e20816d626720da511bdb3030d
mistakia <1823355+mistakia@users.noreply.github.com>
2024-08-15 12:32:54 -0400

    style: refactor styling and layout of league team page

--- app/views/components/league-team-value-deltas/league-team-value-deltas.js
+++ app/views/components/league-team-value-deltas/league-team-value-deltas.js
@@ -6,15 +6,13 @@ import LeagueTeamValueOverTime from '@components/league-team-value-over-time'
 
 import './league-team-value-deltas.styl'
 
-function formatTimeShorthand(days) {
-  if (days >= 365) {
-    return `${Math.floor(days / 365)}Y`
-  } else if (days >= 30) {
-    return `${Math.floor(days / 30)}M`
-  } else {
-    return `${days}D`
-  }
-}
+const placeholder_deltas = [
+  { days: 7, label: '7D' },
+  { days: 30, label: '1M' },
+  { days: 90, label: '3M' },
+  { days: 365, label: '1Y' },
+  { days: 730, label: '2Y' }
+]
 
 export default function LeagueTeamValueDeltas({
   team_value_deltas,
@@ -25,36 +23,50 @@ export default function LeagueTeamValueDeltas({
     load_league_team_daily_values()
   }, [load_league_team_daily_values])
 
-  if (!team_value_deltas) return null
-  const delta_items = []
-  const deltas = team_value_deltas.get('deltas', [])
-  deltas.forEach((delta, i) => {
-    const { days, delta_pct, delta_dollar_amount } = delta
-    const pct = (delta_pct * 100).toFixed(2)
-    const dollar_amount_abs = Math.abs(delta_dollar_amount).toFixed(2)
+  const deltas = team_value_deltas ? team_value_deltas.get('deltas', []) : []
 
-    const is_negative = delta_dollar_amount < 0
+  const delta_items = placeholder_deltas.map(({ days, label }) => {
+    const delta = deltas.find((d) => d.days === days)
 
-    const background_intensity = Math.min(
-      Math.abs(delta_dollar_amount) / 30,
-      0.2
-    )
-    const background = is_negative
-      ? `rgba(255, 80, 0, ${background_intensity})`
-      : `rgba(0, 200, 5, ${background_intensity})`
-    const color =
-      delta_dollar_amount > 0 ? 'rgba(0, 200, 5, 1)' : 'rgba(255, 80, 0, 1)'
-    const style = { background, color }
+    if (delta) {
+      const { delta_pct, delta_dollar_amount } = delta
+      const pct = (delta_pct * 100).toFixed(2)
+      const dollar_amount_abs = Math.abs(delta_dollar_amount).toFixed(2)
+      const is_negative = delta_dollar_amount < 0
 
-    delta_items.push(
-      <div key={i} className='league__team-value-deltas-item' style={style}>
-        <div className='deltas-item-time'>{formatTimeShorthand(days)}</div>
-        <div className='deltas-item-amount'>
-          {is_negative ? '-' : ''}${dollar_amount_abs}
+      const background_intensity = Math.min(
+        Math.abs(delta_dollar_amount) / 30,
+        0.2
+      )
+      const background = is_negative
+        ? `rgba(255, 80, 0, ${background_intensity})`
+        : `rgba(0, 200, 5, ${background_intensity})`
+      const color =
+        delta_dollar_amount > 0 ? 'rgba(0, 200, 5, 1)' : 'rgba(255, 80, 0, 1)'
+      const style = { background, color }
+
+      return (
+        <div
+          key={days}
+          className='league__team-value-deltas-item'
+          style={style}
+        >
+          <div className='deltas-item-time'>{label}</div>
+          <div className='deltas-item-pct'>{pct}%</div>
+          <div className='deltas-item-amount'>
+            {is_negative ? '-' : ''}${dollar_amount_abs}
+          </div>
         </div>
-        <div className='deltas-item-pct'>{pct}%</div>
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div key={days} className='league__team-value-deltas-item placeholder'>
+          <div className='deltas-item-time'>{label}</div>
+          <div className='deltas-item-pct'>-- %</div>
+          <div className='deltas-item-amount' />
+        </div>
+      )
+    }
   })
 
   return (
