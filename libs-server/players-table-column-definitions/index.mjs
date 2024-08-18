190fb8161a201afcf1aafa5b0f693f5ca7b15f2f
mistakia <1823355+mistakia@users.noreply.github.com>
2024-08-18 11:11:42 -0400

    feat: add player contract players table column definitions

--- libs-server/players-table-column-definitions/index.mjs
+++ libs-server/players-table-column-definitions/index.mjs
@@ -13,6 +13,7 @@ import defensive_player_stats_from_plays_column_definitions from './defensive-pl
 import team_stats_from_plays_column_definitions from './team-stats-from-plays-column-definitions.mjs'
 import player_keeptradecut_column_definitions from './player-keeptradecut-column-definitions.mjs'
 import player_games_played_column_definitions from './player-games-played-column-definitions.mjs'
+import player_contract_column_definitions from './player-contract-column-definitions.mjs'
 
 const player_league_roster_status_select = `CASE WHEN rosters_players.slot = ${constants.slots.IR} THEN 'injured_reserve' WHEN rosters_players.slot = ${constants.slots.PS} THEN 'practice_squad' WHEN rosters_players.slot IS NULL THEN 'free_agent' ELSE 'active_roster' END`
 
@@ -29,6 +30,7 @@ export default {
   ...team_stats_from_plays_column_definitions,
   ...player_keeptradecut_column_definitions,
   ...player_games_played_column_definitions,
+  ...player_contract_column_definitions,
 
   player_league_roster_status: {
     table_name: 'rosters_players',
