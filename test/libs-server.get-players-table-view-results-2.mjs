8a57e39b9f80efecd4ad249ac63c9af84011e4f4
mistakia <1823355+mistakia@users.noreply.github.com>
2024-08-18 12:21:51 -0400

    feat: add `player_contract_years_remaining` column definition

--- test/libs-server.get-players-table-view-results-2.mjs
+++ test/libs-server.get-players-table-view-results-2.mjs
@@ -49,4 +49,29 @@ describe('LIBS SERVER get_players_table_view_results', () => {
     const expected_query = `select "player"."pid", "ta51741002ab8f03ebbd01303bdd3cf2a"."base_salary" AS "base_salary_0", "tb8130d7f4a3233d1b3579e919af8b79b"."base_salary" AS "base_salary_1", "player"."pos" from "player" left join "player_contracts" as "ta51741002ab8f03ebbd01303bdd3cf2a" on "ta51741002ab8f03ebbd01303bdd3cf2a"."pid" = "player"."pid" and ta51741002ab8f03ebbd01303bdd3cf2a.year = '2022' left join "player_contracts" as "tb8130d7f4a3233d1b3579e919af8b79b" on "tb8130d7f4a3233d1b3579e919af8b79b"."pid" = "player"."pid" and tb8130d7f4a3233d1b3579e919af8b79b.year = 'Total' group by "ta51741002ab8f03ebbd01303bdd3cf2a"."base_salary", "tb8130d7f4a3233d1b3579e919af8b79b"."base_salary", "player"."pid", "player"."lname", "player"."fname", "player"."pos" order by 2 DESC NULLS LAST, "player"."pid" asc limit 500`
     compare_queries(query.toString(), expected_query)
   })
+
+  it('player contract years remaining', () => {
+    const query = get_players_table_view_results({
+      columns: [
+        {
+          column_id: 'player_contract_years_remaining'
+        }
+      ],
+      where: [
+        {
+          column_id: 'player_contract_years_remaining',
+          operator: '=',
+          value: 1
+        }
+      ],
+      sort: [
+        {
+          column_id: 'player_contract_years_remaining',
+          desc: true
+        }
+      ]
+    })
+    const expected_query = `select "player"."pid", CASE WHEN player.contract_year_signed IS NULL THEN NULL ELSE player.contract_years - (EXTRACT(YEAR FROM CURRENT_DATE) - player.contract_year_signed) END as contract_years_remaining_0, "player"."pos" from "player" where CASE WHEN player.contract_year_signed IS NULL THEN NULL ELSE player.contract_years - (EXTRACT(YEAR FROM CURRENT_DATE) - player.contract_year_signed) END = '1' group by "player"."pid", "player"."lname", "player"."fname", "player"."pos" order by "player"."pid" asc limit 500`
+    compare_queries(query.toString(), expected_query)
+  })
 })
