fcef8b2997e1bb165717c041d549590fb7e60c73
mistakia <1823355+mistakia@users.noreply.github.com>
2024-08-18 22:45:00 -0400

    fix: players table select view reset players table state items

--- app/core/players-table/reducer.js
+++ app/core/players-table/reducer.js
@@ -5,6 +5,7 @@ import { players_table_views_actions } from '@core/players-table-views/actions'
 
 export function players_table_reducer(state = new List(), { payload, type }) {
   switch (type) {
+    case players_table_views_actions.SET_SELECTED_PLAYERS_TABLE_VIEW:
     case players_table_views_actions.PLAYERS_TABLE_VIEW_CHANGED: {
       if (payload.view_change_params.view_state_changed) {
         return new List()
