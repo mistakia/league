c4fde60aaccca15f956049ed27710545220d97e6
mistakia <1823355+mistakia@users.noreply.github.com>
2024-08-19 21:16:26 -0400

    fix: auction style issues from code splitting

--- app/views/components/auction-team/auction-team.js
+++ app/views/components/auction-team/auction-team.js
@@ -6,50 +6,44 @@ import TeamName from '@components/team-name'
 
 import './auction-team.styl'
 
-export default class AuctionTeam extends React.Component {
-  render = () => {
-    const {
-      team,
-      isConnected,
-      isWinningBid,
-      bid,
-      isNominating,
-      roster,
-      isOwner
-    } = this.props
-
-    const classNames = ['auction__team']
-    if (!isConnected) {
-      classNames.push('offline')
-    }
-
-    if (isNominating) {
-      classNames.push('nominating')
-    }
-
-    if (isWinningBid) {
-      classNames.push('winning')
-    }
-
-    if (isOwner) {
-      classNames.push('owner')
-    }
-
-    const hasBid = bid !== null
-
-    return (
-      <div className={classNames.join(' ')}>
-        <div className='auction__team-name'>
-          <TeamName abbrv color tid={team.uid} />
-        </div>
-        <div className='auction__team-cap'>${roster.availableCap}</div>
-        <div className='auction__team-roster-space'>
-          {roster.availableSpace}
-        </div>
-        {hasBid && <div className='auction__team-bid'>$ {bid}</div>}
-      </div>
-    )
+export default function AuctionTeam({
+  team,
+  isConnected,
+  isWinningBid,
+  bid,
+  isNominating,
+  roster,
+  isOwner
+}) {
+  const classNames = ['auction__team']
+  if (!isConnected) {
+    classNames.push('offline')
   }
+
+  if (isNominating) {
+    classNames.push('nominating')
+  }
+
+  if (isWinningBid) {
+    classNames.push('winning')
+  }
+
+  if (isOwner) {
+    classNames.push('owner')
+  }
+
+  const hasBid = bid !== null
+
+  return (
+    <div className={classNames.join(' ')}>
+      <div className='auction__team-name'>
+        <TeamName abbrv color tid={team.uid} />
+      </div>
+      <div className='auction__team-cap'>${roster.availableCap}</div>
+      <div className='auction__team-roster-space'>{roster.availableSpace}</div>
+      {hasBid && <div className='auction__team-bid'>$ {bid}</div>}
+    </div>
+  )
 }
 
 AuctionTeam.propTypes = {
