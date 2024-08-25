4c6a5e611f339f873175899d27498f6ce4ca3ad3
mistakia <1823355+mistakia@users.noreply.github.com>
2024-08-25 12:20:21 -0400

    fix: typo

--- scripts/import-pff-grades.mjs
+++ scripts/import-pff-grades.mjs
@@ -1,6 +1,7 @@
 import debug from 'debug'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+import fetch from 'node-fetch'
 
 import { isMain, report_job, pff, wait, getPlayer } from '#libs-server'
 import { constants, job_constants } from '#libs-shared'
