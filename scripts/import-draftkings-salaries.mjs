b77a12c1dbdcba0e8bc98d6f39fd39a8c430ab4a
mistakia <1823355+mistakia@users.noreply.github.com>
2024-08-28 21:41:20 -0400

    refactor: draftkings contest salary script

--- scripts/import-draftkings-salaries.mjs
+++ scripts/import-draftkings-salaries.mjs
@@ -28,10 +28,11 @@ const import_draftkings_salaries = async ({ dry_run = false } = {}) => {
   })
 
   const salary_inserts = []
-  const matched_draftkings_ids = new Set()
-  const unmatched_draftables = []
 
   for (const draft_group of draft_groups) {
+    const matched_draftkings_ids = new Set()
+    const unmatched_draftables = []
+
     const data = await draftkings.get_draftkings_draft_group_draftables({
       draft_group_id: draft_group.DraftGroupId
     })
@@ -65,40 +66,40 @@ const import_draftkings_salaries = async ({ dry_run = false } = {}) => {
         unmatched_draftables.push({ draftable, draft_group })
       }
     }
-  }
 
-  // Handle unmatched draftables
-  for (const { draftable, draft_group } of unmatched_draftables) {
-    let player_row
+    // Handle unmatched draftables
+    for (const { draftable, draft_group } of unmatched_draftables) {
+      let player_row
 
-    try {
-      player_row = await getPlayer({
-        name: `${draftable.firstName} ${draftable.lastName}`,
-        pos: draftable.position,
-        team: draftable.teamAbbreviation
-      })
-    } catch (err) {
-      log(err)
-    }
-
-    if (player_row) {
-      if (matched_draftkings_ids.has(draftable.playerDkId)) {
-        log(`Duplicate DraftKings ID: ${draftable.playerDkId}`)
-        continue
+      try {
+        player_row = await getPlayer({
+          name: `${draftable.firstName} ${draftable.lastName}`,
+          pos: draftable.position,
+          team: draftable.teamAbbreviation
+        })
+      } catch (err) {
+        log(err)
       }
 
-      matched_draftkings_ids.add(draftable.playerDkId)
-      await process_matched_player({
-        player_row,
-        draftable,
-        draft_group,
-        nfl_games,
-        salary_inserts
-      })
-    } else {
-      log(
-        `no player found for ${draftable.firstName} ${draftable.lastName} - ${draftable.playerDkId}`
-      )
+      if (player_row) {
+        if (matched_draftkings_ids.has(draftable.playerDkId)) {
+          log(`Duplicate DraftKings ID: ${draftable.playerDkId}`)
+          continue
+        }
+
+        matched_draftkings_ids.add(draftable.playerDkId)
+        await process_matched_player({
+          player_row,
+          draftable,
+          draft_group,
+          nfl_games,
+          salary_inserts
+        })
+      } else {
+        log(
+          `no player found for ${draftable.firstName} ${draftable.lastName} - ${draftable.playerDkId}`
+        )
+      }
     }
   }
 
