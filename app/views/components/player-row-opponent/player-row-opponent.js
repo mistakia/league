5f9cb9337cf4c33a9e5d8ca607c63ecfdc770e35
mistakia <1823355+mistakia@users.noreply.github.com>
2024-09-17 18:01:29 -0400

    feat: add `game_opponent` data view field (closes #253)

--- app/views/components/player-row-opponent/player-row-opponent.js
+++ app/views/components/player-row-opponent/player-row-opponent.js
@@ -1,24 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import NFLTeamLogo from '@components/nfl-team-logo'
-
-import './player-row-opponent.styl'
+import GameOpponent from '@components/game-opponent'
 
 export default function PlayerRowOpponent({ game, nfl_team }) {
   if (!game) {
-    return <div className='player__row-opponent' />
+    return <div className='game-opponent' />
   }
 
-  const isHome = game.h === nfl_team
-  const opp = isHome ? game.v : game.h
+  const is_home = game.h === nfl_team
+  const opponent = is_home ? game.v : game.h
 
-  return (
-    <div className='player__row-opponent'>
-      {(isHome ? '' : '@') + opp}
-      <NFLTeamLogo abbrv={opp} size={24} />
-    </div>
-  )
+  return <GameOpponent {...{ is_home, opponent }} />
 }
 
 PlayerRowOpponent.propTypes = {
