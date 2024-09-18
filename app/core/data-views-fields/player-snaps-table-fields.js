4369c0a6a6ebc5fff41d473ea48fdc04be29eff9
mistakia <1823355+mistakia@users.noreply.github.com>
2024-09-18 10:40:59 -0400

    feat: add `player_snaps` data view field (#263)

---
+++ app/core/data-views-fields/player-snaps-table-fields.js
@@ -0,0 +1,16 @@
+import * as table_constants from 'react-table/src/constants.mjs'
+import { nfl_plays_column_params } from '@libs-shared'
+import COLUMN_GROUPS from './column-groups'
+
+export default {
+  player_snaps: {
+    column_title: 'Player Snaps (By Play)',
+    header_label: 'Snaps',
+    player_value_path: 'player_snaps',
+    size: 60,
+    data_type: table_constants.TABLE_DATA_TYPES.NUMBER,
+    column_groups: [COLUMN_GROUPS.OPPURTUNITY],
+    column_params: nfl_plays_column_params,
+    splits: ['year', 'week']
+  }
+}
