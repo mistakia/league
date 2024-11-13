1f95d3aac993d2632af05006c90349f349e721af
mistakia <1823355+mistakia@users.noreply.github.com>
2024-11-13 18:45:55 -0500

    fix: duplicate waiver dates

--- app/views/components/waiver-processed-filter/waiver-processed-filter.js
+++ app/views/components/waiver-processed-filter/waiver-processed-filter.js
@@ -4,32 +4,30 @@ import dayjs from 'dayjs'
 
 import WaiversFilter from '@components/waivers-filter'
 
-export default class WaiverTypeFilter extends React.Component {
-  render = () => {
-    const { processingTimes } = this.props
-
-    const state = {
-      single: true,
-      type: 'processed',
-      label: 'DATE',
-      values: []
-    }
-
-    for (const date of processingTimes.valueSeq()) {
-      const label = dayjs.unix(date).format('ddd, MMM D h:mm YYYY')
-
-      state.values.push({
-        value: date,
-        label,
-        selected: this.props.processed.includes(date)
-      })
-    }
-
-    return <WaiversFilter {...state} />
+export default function WaiverTypeFilter({ processing_times, processed }) {
+  const state = {
+    single: true,
+    type: 'processed',
+    label: 'DATE',
+    values: []
   }
+
+  const unique_dates = new Set(processing_times.valueSeq())
+
+  for (const date of unique_dates) {
+    const label = dayjs.unix(date).format('ddd, MMM D h:mm YYYY')
+
+    state.values.push({
+      value: date,
+      label,
+      selected: processed.includes(date)
+    })
+  }
+
+  return <WaiversFilter {...state} />
 }
 
 WaiverTypeFilter.propTypes = {
-  processingTimes: ImmutablePropTypes.list,
+  processing_times: ImmutablePropTypes.list,
   processed: ImmutablePropTypes.list
 }
