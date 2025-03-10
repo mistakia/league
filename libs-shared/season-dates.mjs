d17019af4ae4bbcd7c7a81ded6bbac19938cef36
mistakia <1823355+mistakia@users.noreply.github.com>
2025-03-09 21:44:10 -0400

    chore: update season dates

--- libs-shared/season-dates.mjs
+++ libs-shared/season-dates.mjs
@@ -2,16 +2,16 @@ const season_dates = {
   // use unix timestamp, start of day, new york timezone
 
   // super bowl
-  offseason: 1710129600,
+  offseason: 1739077200,
 
   // Two Tuesdays before first game
-  start: 1724731200,
+  start: 1756180800,
 
   // super bowl
-  end: 1739077200,
+  end: 1770526800,
 
   // first game
-  openingDay: 1725508800,
+  openingDay: 1756958400,
 
   finalWeek: 17,
   nflFinalWeek: 18,
