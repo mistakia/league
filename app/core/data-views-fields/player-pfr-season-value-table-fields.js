9dc0544d29e470b1c4681b40f449b238bdb6d05c
mistakia <1823355+mistakia@users.noreply.github.com>
2025-05-11 10:37:16 -0400

    feat: enhance player_pfr_season_value column with additional column_params for year and career_year

--- app/core/data-views-fields/player-pfr-season-value-table-fields.js
+++ app/core/data-views-fields/player-pfr-season-value-table-fields.js
@@ -1,5 +1,8 @@
 import COLUMN_GROUPS from './column-groups'
 import * as table_constants from 'react-table/src/constants.mjs'
+import { common_column_params } from '@libs-shared'
+
+const { single_year, career_year } = common_column_params
 
 export default {
   player_pfr_season_value: {
@@ -8,6 +11,10 @@ export default {
     size: 80,
     data_type: table_constants.TABLE_DATA_TYPES.NUMBER,
     player_value_path: 'pfr_season_value',
-    column_groups: [COLUMN_GROUPS.PFR, COLUMN_GROUPS.SEASON]
+    column_groups: [COLUMN_GROUPS.PFR, COLUMN_GROUPS.SEASON],
+    column_params: {
+      year: single_year,
+      career_year
+    }
   }
 }
