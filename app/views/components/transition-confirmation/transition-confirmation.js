7e32ffddad569c146b32c03cccc538f5023e7615
mistakia <1823355+mistakia@users.noreply.github.com>
2025-06-08 20:50:17 -0400

    refactor: update variable naming to snake_case and enhance transition confirmation component with bid warning styling

--- app/views/components/transition-confirmation/transition-confirmation.js
+++ app/views/components/transition-confirmation/transition-confirmation.js
@@ -29,15 +29,16 @@ export default class TransitionConfirmation extends React.Component {
     const { team, playerMap } = props
 
     this.state = {
-      releaseIds: playerMap.get(
+      release_ids: playerMap.get(
         'restricted_free_agency_conditional_releases',
         []
       ),
       untag: '',
       bid: playerMap.get('bid', 0),
       error: false,
-      missingRelease: false,
-      missingUntag: false
+      missing_release: false,
+      missing_untag: false,
+      bid_exceeds_max: false
     }
 
     this._untags = []
@@ -46,13 +47,13 @@ export default class TransitionConfirmation extends React.Component {
     )
     const tagged_pids = tagged_players.map((p) => p.pid)
     for (const pid of tagged_pids) {
-      const playerMap = team.players.find(
-        (playerMap) => playerMap.get('pid') === pid
+      const player_map = team.players.find(
+        (player_map) => player_map.get('pid') === pid
       )
-      if (playerMap.get('transition_tag_processed')) {
+      if (player_map.get('transition_tag_processed')) {
         continue
       }
-      this._untags.push(playerMap)
+      this._untags.push(player_map)
     }
 
     const pid = playerMap.get('pid')
@@ -70,44 +71,59 @@ export default class TransitionConfirmation extends React.Component {
       team.roster.isEligibleForTag({
         tag: constants.tags.TRANSITION
       })
+
+    // Check if initial bid exceeds max bid
+    const initial_bid = playerMap.get('bid', 0)
+    if (initial_bid > 0) {
+      const max_bid = this.get_max_bid()
+      if (initial_bid > max_bid) {
+        this.state.bid_exceeds_max = true
+      }
+    }
   }
 
-  getMaxBid = () => {
-    const availableSalary = this.props.team.roster.availableCap
+  get_max_bid = () => {
+    const available_salary = this.props.team.roster.availableCap
     const { playerMap, cutlistTotalSalary } = this.props
     const value = playerMap.get('value', 0)
     const bid = playerMap.get('bid', 0)
-    const playerSalary = bid || value
+    const player_salary = bid || value
 
-    const notInCutlist = this.state.releaseIds.filter(
+    const not_in_cutlist = this.state.release_ids.filter(
       (pid) => !this.props.cutlist.includes(pid)
     )
-    const releaseTotalSalary = notInCutlist.reduce((sum, pid) => {
-      const playerMap = this.props.team.players.find(
-        (playerMap) => playerMap.get('pid') === pid
+    const release_total_salary = not_in_cutlist.reduce((sum, pid) => {
+      const player_map = this.props.team.players.find(
+        (player_map) => player_map.get('pid') === pid
       )
-      const value = playerMap.get('value', 0)
+      const value = player_map.get('value', 0)
 
       return sum + value
     }, 0)
 
-    const salarySpaceTotal =
-      availableSalary + cutlistTotalSalary + releaseTotalSalary
-    const onCutlist = this.props.cutlist.includes(playerMap.get('pid'))
+    const salary_space_total =
+      available_salary + cutlistTotalSalary + release_total_salary
+    const on_cutlist = this.props.cutlist.includes(playerMap.get('pid'))
     return (
-      salarySpaceTotal + (this._isOriginalTeam && !onCutlist ? playerSalary : 0)
+      salary_space_total +
+      (this._isOriginalTeam && !on_cutlist ? player_salary : 0)
     )
   }
 
   handleBid = (event) => {
     const { value } = event.target
+    const max_bid = this.get_max_bid()
 
     if (isNaN(value) || value % 1 !== 0) {
-      this.setState({ error: true })
+      this.setState({ error: true, bid_exceeds_max: false })
     } else if (value < 0) {
-      this.setState({ error: true })
+      this.setState({ error: true, bid_exceeds_max: false })
     } else {
-      this.setState({ error: false })
+      const bid_exceeds_max = parseInt(value, 10) > max_bid
+      this.setState({
+        error: false,
+        bid_exceeds_max: bid_exceeds_max
+      })
     }
 
     this.setState({ bid: value })
@@ -115,31 +131,39 @@ export default class TransitionConfirmation extends React.Component {
 
   handleRelease = (event, value) => {
     const pids = value.map((p) => p.id)
-    this.setState({ releaseIds: pids })
+    this.setState({ release_ids: pids }, () => {
+      // Recalculate if bid exceeds max after release changes
+      const current_bid = parseInt(this.state.bid, 10)
+      if (current_bid > 0) {
+        const max_bid = this.get_max_bid()
+        const bid_exceeds_max = current_bid > max_bid
+        this.setState({ bid_exceeds_max: bid_exceeds_max })
+      }
+    })
   }
 
   handleUntag = (event) => {
     const { value } = event.target
-    this.setState({ untag: value, missingUntag: false })
+    this.setState({ untag: value, missing_untag: false })
   }
 
   handleSubmit = () => {
     const { untag, error, bid } = this.state
     const { tid } = this.props.team.roster
     const pid = this.props.playerMap.get('pid')
-    const playerTid = this.props.playerMap.get('tid')
+    const player_tid = this.props.playerMap.get('tid')
 
     if (!this._isEligible && !untag) {
-      return this.setState({ missingUntag: true })
+      return this.setState({ missing_untag: true })
     } else {
-      this.setState({ missingUntag: false })
+      this.setState({ missing_untag: false })
     }
 
     if (!error) {
       const data = {
         pid,
-        release: this.state.releaseIds,
-        playerTid,
+        release: this.state.release_ids,
+        playerTid: player_tid,
         teamId: tid,
         bid: parseInt(bid, 10),
         remove: untag
@@ -158,40 +182,40 @@ export default class TransitionConfirmation extends React.Component {
   render = () => {
     const { team, playerMap } = this.props
 
-    const menuItems = []
-    for (const rPlayerMap of this._untags) {
-      const pid = rPlayerMap.get('pid')
-      menuItems.push(
+    const menu_items = []
+    for (const r_player_map of this._untags) {
+      const pid = r_player_map.get('pid')
+      menu_items.push(
         <MenuItem key={pid} value={pid}>
-          {rPlayerMap.get('name')} ({rPlayerMap.get('pos')})
+          {r_player_map.get('name')} ({r_player_map.get('pos')})
         </MenuItem>
       )
     }
 
     const options = []
     const pid = playerMap.get('pid')
-    team.players.forEach((playerMap) => {
-      const pid_i = playerMap.get('pid')
+    team.players.forEach((player_map) => {
+      const pid_i = player_map.get('pid')
       if (pid_i === pid) {
         return
       }
 
-      if (playerMap.get('tag') === constants.tags.TRANSITION) {
+      if (player_map.get('tag') === constants.tags.TRANSITION) {
         return
       }
 
-      const pos = playerMap.get('pos')
+      const pos = player_map.get('pos')
       options.push({
         id: pid_i,
-        label: playerMap.get('name'),
+        label: player_map.get('name'),
         pos,
-        team: playerMap.get('team'),
-        pname: playerMap.get('pname'),
-        value: playerMap.get('value')
+        team: player_map.get('team'),
+        pname: player_map.get('pname'),
+        value: player_map.get('value')
       })
     })
-    const isOptionEqualToValue = (option, value) => option.id === value.id
-    const renderOption = (props, option) => {
+    const is_option_equal_to_value = (option, value) => option.id === value.id
+    const render_option = (props, option) => {
       return (
         <div {...props}>
           <div className='release__select-player'>
@@ -207,13 +231,13 @@ export default class TransitionConfirmation extends React.Component {
         </div>
       )
     }
-    const renderTags = (value, getTagProps) =>
+    const render_tags = (value, getTagProps) =>
       value.map((option, index) => (
         // eslint-disable-next-line
         <Chip label={option.label} {...getTagProps({ index })} />
       ))
     const title = 'Select players to conditionally release'
-    const renderInput = (params) => (
+    const render_input = (params) => (
       <TextField
         {...params}
         variant='outlined'
@@ -221,18 +245,18 @@ export default class TransitionConfirmation extends React.Component {
         placeholder={title}
       />
     )
-    const releasePlayers = []
-    this.state.releaseIds.forEach((pid) => {
-      const playerMap = this.props.team.players.find(
-        (playerMap) => playerMap.get('pid') === pid
+    const release_players = []
+    this.state.release_ids.forEach((pid) => {
+      const player_map = this.props.team.players.find(
+        (player_map) => player_map.get('pid') === pid
       )
-      releasePlayers.push({
-        id: playerMap.get('pid'),
-        label: playerMap.get('name'),
-        pos: playerMap.get('pos'),
-        team: playerMap.get('team'),
-        pname: playerMap.get('pname'),
-        value: playerMap.get('value')
+      release_players.push({
+        id: player_map.get('pid'),
+        label: player_map.get('name'),
+        pos: player_map.get('pos'),
+        team: player_map.get('team'),
+        pname: player_map.get('pname'),
+        value: player_map.get('value')
       })
     })
 
@@ -248,7 +272,7 @@ export default class TransitionConfirmation extends React.Component {
           <div className='transition__bid-inputs'>
             <TextField
               label='Bid'
-              helperText={`Max Bid: ${this.getMaxBid()}`}
+              helperText={`Max Bid: ${this.get_max_bid()}`}
               error={this.state.error}
               value={this.state.bid}
               onChange={this.handleBid}
@@ -260,17 +284,25 @@ export default class TransitionConfirmation extends React.Component {
               size='small'
               variant='outlined'
             />
+            {this.state.bid_exceeds_max && (
+              <div className='transition__bid-warning'>
+                <strong>⚠️ WARNING:</strong> Your bid of ${this.state.bid}{' '}
+                exceeds the maximum available bid of ${this.get_max_bid()}. You
+                may not have sufficient salary cap space to successfully process
+                this transaction.
+              </div>
+            )}
             {!this._isEligible && (
               <FormControl size='small' variant='outlined'>
                 <InputLabel id='untag-label'>Remove Tag</InputLabel>
                 <Select
                   labelId='untag-label'
-                  error={this.state.missingUntag}
+                  error={this.state.missing_untag}
                   value={this.state.untag}
                   onChange={this.handleUntag}
                   label='Remove Tag'
                 >
-                  {menuItems}
+                  {menu_items}
                 </Select>
               </FormControl>
             )}
@@ -278,13 +310,13 @@ export default class TransitionConfirmation extends React.Component {
               multiple
               options={options}
               getOptionLabel={(x) => x.label}
-              isOptionEqualToValue={isOptionEqualToValue}
-              renderOption={renderOption}
+              isOptionEqualToValue={is_option_equal_to_value}
+              renderOption={render_option}
               filterSelectedOptions
-              value={releasePlayers}
+              value={release_players}
               onChange={this.handleRelease}
-              renderTags={renderTags}
-              renderInput={renderInput}
+              renderTags={render_tags}
+              renderInput={render_input}
             />
           </div>
         </DialogContent>
