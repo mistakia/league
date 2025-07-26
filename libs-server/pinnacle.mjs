2b4c46dbbf81b2c34141cc6cebf31d02506c4540
mistakia <1823355+mistakia@users.noreply.github.com>
2025-07-25 22:19:09 -0400

    refactor: enhance pinnacle market type handling and improve odds processing
    - Renamed get_market_type to get_market_type_for_matchup and get_market_type_for_special for clarity
    - Introduced new logic for handling special market types and improved logging for unmatched cases
    - Updated import-pinnacle-odds script to process market odds with enhanced structure and added options for saving data
    - Improved variable naming consistency and added new arguments for better readability

--- libs-server/pinnacle.mjs
+++ libs-server/pinnacle.mjs
@@ -32,94 +32,192 @@ export const format_selection_type = (selection_name) => {
   return null
 }
 
-export const get_market_type = ({ type, units, category }) => {
-  switch (category) {
-    case 'Game Lines':
-      switch (type) {
-        case 'Spread':
-          return bookmaker_constants.team_game_market_types.GAME_SPREAD
-        case 'Total':
-          return bookmaker_constants.team_game_market_types.GAME_TOTAL
-        case 'Moneyline':
-          return bookmaker_constants.team_game_market_types.GAME_MONEYLINE
-        default:
-          log(`unknown game market type: ${type} for Game Lines`)
-          return null
-      }
-
-    case 'Player Props':
-      switch (units) {
-        case 'RushingYards':
-          return bookmaker_constants.player_game_prop_types.GAME_RUSHING_YARDS
-        case 'Touchdowns':
-          return bookmaker_constants.player_game_prop_types
-            .GAME_RUSHING_RECEIVING_TOUCHDOWNS
-        case 'TouchdownPasses':
-          return bookmaker_constants.player_game_prop_types
-            .GAME_PASSING_TOUCHDOWNS
-        case 'ReceivingYards':
-          return bookmaker_constants.player_game_prop_types.GAME_RECEIVING_YARDS
-        case 'PassReceptions':
-          return bookmaker_constants.player_game_prop_types.GAME_RECEPTIONS
-        case 'LongestPassComplete':
-          return bookmaker_constants.player_game_prop_types
-            .GAME_PASSING_LONGEST_COMPLETION
-        case 'PassingYards':
-          return bookmaker_constants.player_game_prop_types.GAME_PASSING_YARDS
-        case 'LongestReception':
-          return bookmaker_constants.player_game_prop_types
-            .GAME_LONGEST_RECEPTION
-        case 'KickingPoints':
-          return null // Not standardized, return null or create a new constant if needed
-        case 'Completions':
-          return bookmaker_constants.player_game_prop_types
-            .GAME_PASSING_COMPLETIONS
-        case 'PassAttempts':
-          return bookmaker_constants.player_game_prop_types
-            .GAME_PASSING_ATTEMPTS
-        case 'Interceptions':
-          return bookmaker_constants.player_game_prop_types
-            .GAME_PASSING_INTERCEPTIONS
-        case '1st Touchdown':
-          return bookmaker_constants.player_game_prop_types
-            .GAME_FIRST_TOUCHDOWN_SCORER
-        case 'Last Touchdown':
-          return bookmaker_constants.player_game_prop_types
-            .GAME_LAST_TOUCHDOWN_SCORER
-        default:
-          log(`unknown player prop type: ${units} for Player Props`)
-          return null
-      }
-
-    case 'Regular Season Wins':
-      return 'SEASON_WINS'
-    case 'NFL Offensive Rookie of the Year':
-      return 'NFL_OFFENSIVE_ROOKIE_OF_THE_YEAR'
-    case 'NFL Comeback Player of the Year':
-      return 'NFL_COMEBACK_PLAYER_OF_THE_YEAR'
-    case 'NFL Coach of the Year':
-      return 'NFL_COACH_OF_THE_YEAR'
-    case 'NFL Defensive Player of the Year':
-      return 'NFL_DEFENSIVE_PLAYER_OF_THE_YEAR'
-    case 'NFL Most Valuable Player':
-      return 'NFL_MOST_VALUABLE_PLAYER'
-    case 'NFL Defensive Rookie of the Year':
-      return 'NFL_DEFENSIVE_ROOKIE_OF_THE_YEAR'
-    case 'NFL Offensive Player of the Year':
-      return 'NFL_OFFENSIVE_PLAYER_OF_THE_YEAR'
-
+export const get_market_type_for_matchup = ({
+  pinnacle_odds_type,
+  is_alternate_pinnacle_market
+}) => {
+  switch (pinnacle_odds_type.toLowerCase()) {
+    case 'spread':
+      return is_alternate_pinnacle_market
+        ? bookmaker_constants.team_game_market_types.GAME_ALT_SPREAD
+        : bookmaker_constants.team_game_market_types.GAME_SPREAD
+    case 'total':
+      return is_alternate_pinnacle_market
+        ? bookmaker_constants.team_game_market_types.GAME_ALT_TOTAL
+        : bookmaker_constants.team_game_market_types.GAME_TOTAL
+    case 'moneyline':
+      return bookmaker_constants.team_game_market_types.GAME_MONEYLINE
+    case 'team_total':
+      return is_alternate_pinnacle_market
+        ? bookmaker_constants.team_game_market_types.GAME_ALT_TEAM_TOTAL
+        : bookmaker_constants.team_game_market_types.GAME_TEAM_TOTAL
     default:
-      log(`unknown market type: ${category} / ${type} / ${units}`)
+      log(`unknown game market type: ${pinnacle_odds_type} for matchup`)
       return null
   }
 }
 
-export const get_nfl_markets = async ({ ignore_cache = false } = {}) => {
-  const cache_key = `/pinnacle/nfl_markets/${constants.season.year}/${constants.season.week}.json`
+export const get_market_type_for_special = ({
+  pinnacle_special_category,
+  pinnacle_special_description,
+  pinnacle_odds_type,
+  pinnacle_matchup_units,
+  is_alternate_pinnacle_market
+}) => {
+  switch (pinnacle_special_category) {
+    case 'To Make Playoffs':
+      if (
+        pinnacle_odds_type === 'moneyline' &&
+        pinnacle_matchup_units === 'Regular'
+      ) {
+        return bookmaker_constants.team_season_types.TEAM_TO_MAKE_PLAYOFFS
+      }
+      break
+
+    case 'Regular Season Wins':
+      if (pinnacle_odds_type === 'total' && pinnacle_matchup_units === 'Wins') {
+        return bookmaker_constants.team_season_types.TEAM_REGULAR_SEASON_WINS
+      }
+      break
+
+    case 'Futures':
+      if (
+        pinnacle_odds_type === 'moneyline' &&
+        pinnacle_matchup_units === 'Place 1st'
+      ) {
+        if (pinnacle_special_description?.includes('Super Bowl')) {
+          return bookmaker_constants.futures_types.SUPER_BOWL_WINNER
+        } else if (pinnacle_special_description?.includes('AFC Winner')) {
+          return bookmaker_constants.futures_types.CONFERENCE_WINNER
+        } else if (pinnacle_special_description?.includes('NFC Winner')) {
+          return bookmaker_constants.futures_types.CONFERENCE_WINNER
+        } else if (
+          pinnacle_special_description?.includes('East Winner') ||
+          pinnacle_special_description?.includes('West Winner') ||
+          pinnacle_special_description?.includes('North Winner') ||
+          pinnacle_special_description?.includes('South Winner')
+        ) {
+          return bookmaker_constants.futures_types.DIVISION_WINNER
+        }
+      }
+      break
+
+    case 'NFL Most Valuable Player':
+      if (
+        pinnacle_odds_type === 'moneyline' &&
+        pinnacle_matchup_units === 'Regular'
+      ) {
+        return bookmaker_constants.awards_prop_types.SEASON_MVP
+      }
+      break
+
+    case 'NFL Defensive Player of the Year':
+      if (
+        pinnacle_odds_type === 'moneyline' &&
+        pinnacle_matchup_units === 'Regular'
+      ) {
+        return bookmaker_constants.awards_prop_types
+          .DEFENSIVE_PLAYER_OF_THE_YEAR
+      }
+      break
+
+    case 'NFL Offensive Player of the Year':
+      if (
+        pinnacle_odds_type === 'moneyline' &&
+        pinnacle_matchup_units === 'Regular'
+      ) {
+        return bookmaker_constants.awards_prop_types
+          .OFFENSIVE_PLAYER_OF_THE_YEAR
+      }
+      break
+
+    case 'NFL Offensive Rookie of the Year':
+      if (
+        pinnacle_odds_type === 'moneyline' &&
+        pinnacle_matchup_units === 'Regular'
+      ) {
+        return bookmaker_constants.awards_prop_types
+          .OFFENSIVE_ROOKIE_OF_THE_YEAR
+      }
+      break
+
+    case 'NFL Defensive Rookie of the Year':
+      if (
+        pinnacle_odds_type === 'moneyline' &&
+        pinnacle_matchup_units === 'Regular'
+      ) {
+        return bookmaker_constants.awards_prop_types
+          .DEFENSIVE_ROOKIE_OF_THE_YEAR
+      }
+      break
+
+    case 'NFL Coach of the Year':
+      if (
+        pinnacle_odds_type === 'moneyline' &&
+        pinnacle_matchup_units === 'Regular'
+      ) {
+        return bookmaker_constants.awards_prop_types.COACH_OF_THE_YEAR
+      }
+      break
+
+    case 'NFL Protector of the Year':
+      if (
+        pinnacle_odds_type === 'moneyline' &&
+        pinnacle_matchup_units === 'Place 1st'
+      ) {
+        return bookmaker_constants.awards_prop_types.PROTECTOR_OF_THE_YEAR
+      }
+      break
+
+    default:
+      log(`unmapped special category: ${pinnacle_special_category}`)
+      break
+  }
+
+  // Log unmatched cases for known categories
+  if (pinnacle_special_category) {
+    log(
+      `unmatched special category: ${pinnacle_special_category} / ${pinnacle_special_description} / ${pinnacle_odds_type} / ${pinnacle_matchup_units}`
+    )
+  }
+
+  return null
+}
+
+export const get_market_type = ({
+  is_alternate_pinnacle_market = false,
+  pinnacle_matchup_type,
+  pinnacle_special_description,
+  pinnacle_special_category,
+  pinnacle_odds_type,
+  pinnacle_matchup_units
+}) => {
+  switch (pinnacle_matchup_type) {
+    case 'matchup':
+      return get_market_type_for_matchup({
+        pinnacle_odds_type,
+        is_alternate_pinnacle_market
+      })
+    case 'special':
+      return get_market_type_for_special({
+        pinnacle_special_category,
+        pinnacle_special_description,
+        pinnacle_odds_type,
+        pinnacle_matchup_units,
+        is_alternate_pinnacle_market
+      })
+    default:
+      log(`unknown market type: ${pinnacle_matchup_type}`)
+      return null
+  }
+}
+
+export const get_nfl_matchups = async ({ ignore_cache = false } = {}) => {
+  const cache_key = `/pinnacle/nfl_matchups/${constants.season.year}/${constants.season.week}.json`
   if (!ignore_cache) {
     const cache_value = await cache.get({ key: cache_key })
     if (cache_value) {
-      log('cache hit for nfl markets')
+      log('cache hit for nfl matchups')
       return cache_value
     }
   }
@@ -143,27 +241,25 @@ export const get_nfl_markets = async ({ ignore_cache = false } = {}) => {
 }
 
 export const get_market_odds = async ({
-  market_parent_id,
+  matchup_id,
   ignore_cache = false
 } = {}) => {
-  if (!market_parent_id) {
-    throw new Error('market_parent_id is required')
+  if (!matchup_id) {
+    throw new Error('matchup_id is required')
   }
 
-  const cache_key = `/pinnacle/market_odds/${market_parent_id}.json`
+  const cache_key = `/pinnacle/market_odds/${matchup_id}.json`
   if (!ignore_cache) {
     const cache_value = await cache.get({ key: cache_key })
     if (cache_value) {
-      log(
-        `cache hit for market odds with market_parent_id: ${market_parent_id}`
-      )
+      log(`cache hit for market odds with matchup_id: ${matchup_id}`)
       return cache_value
     }
   }
 
   const pinnacle_config = await get_pinnacle_config()
 
-  const url = `${pinnacle_config.api_url}/matchups/${market_parent_id}/markets/related/straight`
+  const url = `${pinnacle_config.api_url}/matchups/${matchup_id}/markets/related/straight`
   log(`fetching ${url}`)
 
   const response = await fetch_http2(url, {
